ef238e4b823aac19eedb6d40a4170e9955ef3e98
K. Shankari <shankari@eecs.berkeley.edu>
2016-01-27 23:01:47 -0800

    Change the app name from "starter" to "emission"

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,11 @@
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'emission' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+// 'emission.services' is found in services.js
+// 'emission.controllers' is found in controllers.js
+angular.module('emission', ['ionic', 'emission.controllers', 'emission.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
