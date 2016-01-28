d2592afb431b677656f2be2c5867e667912d9b68
K. Shankari <shankari@eecs.berkeley.edu>
2016-01-28 11:50:22 -0800

    Turn off swiping after the first slide
    While it is fine to swipe past the summary slide, we don't want users to have
    the ability to skip past any of the other slides.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,20 +1,40 @@
 angular.module('emission.controllers', [])
 
 .controller('IntroCtrl', function($scope, $state, $ionicSlideBoxDelegate) {
+  $scope.getIntroBox = function() {
+    return $ionicSlideBoxDelegate.$getByHandle('intro-box');
+  };
+
+  $scope.stopSliding = function() {
+    $scope.getIntroBox().enableSlide(false);
+  };
+
   $scope.startApp = function() {
     $state.go('main.dash');
   };
 
   $scope.next = function() {
-    $ionicSlideBoxDelegate.next();
+    $scope.getIntroBox().next();
   };
   $scope.previous = function() {
-    $ionicSlideBoxDelegate.previous();
+    $scope.getIntroBox().previous();
   };
 
   // Called each time the slide changes
   $scope.slideChanged = function(index) {
     $scope.slideIndex = index;
+    /*
+     * The slidebox is created as a child of the HTML page that this controller
+     * is associated with, so it is not available when the controller is created.
+     * There is an onLoad, but it is for ng-include, not for random divs, apparently.
+     * Trying to create a new controller complains because then both the
+     * directive and the controller are trying to ask for a new scope.
+     * So instead, I turn off swiping after the initial summary is past.
+     * Since the summary is not legally binding, it is fine to swipe past it...
+     */
+    if (index > 0) {
+        $scope.getIntroBox().enableSlide(false);
+    }
   };
 })
 
