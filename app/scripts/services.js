124d8241a632af035b105fb7019515ba4f493649
K. Shankari <shankari@eecs.berkeley.edu>
2016-02-03 11:55:00 -0800

    Fix the path to the images back since we are going with original cordova

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -18,52 +18,52 @@ angular.module('emission.services', [])
     id: 0,
     name: 'Ben Sparrow',
     lastText: 'You on your way?',
-    face: 'images/ben.png'
+    face: 'img/ben.png'
   }, {
     id: 1,
     name: 'Max Lynx',
     lastText: 'Hey, it\'s me',
-    face: 'images/max.png'
+    face: 'img/max.png'
   }, {
     id: 2,
     name: 'Adam Bradleyson',
     lastText: 'I should buy a boat',
-    face: 'images/adam.jpg'
+    face: 'img/adam.jpg'
   }, {
     id: 3,
     name: 'Perry Governor',
     lastText: 'Look at my mukluks!',
-    face: 'images/perry.png'
+    face: 'img/perry.png'
   }, {
     id: 4,
     name: 'Mike Harrington',
     lastText: 'This is wicked good ice cream.',
-    face: 'images/mike.png'
+    face: 'img/mike.png'
   }, {
     id: 5,
     name: 'Ben Sparrow',
     lastText: 'You on your way again?',
-    face: 'images/ben.png'
+    face: 'img/ben.png'
   }, {
     id: 6,
     name: 'Max Lynx',
     lastText: 'Hey, it\'s me again',
-    face: 'images/max.png'
+    face: 'img/max.png'
   }, {
     id: 7,
     name: 'Adam Bradleyson',
     lastText: 'I should buy a boat again',
-    face: 'images/adam.jpg'
+    face: 'img/adam.jpg'
   }, {
     id: 8,
     name: 'Perry Governor',
     lastText: 'Look at my mukluks again!',
-    face: 'images/perry.png'
+    face: 'img/perry.png'
   }, {
     id: 9,
     name: 'Mike Harrington',
     lastText: 'This is wicked good ice cream again.',
-    face: 'images/mike.png'
+    face: 'img/mike.png'
   }];
 
   return {
