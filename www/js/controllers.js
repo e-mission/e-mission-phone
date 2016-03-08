7b5b6214d5772e11962d11cc3a84d0f024eafdaa
K. Shankari <shankari@eecs.berkeley.edu>
2016-03-07 20:43:46 -0800

    Finally fix WSOD for once and for all
    I think that part of the reason that the WSOD issue was so hard to fix is that
    there were multiple causes.
    
    - race between splashscreen plugin and the device plugin causes device ready to
      never fire (https://issues.apache.org/jira/browse/CB-10530), fixed in
    cordova-ios@4.1.0.
    - issues with races between config and run if we waited for device ready and
      then started loading
    - if the initial load failed, we never retry
    
    Fixed all of these by adding a new splash screen and splash state that is
    loaded by default. The splash screen then loads the user preferred state in a
    loop until the load completes.
    
    This seems to work pretty reliably, after moving the pluginInit of the data
    collector to its own thread, I don't get a WSOD even on the initial load, which
    used to fail pretty reliably.
    
    And because I try until I find the plugin for the appPreferences, all subequent
    code can use native plugins without any problems.
    
    Because of this, I was able to restore the original principled loading.
    
    I believe that this fixes both:
    https://github.com/e-mission/e-mission-phone/issues/12
    and
    https://github.com/e-mission/e-mission-phone/issues/19
    
    Yay!

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,35 +2,48 @@
 
 angular.module('emission.controllers', [])
 
-.controller('RootCtrl', function($scope, $state, $ionicPopup, $ionicLoading) {
-  console.log('RootCtrl invoked');
-  alert("Welcome to e-mission! This will be removed post-alpha");
-  $ionicLoading.show({
-      template: 'Checking startup state...'
-  });
-  var prefs = window.plugins.appPreferences;
-  prefs.fetch('setup_complete').then(function(value) {
-      console.log('setup_complete result '+value);
-      $scope.$apply(function() {
-        if (value == true) {
-            $state.go('root.main.diary');
-            $ionicLoading.hide();
-        } else {
-            $state.go('root.intro');
-            $ionicLoading.hide();
-        }
-      });
-  }, function(error) {
-      $scope.$apply(function() {
-          $ionicPopup.alert({template: "setup_complete, error -> "+error});
-          $state.go('root.intro');
-          $ionicLoading.hide();
-      });
-  });
-  console.log('RootCtrl invoke finished');
-})
+.controller('RootCtrl', function($scope) {})
 
 .controller('DashCtrl', function($scope) {})
+
+.controller('SplashCtrl', function($scope, $ionicPlatform, $state, $interval, $rootScope) {
+  console.log('SplashCtrl invoked');
+  // Currently loads main or intro based on whether onboarding is complete.
+  // But easily extensible to storing the last screen that the user was on, 
+  // or the users' preferred screen
+  var changeState = function(destState) {
+    console.log("loading "+destState);
+    $state.go(destState);
+    $interval.cancel(currPromise);
+  };
+
+  var loadPreferredScreen = function() {
+    console.log("Checking to see whether we are ready to load the screen");
+    if (window.plugins && window.plugins.appPreferences) {
+      var prefs = plugins.appPreferences;
+        prefs.fetch('setup_complete').then(function(value) {
+          console.log('setup_complete result '+value);
+          if (value == true) {
+              changeState('root.main.diary');
+          } else {
+              changeState('root.intro');
+          }
+        }, function(error) {
+          console.log("error "+error+" loading root.intro");
+          changeState('root.intro');
+        });
+    } else {
+      console.log("appPreferences plugin not installed, waiting...");
+    }
+  }
+  var currPromise = $interval(loadPreferredScreen, 1000);
+  $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    console.log("Finished changing state from "+JSON.stringify(fromState)
+        + " to "+JSON.stringify(toState)
+        + " with event" + JSON.stringify(event));
+  });
+  console.log('SplashCtrl invoke finished');
+})
 
 
 .controller('ChatsCtrl', function($scope, Chats) {
