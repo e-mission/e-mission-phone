505db5b51108120d258714b372ab1fe7e8ef522e
K. Shankari <shankari@eecs.berkeley.edu>
2016-03-18 23:13:06 -0700

    Minor changes to the detail screen to make it actually show the trip
    It was an easy change for me to take, and should make it much easier for Kris
    to finish the detail screen.

--- www/js/diary/detail.js
+++ www/js/diary/detail.js
@@ -6,5 +6,23 @@ angular.module('emission.main.diary.detail',['ui-leaflet', 'nvd3ChartDirectives'
 .controller("DiaryDetailCtrl", function($scope, $stateParams, Timeline) {
   console.log("controller DiaryDetailCtrl called with params = "+
     JSON.stringify($stateParams));
+
+  $scope.mapCtrl = {};
+
+  angular.extend($scope.mapCtrl, {
+    defaults : {
+      tileLayer: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      tileLayerOptions: {
+          opacity: 0.9,
+          detectRetina: true,
+          reuseTiles: true,
+      }
+    } 
+  });
+
   $scope.trip = Timeline.getTrip($stateParams.tripId);
+
+  $scope.tripgj = {}
+  $scope.tripgj.data = $scope.trip;
+  console.log("trip.start_place = " + JSON.stringify($scope.trip.start_place));
 })
