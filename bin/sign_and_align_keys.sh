112a094980c915439b49299e6869b25cec2744f2
K. Shankari <shankari@eecs.berkeley.edu>
2016-06-23 17:15:09 -0700

    Create a non-crosswalk version for android 5.0+, which has a system updateable webview
    https://play.google.com/store/apps/details?id=com.google.android.webview
    https://infinum.co/the-capsized-eight/articles/the-updateable-webview-on-android-5-lollipop-what-is-it-and-why-should-you-care
    
    Instructions from:
    https://github.com/crosswalk-project/cordova-plugin-crosswalk-webview#install

--- bin/sign_and_align_keys.sh
+++ bin/sign_and_align_keys.sh
@@ -5,10 +5,22 @@ if [[ $# -eq 0 ]]; then
     exit 1
 fi
 
+# Sign and release arm7
 cp platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk platforms/android/build/outputs/apk/android-armv7-release-signed-unaligned.apk
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/Safety_Infrastructure/MovesConnect/production.keystore ./platforms/android/build/outputs/apk/android-armv7-release-signed-unaligned.apk androidproductionkey
 ~/Library/Android/sdk/build-tools/22.0.1/zipalign -v 4 platforms/android/build/outputs/apk/android-armv7-release-signed-unaligned.apk emission-armv7-build-$1.apk
 
+# Sign and release x86
 cp platforms/android/build/outputs/apk/android-x86-release-unsigned.apk platforms/android/build/outputs/apk/android-x86-release-signed-unaligned.apk
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/Safety_Infrastructure/MovesConnect/production.keystore ./platforms/android/build/outputs/apk/android-x86-release-signed-unaligned.apk androidproductionkey
 ~/Library/Android/sdk/build-tools/22.0.1/zipalign -v 4 platforms/android/build/outputs/apk/android-x86-release-signed-unaligned.apk emission-x86-build-$1.apk
+
+# Remove the plugin, sign and release general
+ionic plugin remove cordova-plugin-crosswalk-webview
+ionic build android --release -- --minSdkVersion=21
+cp platforms/android/build/outputs/apk/android-release-unsigned.apk platforms/android/build/outputs/apk/android-L+-release-signed-unaligned.apk
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/Safety_Infrastructure/MovesConnect/production.keystore ./platforms/android/build/outputs/apk/android-L+-release-signed-unaligned.apk androidproductionkey
+~/Library/Android/sdk/build-tools/22.0.1/zipalign -v 4 platforms/android/build/outputs/apk/android-L+-release-signed-unaligned.apk emission-L+-build-$1.apk
+
+# Re-add the plugin
+ionic plugin add cordova-plugin-crosswalk-webview  --variable XWALK_MODE="lite"
