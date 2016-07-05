ed03f3e2e07c2c789594bc9ba2b1cdf783aa1a3e
Weijian <weijian.ma@berkeley.edu>
2016-07-05 11:39:55 -0700

    StylesHelper not working

---
+++ www/js/style.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('emission.styles', [])
+
+.service('StylesHelper', function($window, $scope, $rootScope) {
+	this.listCardClass = function() {
+      if ($window.screen.width <= 320) {
+        return ($scope.dark_theme)? "list card list-card-dark list-card-sm" : "list card list-card list-card-sm"; 
+      } else if ($window.screen.width <= 375) {
+        return ($scope.dark_theme)? "list card list-card-dark list-card-md" : "list card list-card list-card-md"; 
+      } else {
+        return ($scope.dark_theme)? "list card list-card-dark list-card-lg" : "list card list-card list-card-lg"; 
+      }
+      
+    }
+});
