efaf2b24f69a76ad140124d1dfae3cf6589853f9
SUNILJTHAPA <sunil07t@gmail.com>
2016-07-08 20:35:20 -0700

    Form regresters the username

--- www/js/goals/signup.js
+++ www/js/goals/signup.js
@@ -4,25 +4,20 @@ angular.module('emission.main.signup', ['emission.services'])
 
 .controller('SignupCtrl', function($scope, CommHelper, $ionicLoading, $state, $rootScope, $ionicPopup) {
 	$scope.user = {}
-	var theUser = "Test";
-	$scope.submitForm = function(){
-		theUser = $scope.user.username;
-		console.log($scope.user.username);
-		console.log(theUser);
-	}
-
+	
 	$scope.signup = function(){
-		var regConfig = {'username': theUser};
+		console.log($scope.user.username);
+		var regConfig = {'username': $scope.user.username};
 		console.log(regConfig);
 		$ionicLoading.show({
 			template: 'Loading...'
 		});
 		CommHelper.habiticaRegister(regConfig, function(response) {
 			$ionicLoading.hide();
-			$state.go("root.main.goals");
+			$state.go('root.main.goals');
 		}, function(error) {
 			$ionicLoading.hide();
-			$state.go("root.main.goals");
+			$state.go('root.main.goals');
 			$ionicPopup.alert({title: "<h4 class='center-align'>Username is Required</h4>",
 								okText: "Try Again",
 								okType: "button-assertive"});
