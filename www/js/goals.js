d3418834f1b437de5f1b0951fc93b38acdf35539
SUNILJTHAPA <sunil07t@gmail.com>
2016-07-16 12:13:18 -0700

    Goals list is connected with the server, Add goal, delet goal, score up and down.

--- www/js/goals.js
+++ www/js/goals.js
@@ -18,7 +18,7 @@ angular.module('emission.main.goals',['emission.services', 'ngSanitize'])
 
 
 .controller('GoalsCtrl', function(CommHelper, $state, $ionicLoading, $scope, $rootScope, $ionicModal, 
-								$window, $http, $sce, $ionicPopup){
+								$window, $http, $ionicGesture, $ionicPopup, $timeout){
 	$scope.goals = [];
 	$scope.goal = {};
 
@@ -28,6 +28,16 @@ angular.module('emission.main.goals',['emission.services', 'ngSanitize'])
 	}).then(function(modal) {
 		$scope.modal = modal;
 	});
+
+	/*$scope.onGesture = function(gesture) {
+    	console.log(gesture);
+  	}*/
+
+  	//var element = angular.element(document.querySelector('#todo')); 
+
+	$scope.data = {
+    	showDelete: false
+  	};  
 
 	$scope.openModal = function() {
     	$scope.modal.show();
@@ -44,10 +54,6 @@ angular.module('emission.main.goals',['emission.services', 'ngSanitize'])
 
 	$scope.removeGoal = function(goal) {
 		$scope.goals.splice($scope.goals.indexOf(goal), 1);
-	};
-
-	$scope.completeGoal = function(goal) {
-		$scope.goal.completed = true;
 	};
 
 	$scope.theUser = {}
@@ -77,16 +83,21 @@ angular.module('emission.main.goals',['emission.services', 'ngSanitize'])
     $ionicLoading.show({
 			template: '<ion-spinner icon="bubbles" class="costume"></ion-spinner>'
 		});
+
+    var userInfo = function(){
 	var callOpts = {'method': 'GET', 'method_url': "/api/v3/user",
                     'method_args': null};
-    //callOpts = {'method': 'GET', 'method_url': "/export/avatar-",
-    				//'method_args': };
+
 	CommHelper.habiticaProxy(callOpts, function(response){
 		$scope.screen = response.success;
 		$scope.$apply(function() {
 			$scope.profile = response.data;
 		})
 		console.log("Proxy Sucess");
+		$scope.gold = Math.round($scope.profile.stats.gp);
+		$scope.gem = Math.round($scope.profile.balance);
+		$scope.silver = Math.round(($scope.profile.stats.gp - 
+			Math.floor($scope.profile.stats.gp))*100);
 		console.log($scope.profile);
 		$ionicLoading.hide();
 		}, function(error){
@@ -94,21 +105,103 @@ angular.module('emission.main.goals',['emission.services', 'ngSanitize'])
 			console.log(error.data);
 			console.log("error");
 		});
+	};
+
+	userInfo();
 
 	var callOpts = {'method': 'GET', 'method_url': "/api/v3/tasks/user",
                     'method_args': null};
-
+    var tasks;
     CommHelper.habiticaProxy(callOpts, function(response){
 		$scope.$apply(function() {
-			$scope.task = response.data;
+			tasks = response.data;
 		})
-		console.log($scope.task[0]);
-		$scope.goal.name = $scope.task[0].text;
-		$scope.createGoal();
+		console.log(tasks);
+			for(var habit in tasks){
+				if(tasks[habit].type == "habit"){
+					$scope.goal.name = tasks[habit].text;
+					$scope.goal.note = tasks[habit].notes;
+					$scope.goal._id = tasks[habit]._id;
+					$scope.createGoal();
+				}
+			}
 		}, function(error){
 			console.log(JSON.stringify(error));
 			console.log("error");
 		});
+
+    $scope.createGoalPhone = function() {
+	   	var callOpts = {'method': 'POST', 'method_url': "/api/v3/tasks/user",
+	                    'method_args': {'type': "habit", 'text': $scope.goal.name, 'notes': $scope.goal.note}};
+
+	    CommHelper.habiticaProxy(callOpts, function(response){
+	    		$scope.goal._id = response.data._id;
+	    		console.log(response.data)
+				$scope.createGoal()
+				console.log("Sucessfully added the habit");
+				$scope.goal = {};
+				$scope.modal.hide();
+			}, function(error){
+				console.log(JSON.stringify(error));
+				console.log("error");
+			});
+	};
+
+	console.log("Outside createGoal phone");
+
+
+	$scope.deleteGoal = function(taskId,  goal) {
+		$scope.removeGoal(goal);
+	   	var callOpts = {'method': 'DELETE', 'method_url': "/api/v3/tasks/" + taskId,
+	                    'method_args': null};
+
+	    CommHelper.habiticaProxy(callOpts, function(response){
+				console.log("Sucessfully deleted the habit");
+			}, function(error){
+				console.log(JSON.stringify(error));
+				console.log("error");
+			});
+	};
+
+	$scope.scoreUp = function(taskId) {
+	   	var callOpts = {'method': 'POST', 'method_url': "/api/v3/tasks/" + taskId+ "/score/up",
+	                    'method_args': null};
+
+	    CommHelper.habiticaProxy(callOpts, function(response){
+				userInfo();
+				console.log("Score up");
+			}, function(error){
+				console.log(JSON.stringify(error));
+				console.log("error");
+			});
+	};
+
+	$scope.scoreDown = function(taskId) {
+	   	var callOpts = {'method': 'POST', 'method_url': "/api/v3/tasks/" + taskId+ "/score/down",
+	                    'method_args': null};
+
+	    CommHelper.habiticaProxy(callOpts, function(response){
+				userInfo();
+				console.log("Score down");
+			}, function(error){
+				console.log(JSON.stringify(error));
+				console.log("error");
+			});
+	};
+
+	$scope.joinParty = function() {
+	   	var callOpts = {'method': 'POST', 'method_url': "/api/v3/groups/dfb01232-1d59-4f12-9c74-94b595dc1984/join",
+	                    'method_args': null};
+
+	    CommHelper.habiticaProxy(callOpts, function(response){
+				console.log("Sucessfully joing the party");
+				//console.log(response);
+			}, function(error){
+				console.log(error);
+				console.log("error");
+			});
+	};
+
 
 	/*$http.get('http://54.159.38.241:3000/export/avatar-'+userId+'.html')
     .then(function(response) {
