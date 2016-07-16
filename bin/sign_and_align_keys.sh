fb6362a90b24ba917b0bc9cd80f1ca6af9129d6f
K. Shankari <shankari@eecs.berkeley.edu>
2016-07-16 10:11:01 -0700

    Revert to the full version of crosswalk for all android devices
    The attempt with reducing apk size was a miserable failure.
    
    - crosswalk-lite tries to download the library on the fly, but takes so long
      that the app does not load on first init
    - system webview on chrome is slightly different than the crosswalk and the iOS
      version, and does not display the heatmap control screen correctly, for
      example.
    
    So we revert back to the full crosswalk version until we have time to
    investigate this again.

--- bin/sign_and_align_keys.sh
+++ bin/sign_and_align_keys.sh
@@ -14,13 +14,3 @@ jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/Safety_Infras
 cp platforms/android/build/outputs/apk/android-x86-release-unsigned.apk platforms/android/build/outputs/apk/android-x86-release-signed-unaligned.apk
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/Safety_Infrastructure/MovesConnect/production.keystore ./platforms/android/build/outputs/apk/android-x86-release-signed-unaligned.apk androidproductionkey
 ~/Library/Android/sdk/build-tools/22.0.1/zipalign -v 4 platforms/android/build/outputs/apk/android-x86-release-signed-unaligned.apk emission-x86-build-$1.apk
-
-# Remove the plugin, sign and release general
-ionic plugin remove cordova-plugin-crosswalk-webview
-ionic build android --release -- --minSdkVersion=21
-cp platforms/android/build/outputs/apk/android-release-unsigned.apk platforms/android/build/outputs/apk/android-L+-release-signed-unaligned.apk
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/Safety_Infrastructure/MovesConnect/production.keystore ./platforms/android/build/outputs/apk/android-L+-release-signed-unaligned.apk androidproductionkey
-~/Library/Android/sdk/build-tools/22.0.1/zipalign -v 4 platforms/android/build/outputs/apk/android-L+-release-signed-unaligned.apk emission-L+-build-$1.apk
-
-# Re-add the plugin
-ionic plugin add cordova-plugin-crosswalk-webview  --variable XWALK_MODE="lite"
