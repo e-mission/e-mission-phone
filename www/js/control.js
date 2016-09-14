eeb8b81ba0d77bb2c268302d0a9a9bbf9331ab69
SUNILJTHAPA <sunil07t@gmail.com>
2016-09-13 18:15:09 -0700

    share sheet

--- www/js/control.js
+++ www/js/control.js
@@ -419,4 +419,20 @@ angular.module('emission.main.control',['emission.services',
             $ionicPopup.alert({template: error});
         });
     }
+    var options = {
+      message: 'Join me in making transportation greener and healthier \n Download the emission app:', // not supported on some apps (Facebook, Instagram)
+      //subject: 'the subject', // fi. for email
+      //files: ['', ''], // an array of filenames either locally or remotely
+      url: 'https://itunes.apple.com/us/app/emission/id1084198445',
+      //chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title
+    }
+
+    $scope.share = function() {
+        window.plugins.socialsharing.shareWithOptions(options, function(result) {
+            console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+            console.log("Shared to app: " + result.app); // On Android result.app is currently empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+        }, function(msg) {
+            console.log("Sharing failed with message: " + msg);
+        });
+    }
 });
