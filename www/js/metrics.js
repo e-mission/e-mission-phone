8b39d108331a157655c535daf791e0692346c693
Jack Sullivan <jsullivan27@berkeley.edu>
2018-01-28 23:18:19 -0800

    ui changes

--- www/js/metrics.js
+++ www/js/metrics.js
@@ -1140,6 +1140,10 @@ angular.module('emission.main.metrics',['nvd3', 'emission.services', 'ionic-date
     }
   }
 
+  $scope.linkToDiary = function(trip_id) {
+    window.location.href = "#/root/main/diary/" + trip_id;
+  }
+
   $scope.modeIcon = function(key) {
     var icons = {"BICYCLING":"ion-android-bicycle",
     "ON_FOOT":" ion-android-walk",
