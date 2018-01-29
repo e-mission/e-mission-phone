4fec54afa993a5e00be2196fe94bd300599d942c
AndyY <yan.andy4@gmail.com>
2018-01-28 22:27:57 -0800

    Added fn to commHelper

--- www/js/bear.js
+++ www/js/bear.js
@@ -6,7 +6,14 @@ angular.module('emission.main.bear',['nvd3', 'emission.services', 'ionic-datepic
                                     CommHelper, $window, $ionicPopup,
                                     FootprintHelper, CalorieCal, $ionicModal, $timeout, storage,
                                     $ionicScrollDelegate, $rootScope, $location,  $state, ReferHelper, $http, Logger) {
-
-
-
+  $scope.onLoad = function(){
+      CommHelper.getHappiness().then(function(response) {
+          console.log("Success!");
+          console.log(response);
+          refreshInfo();
+      }, function(error) {
+          console.log(error);
+          console.log("Failed");
+      });
+  };
 });
