34471a66661ecb4fbc3c17ec01447456877f5033
tan95 <andrew--tan@outlook.com>
2018-02-04 02:35:32 -0800

    open external browser instead of using embedded iframe

--- www/js/accessmap/accessmap.js
+++ www/js/accessmap/accessmap.js
@@ -9,7 +9,7 @@ angular.module('emission.main.accessmap', [
 ]).
   controller('AccessMapCtrl',
     function ($scope, $state, $ionicPopup, nzTour, $ionicPopover, storage,
-              CommHelper, $sce) {
+              CommHelper, $sce, $cordovaInAppBrowser) {
       // Tour steps
       var tour = {
         config: {
@@ -20,20 +20,12 @@ angular.module('emission.main.accessmap', [
         },
         steps: [
           {
-            target: '#accessmap-webframe',
-            content: 'Use AccessMap to plan your route and go on to trips',
+            target: '#open-accessmap-button',
+            content: 'Click the button to start AccessMap Emission edition and plan your route',
           },
           {
-            target: '#accessmap-webframe',
-            content: 'When a trip ends, wait for prompt, then find your trip in diary',
-          },
-          {
-            target: '#accessmap-webframe',
-            content: 'Click ">" button of a particular trip to expand the trip detail, and click take survey',
-          },
-          {
-            target: '#accessmap-webframe',
-            content: 'Please login before taking the survey, so we can associate this trip with you',
+            target: '#open-accessmap-button',
+            content: 'This will bring you outside the Emission app, but Emission will stays at background to continue tracking',
           },
           {
             target: '#accessmap-help',
@@ -63,6 +55,31 @@ angular.module('emission.main.accessmap', [
         }
       };
 
+      var sliceUUID = function (uuidWithNoDash) {
+        var uuidParts = [];
+        uuidParts.push(uuidWithNoDash.slice(0,8));
+        uuidParts.push(uuidWithNoDash.slice(8,12));
+        uuidParts.push(uuidWithNoDash.slice(12,16));
+        uuidParts.push(uuidWithNoDash.slice(16,20));
+        uuidParts.push(uuidWithNoDash.slice(20,32));
+        return uuidParts.join('-');
+      };
+
+      $scope.openBrowser = function () {
+        // Load AccessMap with uuid
+        CommHelper.getUser().then(function(userProfile) {
+          var uuid = sliceUUID(userProfile.user_id['$uuid']);
+          var url = $sce.trustAsResourceUrl('https://emission-accessmap.open-to-all.com/?emissionId=' + uuid);
+          console.log("ACCESSMAP URL: " + url);
+
+          $cordovaInAppBrowser.open(url, '_system', {
+            location: 'no',
+            clearcache: 'no',
+            toolbar: 'yes'
+          });
+        });
+      };
+
       $scope.$on('$ionicView.enter', function(ev) {
         // Workaround from
         // https://github.com/driftyco/ionic/issues/3433#issuecomment-195775629
@@ -71,19 +88,5 @@ angular.module('emission.main.accessmap', [
         }
 
         checkTutorialDone();
-
-        // Load AccessMap with uuid
-        CommHelper.getUser().then(function(userProfile) {
-          var uuidRaw = userProfile.user_id['$uuid'];
-          var parts = [];
-          parts.push(uuidRaw.slice(0,8));
-          parts.push(uuidRaw.slice(8,12));
-          parts.push(uuidRaw.slice(12,16));
-          parts.push(uuidRaw.slice(16,20));
-          parts.push(uuidRaw.slice(20,32));
-          var uuid = parts.join('-');
-          $scope.currentProjectUrl = $sce.trustAsResourceUrl('https://emission-accessmap.open-to-all.com/?emissionId=' + uuid);
-          console.log("ACCESSMAP URL: " + $scope.currentProjectUrl);
-        });
       });
     });
