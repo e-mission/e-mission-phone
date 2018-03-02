19b30e2fe93d081129681040467a6779596bebae
jesbu1 <jessezhang5@gmail.com>
2018-03-02 11:47:09 -0800

    fixed clientstats error

--- www/js/metrics.js
+++ www/js/metrics.js
@@ -4,7 +4,7 @@ angular.module('emission.main.metrics',['nvd3', 'emission.services', 'ionic-date
 
 .controller('MetricsCtrl', function($scope, $stateParams, $ionicActionSheet, $ionicLoading,
                                     CommHelper, $window, $ionicPopup,
-                                    ionicDatePicker,
+                                    ionicDatePicker, ClientStats,
                                     FootprintHelper, CalorieCal, $ionicModal, $timeout, storage,
                                     $ionicScrollDelegate, $rootScope, $location,  $state, ReferHelper, $http, Logger, Timeline) {
     var lastTwoWeeksQuery = true;
