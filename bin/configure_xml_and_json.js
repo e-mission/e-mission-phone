8bb0123e5d04c4321764aac38295ce51c72cc9ad
K. Shankari <shankari@eecs.berkeley.edu>
2018-05-22 18:27:09 -0700

    Configuration script now has argument of the enviroment to configure
    `serve` for the serve case
    `cordovabuild` for the full cordova build case
    
    See 26d90bc0f095194f3f169d8099f4b74c0ad2f744 for the change in which the two
    versions were created

--- bin/configure_xml_and_json.js
+++ bin/configure_xml_and_json.js
@@ -2,7 +2,21 @@
 
 const fs = require('fs');
 
-fs.copyFileSync("config.serve.xml", "config.xml");
-fs.copyFileSync("package.serve.json", "package.json");
+var copyInlinedFiles = function(inlineString) {
+    var selConfigXml = "config."+inlineString+".xml";
+    var selPkgJson = "package."+inlineString+".json";
+    fs.copyFileSync(selConfigXml, "config.xml");
+    fs.copyFileSync(selPkgJson, "package.json");
 
-console.log("Changed config.xml and package.json to ignore all plugins and engines");
+    console.log("Copied "+selConfigXml+" -> config.xml and "+
+                selPkgJson + " -> package.json");
+}
+
+if (process.argv.length != 3) {
+    console.log("process.argv = "+process.argv+
+                " with length "+process.argv.length+
+                " expected 3");
+    process.exit(1);
+} else {
+    copyInlinedFiles(process.argv[2]);
+}
