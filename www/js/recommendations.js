82c46c4d26e37e2f3fb1e375c713e3ff94473d28
Samantha Banchik <sam11b@berkeley.edu>
2018-10-28 22:27:13 -0700

    adding JSON pull, taking out HTML things

--- www/js/recommendations.js
+++ www/js/recommendations.js
@@ -6,15 +6,19 @@ angular.module('emission.main.recommendations',['emission.services', 'emission.p
                 'emission.splash.referral', 'angularLocalStorage',
                 'ng-walkthrough', 'nzTour'])
 
+/*
+Ask Shankari about this Wednesday
+.service('CommHelper', function($http) {
+    var getConnectURL = function(successCallback, errorCallback) {
+        window.cordova.plugins.BEMConnectionSettings.getSettings(
+            function(settings) {
+                successCallback(settings.connectUrl);
+            }, errorCallback);
+    };
+  */
+
 .controller('RecommendationsCtrl', function(CommHelper, $state, $ionicLoading, $scope, $rootScope, $ionicModal, nzTour,
                                 $window, $http, $ionicPopup, $timeout, storage, ReferralHandler, ReferHelper, Logger, $cordovaInAppBrowser, SurveyLaunch) {
-    $scope.xCoord = 37.8756;
-    $scope.yCoord = -122.2588;
-    $scope.currentDisplay = "";
-    $http.get('json/sampleRecommendation.json').then(function(result) {
-        $scope.recs = result.data;
-      }
-    )
     $scope.clickDirections = function(id) {
       $scope.currentDisplay = document.getElementById("mapModal" + id);
       $scope.currentDisplay.style.display = "block";
@@ -27,13 +31,15 @@ angular.module('emission.main.recommendations',['emission.services', 'emission.p
       $scope.currentDisplay = document.getElementById("reviewModal" + id);
       $scope.currentDisplay.style.display = "block";
     };
-    $scope.getDistance = function(x, y) {
-      var xDif = ($scope.xCoord - x) * 55;
-      var yDif = ($scope.yCoord - y) * 69;
-      var x2 = Math.pow(xDif, 2);
-      var y2 = Math.pow(yDif, 2);
-      var dist = Math.sqrt(x2 + y2);
-      return Math.round(dist * 10) / 10 + " miles away";
-    }
+
+    $scope.getSuggestion = function() {
+      return new Promise(function(resolve, reject) {
+        var msgFiller = function(message) {
+          console.log("THIS IS THE SUGGESTION" + JSON.stringify(message));
+          document.getElementById("0").innerHTML = JSON.stringify(message);
+        }
+        window.cordova.plugins.BEMServerComm.pushGetJSON("/suggestion_sys", msgFiller, resolve, reject);
+      });
+    };
 
 });
