4bead0ef3d0c1e9c1674627c669b6f082bdb3d60
Samantha Banchik <sam11b@berkeley.edu>
2018-10-31 19:31:34 -0700

    adding newly changed end-to-end integration

--- www/js/recommendations.js
+++ www/js/recommendations.js
@@ -8,13 +8,29 @@ angular.module('emission.main.recommendations',['emission.services', 'emission.p
 
 .controller('RecommendationsCtrl', function(CommHelper, $state, $ionicLoading, $scope, $rootScope, $ionicModal, nzTour,
                                 $window, $http, $ionicPopup, $timeout, storage, ReferralHandler, ReferHelper, Logger, $cordovaInAppBrowser, SurveyLaunch) {
-    $scope.xCoord = 37.8756;
-    $scope.yCoord = -122.2588;
-    $scope.currentDisplay = "";
+
+    //$scope.currentDisplay = "";
+    //Check and see if this is right with Vanessa
+    //for now using sample
+    /*
     $http.get('json/sampleRecommendation.json').then(function(result) {
-        $scope.recs = result.data;
+        var name = JSON.stringify(result.data.name);
+        var mode = JSON.stringify(result.data.mode);
+        document.getElementById("message_1").innerHTML = name.replace(/\"/g, "");
+        document.getElementById("mode_1").innerHTML = mode.replace(/\"/g, "");
+        // Eventually this will be a for loop with all the messages 
       }
     )
+    */
+
+     CommHelper.getSuggestion().then(function(result) {
+        console.log(result);
+        var name = JSON.stringify(result.message);
+        var mode = JSON.stringify(result.method);
+        document.getElementById("message_1").innerHTML = name.replace(/\"/g, "");
+        document.getElementById("mode_1").innerHTML = mode.replace(/\"/g, "");
+     });
+
     $scope.clickDirections = function(id) {
       $scope.currentDisplay = document.getElementById("mapModal" + id);
       $scope.currentDisplay.style.display = "block";
@@ -27,13 +43,15 @@ angular.module('emission.main.recommendations',['emission.services', 'emission.p
       $scope.currentDisplay = document.getElementById("reviewModal" + id);
       $scope.currentDisplay.style.display = "block";
     };
-    $scope.getDistance = function(x, y) {
-      var xDif = ($scope.xCoord - x) * 55;
-      var yDif = ($scope.yCoord - y) * 69;
-      var x2 = Math.pow(xDif, 2);
-      var y2 = Math.pow(yDif, 2);
-      var dist = Math.sqrt(x2 + y2);
-      return Math.round(dist * 10) / 10 + " miles away";
+
+    $scope.clickSuggestion = function(id) {
+      CommHelper.getSuggestion().then(function(result) {
+        console.log(result);
+        var name = JSON.stringify(result.message);
+        var mode = JSON.stringify(result.method);
+        document.getElementById("message_1").innerHTML = name.replace(/\"/g, "");
+        document.getElementById("mode_1").innerHTML = mode.replace(/\"/g, "");
+     });
     }
 
 });
