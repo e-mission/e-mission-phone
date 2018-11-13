8eb55a73616ebf189272d807e88500ba5b5dea3d
Samantha Banchik <sam11b@berkeley.edu>
2018-11-13 15:40:56 -0800

    changed to match BigData, only one recommendbox

--- www/js/recommendations.js
+++ www/js/recommendations.js
@@ -8,18 +8,15 @@ angular.module('emission.main.recommendations',['emission.services', 'emission.p
 
 .controller('RecommendationsCtrl', function(CommHelper, $state, $ionicLoading, $scope, $rootScope, $ionicModal, nzTour,
                                 $window, $http, $ionicPopup, $timeout, storage, ReferralHandler, ReferHelper, Logger, $cordovaInAppBrowser, SurveyLaunch) {
-    $scope.xCoord = 37.8756;
-    $scope.yCoord = -122.2588;
-    $scope.currentDisplay = "";
-    $http.get('json/sampleRecommendation.json').then(function(result) {
-        $scope.recs = result.data;
-      }
-    )
-    $scope.clickDirections = function(id) {
-      $scope.currentDisplay = document.getElementById("mapModal" + id);
-      $scope.currentDisplay.style.display = "block";
 
-    };
+     CommHelper.getSuggestion().then(function(result) {
+        console.log(result);
+        var name = JSON.stringify(result.message);
+        var mode = JSON.stringify(result.method);
+        document.getElementById("message_1").innerHTML = name.replace(/\"/g, "");
+        document.getElementById("mode_1").innerHTML = mode.replace(/\"/g, "");
+     });
+    
     $scope.clickX = function() {
       $scope.currentDisplay.style.display = "none";
     };
@@ -27,13 +24,15 @@ angular.module('emission.main.recommendations',['emission.services', 'emission.p
       $scope.currentDisplay = document.getElementById("reviewModal" + id);
       $scope.currentDisplay.style.display = "block";
     };
-    $scope.getDistance = function(x, y) {
-      var xDif = ($scope.xCoord - x) * 55;
-      var yDif = ($scope.yCoord - y) * 69;
-      var x2 = Math.pow(xDif, 2);
-      var y2 = Math.pow(yDif, 2);
-      var dist = Math.sqrt(x2 + y2);
-      return Math.round(dist * 10) / 10 + " miles away";
-    }
 
+    $scope.clickSuggestion = function(id) {
+      CommHelper.getSuggestion().then(function(result) {
+        var name = JSON.stringify(result.message);
+        var mode = JSON.stringify(result.method);
+        document.getElementById("message_1").innerHTML = name.replace(/\"/g, "");
+        document.getElementById("mode_1").innerHTML = mode.replace(/\"/g, "");
+     }).catch(function(err) {
+      console.log("Error while getting suggestion" + err);
+    });
+  };
 });
