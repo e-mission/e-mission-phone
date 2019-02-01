736e7cc17b426d815b5a0e91c108c9ee7ab900a8
trevor-wu <35517530+trevor-wu@users.noreply.github.com>
2019-02-01 13:19:38 -0800

    Addressed issues in Pull Request #520 (#524)
    * api calling function
    
    * git being picky
    
    * addressed pr changes, reduced api calls to 1, integrated with server changes

--- www/js/recommendations.js
+++ www/js/recommendations.js
@@ -12,14 +12,32 @@ angular.module('emission.main.recommendations',['emission.services', 'emission.p
 
     $scope.name = "Cannot Retrieve Suggestion";
     $scope.mode = "Cannot Retrieve Mode";
+    $scope.bid = "yalis-stanley-hall-cafe-berkeley";
+    $scope.stars = 4.5;
+    $http.get('json/yelpfusion.json').then(function(result) {
+          $scope.yelp = result.data;
+        }
+    )
     $scope.clickX = function() {
       $scope.currentDisplay.style.display = "none";
     };
     $scope.clickReview = function(id) {
       $scope.currentDisplay = document.getElementById("reviewModal" + id);
       $scope.currentDisplay.style.display = "block";
+      $ionicLoading.show({
+        template: 'Loading Reviews...'
+        });
+      $http({
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.yelp.com/v3/businesses/"+$scope.bid+"/reviews",
+        "method": "GET",
+        "headers": $scope.yelp.headers
+      }).then(function(res) {
+        $scope.revs = res.data.reviews;
+      });
+      $ionicLoading.hide();
     };
-
 
     var uuid = $stateParams.uuid;
     $scope.clickSuggestion = function(id) {
@@ -30,10 +48,12 @@ angular.module('emission.main.recommendations',['emission.services', 'emission.p
         console.log(result);
         $scope.name = result.message;
         $scope.mode = result.method;
-        $ionicLoading.hide();
+        $scope.bid = result.businessid;
+        $scope.stars = result.rating;
      }).catch(function(err) {
       console.log("Error while getting suggestion" + err);
     });
+
   };
 
   $scope.pickTrip = function(id) {
