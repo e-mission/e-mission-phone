d60f28ffaed67c602e902da990f7b16f0a4e4c2b
Deepali <you@example.com>
2019-03-26 10:33:22 +0530

    removing serial number div

--- www/js/cci-about/about.js
+++ www/js/cci-about/about.js
@@ -2,28 +2,32 @@
 
 angular.module('emission.main.cci-about', ['emission.plugin.logger'])
 
+.controller('CCIAboutCtrl', function($scope, $state, $cordovaEmailComposer, $ionicPopup, SurveyLaunch) {
 
-  
-  
-  .controller('CommHelper', function($http) {
-    var getConnectURL = function(successCallback, errorCallback) {
-        window.cordova.plugins.BEMConnectionSettings.getSettings(
-            function(settings) {
-                successCallback(settings.connectUrl);
-            }, errorCallback);
-    };
- 
-  $scope.signupSerial = function (user.username) {
-         return new Promise(function(resolve, reject) {
-            window.cordova.plugins.BEMServerComm.postUserPersonalData("/profile/update", "update_doc", updateDoc, resolve, reject);
+  $scope.studyEmail = "cci.vmt@gmail.com";
+
+  $scope.startSurvey = function () {
+      SurveyLaunch.startSurvey('https://berkeley.qualtrics.com/jfe/form/SV_eQBjPXx10yaAScl', 'QR~QID3');
+      startSurvey();
+  };
+	
+  $scope.emailCCI = function() {
+        var email = {
+            to: [$scope.studyEmail],
+            subject: 'Question from Emission User',
+            body: ''
+        }
+
+        $cordovaEmailComposer.open(email).then(function() {
+           window.Logger.log(window.Logger.LEVEL_DEBUG,
+               "CCI email queued successfully");
+        },
+        function () {
+           window.Logger.log(window.Logger.LEVEL_INFO,
+               "CCI email cancel reported, seems to be an error on android");
         });
-    }; 
-	
-	
-	
-
-
-});
+  };
+})
 
 
   
