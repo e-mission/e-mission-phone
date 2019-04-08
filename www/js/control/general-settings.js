e-mission/e-mission-phone

commit a99c4e8798b7cfa71db5b366c73bbf4f5f2f0237

Move other locally stored data to native storage
In particular:
- metrics stores the user display preference for calories
- metrics stores the user BMI data to correct for calorie burn
- updatecheck stores the channel

Also change the control panel to read the data using promises instead of
directly from local storage.

This also involved minor changes (function -> field) that also required changes
to the HTML.

Also, add new clear methods to the storage function and call them from the
control panel.

Testing done:
- Navigated between dashboard and profile multiple times with no errors
- Set the user information in the dashboard - verified that it was visible in
  the profile

www/js/control/general-settings.js
@@ -10,12 +10,13 @@ angular.module('emission.main.control',['emission.services',
                                         'emission.splash.updatecheck',
                                         'emission.main.metrics.factory',
                                         'emission.stats.clientstats',
-                                        'angularLocalStorage',
+                                        'emission.plugin.kvstore',
                                         'emission.plugin.logger'])
 
 .controller('ControlCtrl', function($scope, $window, $ionicScrollDelegate,
+               $ionicPlatform,
                $state, $ionicPopup, $ionicActionSheet, $ionicPopover,
-               $rootScope, storage, ionicDatePicker,
+               $rootScope, KVStore, ionicDatePicker,
                StartPrefs, ControlHelper,
                ControlCollectionHelper, ControlSyncHelper,
                ControlTransitionNotifyHelper,
@@ -50,27 +51,35 @@ angular.module('emission.main.control',['emission.services',
     $scope.emailLog = ControlHelper.emailLog;
     $scope.userData = []
     $scope.getUserData = function() {
-        var userDataFromStorage = CalorieCal.get();
-        $scope.userData = []
-        var height = userDataFromStorage.height.toString();
-        var weight = userDataFromStorage.weight.toString();
-        var temp  =  {
-            age: userDataFromStorage.age,
-            height: height + (userDataFromStorage.heightUnit == 1? ' cm' : ' ft'),
-            weight: weight + (userDataFromStorage.weightUnit == 1? ' kg' : ' lb'),
-            gender: userDataFromStorage.gender == 1? 'Male' : 'Female'
+        return CalorieCal.get().then(function(userDataFromStorage) {
+        $scope.rawUserData = userDataFromStorage;
+        if ($scope.userDataSaved()) {
+            $scope.userData = []
+            var height = userDataFromStorage.height.toString();
+            var weight = userDataFromStorage.weight.toString();
+            var temp  =  {
+                age: userDataFromStorage.age,
+                height: height + (userDataFromStorage.heightUnit == 1? ' cm' : ' ft'),
+                weight: weight + (userDataFromStorage.weightUnit == 1? ' kg' : ' lb'),
+                gender: userDataFromStorage.gender == 1? 'Male' : 'Female'
+            }
+            for (var i in temp) {
+                $scope.userData.push({key: i, value: temp[i]});
+            }
         }
-        for (var i in temp) {
-            $scope.userData.push({key: i, value: temp[i]});
-        }
+        });
     }
 
     $scope.userDataSaved = function() {
-        return CalorieCal.get().userDataSaved == true;
+        if (angular.isDefined($scope.rawUserData) && $scope.rawUserData != null) {
+            return $scope.rawUserData.userDataSaved;
+        } else {
+            return false;
+        }
     }
-    if ($scope.userDataSaved()) {
-        $scope.getUserData();
-    }
+    $ionicPlatform.ready().then(function() {
+        $scope.refreshScreen();
+    });
     $scope.getLowAccuracy = function() {
         //  return true: toggle on; return false: toggle off.
         var isMediumAccuracy = ControlCollectionHelper.isMediumAccuracy();
@@ -145,12 +154,12 @@ angular.module('emission.main.control',['emission.services',
         $state.go("root.main.map");
     }
     $scope.getState = function() {
-        ControlCollectionHelper.getState().then(function(response) {
+        return ControlCollectionHelper.getState().then(function(response) {
             $scope.$apply(function() {
                 $scope.settings.collect.state = response;
             });
         }, function(error) {
-            $ionicPopup.alert("while getting email, "+error);
+            $ionicPopup.alert("while reading FSM state, "+error);
         });
     };
 
@@ -172,18 +181,13 @@ angular.module('emission.main.control',['emission.services',
         });
     }
 
-    var clearBoth = function() {
-        storage.clearAll();
-        clearUsercache();
-    }
-
     $scope.nukeUserCache = function() {
         var nukeChoiceActions = [{text: "UI state only",
-                                  action: storage.clearAll},
+                                  action: KVStore.clearOnlyLocal},
                                  {text: 'Native cache only',
-                                  action: clearUsercache},
+                                  action: KVStore.clearOnlyNative},
                                  {text: 'Everything',
-                                  action: clearBoth}];
+                                  action: KVStore.clearAll}];
 
         $ionicActionSheet.show({
             titleText: "Clear data",
@@ -209,7 +213,9 @@ angular.module('emission.main.control',['emission.services',
     }
 
     $scope.$on('$ionicView.afterEnter', function() {
+        $ionicPlatform.ready().then(function() {
         $scope.refreshScreen();
+        });
     })
 
     // Execute action on hidden popover
@@ -229,15 +235,24 @@ angular.module('emission.main.control',['emission.services',
         $scope.settings.auth = {};
         $scope.settings.connect = {};
         $scope.settings.channel = function(newName) {
-          return arguments.length ? (UpdateCheck.setChannel(newName)) : UpdateCheck.getChannel();
+          return arguments.length ? (UpdateCheck.setChannel(newName)) : $scope.settings.storedChannel;
         };
-
+        UpdateCheck.getChannel().then(function(retVal) { 
+            $scope.$apply(function() {
+                $scope.settings.storedChannel = retVal;
+            });
+        });
         $scope.getConnectURL();
         $scope.getCollectionSettings();
         $scope.getSyncSettings();
         $scope.getTNotifySettings();
         $scope.getEmail();
-        $scope.getState();
+        $scope.getState().then($scope.isTrackingOn).then(function(isTracking) {
+            $scope.$apply(function() {
+                $scope.settings.collect.trackingOn = isTracking;
+            });
+        });
+        $scope.getUserData();
     };
 
     $scope.returnToIntro = function() {
@@ -316,18 +331,19 @@ angular.module('emission.main.control',['emission.services',
         return ionic.Platform.isIOS();
     }
 
-    $scope.refreshScreen();
     $ionicPopover.fromTemplateUrl('templates/control/main-sync-settings.html', {
         scope: $scope
     }).then(function(popover) {
         $scope.syncSettingsPopup = popover;
     });
-    $scope.trackingOn = function() {
+    $scope.isTrackingOn = function() {
+        return $ionicPlatform.ready().then(function() {
         if($scope.isAndroid()){
             return $scope.settings.collect.state != "local.state.tracking_stopped";
         } else if ($scope.isIOS()) {
             return $scope.settings.collect.state != "STATE_TRACKING_STOPPED";
         }
+        });
     };
     $scope.userStartStopTracking = function() {
         if ($scope.startStopBtnToggle){
@@ -338,7 +354,7 @@ angular.module('emission.main.control',['emission.services',
             $scope.startStopBtnToggle = true;
         }
     }
-    $scope.startStopBtnToggle = $scope.trackingOn();
+    $scope.startStopBtnToggle = $scope.trackingOn;
     $scope.getExpandButtonClass = function() {
         return ($scope.expanded)? "icon ion-ios-arrow-up" : "icon ion-ios-arrow-down";
     }
@@ -346,9 +362,9 @@ angular.module('emission.main.control',['emission.services',
         return ($scope.dataExpanded)? "icon ion-ios-arrow-up" : "icon ion-ios-arrow-down";
     }
     $scope.eraseUserData = function() {
-        CalorieCal.delete();
+        CalorieCal.delete().then(function() {
         $ionicPopup.alert({template: 'User data erased.'});
-
+        });
     }
     $scope.parseState = function(state) {
         if (state) {

www/js/metrics-factory.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('emission.main.metrics.factory', ['angularLocalStorage'])
+angular.module('emission.main.metrics.factory', ['emission.plugin.kvstore'])
 
 .factory('FootprintHelper', function() {
   var fh = {};
@@ -33,34 +33,19 @@ angular.module('emission.main.metrics.factory', ['angularLocalStorage'])
   return fh;
 })
 
-.factory('CalorieCal', function(storage){
+.factory('CalorieCal', function(KVStore){
 
   var cc = {}; 
+  var USER_DATA_KEY = "user-data";
+
   cc.set = function(info) {
-    for(var key in info){
-      storage.set(key, info[key])
-    }
+    return KVStore.set(USER_DATA_KEY, info);
   };
   cc.get = function() {
-    var userData = {
-        'gender': storage.get('gender'),
-        'heightUnit': storage.get('heightUnit'),
-        'height': storage.get('height'),
-        'weightUnit': storage.get('weightUnit'),
-        'weight': storage.get('weight'),
-        'age': storage.get('age'),
-        'userDataSaved': storage.get('userDataSaved')
-      }
-      return userData;
+    return KVStore.get(USER_DATA_KEY);
   };
   cc.delete = function() {
-    storage.remove('gender');
-        storage.remove('height');
-        storage.remove('heightUnit');
-        storage.remove('weight');
-        storage.remove('weightUnit');
-        storage.remove('age');
-        storage.remove('userDataSaved');
+    return KVStore.remove(USER_DATA_KEY);
   };
   Number.prototype.between = function (min, max) {
     return this >= min && this <= max;

www/js/metrics.js
@@ -1,11 +1,11 @@
 'use strict';
 
-angular.module('emission.main.metrics',['nvd3', 'emission.services', 'ionic-datepicker', 'emission.main.metrics.factory', 'angularLocalStorage', 'emission.plugin.logger'])
+angular.module('emission.main.metrics',['nvd3', 'emission.services', 'ionic-datepicker', 'emission.main.metrics.factory', 'emission.plugin.kvstore', 'emission.plugin.logger'])
 
 .controller('MetricsCtrl', function($scope, $ionicActionSheet, $ionicLoading,
                                     CommHelper, $window, $ionicPopup,
                                     ionicDatePicker, $ionicPlatform,
-                                    FootprintHelper, CalorieCal, $ionicModal, $timeout, storage,
+                                    FootprintHelper, CalorieCal, $ionicModal, $timeout, KVStore,
                                     $rootScope, $location,  $state, ReferHelper, $http, Logger) {
     var lastTwoWeeksQuery = true;
     var first = true;
@@ -52,28 +52,32 @@ angular.module('emission.main.metrics',['nvd3', 'emission.services', 'ionic-date
         });
     }
 
+    // TODO: Move this out into its own service
+    var FOOD_COMPARE_KEY = 'foodCompare';
     $scope.setCookie = function(){
       $scope.foodCompare = 'cookie';
-      storage.set('foodCompare', 'cookie');
+      return KVStore.set(FOOD_COMPARE_KEY, 'cookie');
     }
     $scope.setIceCream = function(){
       $scope.foodCompare = 'iceCream';
-      storage.set('foodCompare', 'iceCream');
+      return KVStore.set(FOOD_COMPARE_KEY, 'iceCream');
     }
     $scope.setBanana = function(){
       $scope.foodCompare = 'banana';
-      storage.set('foodCompare', 'banana');
+      return KVStore.set(FOOD_COMPARE_KEY, 'banana');
     }
-    if(storage.get('foodCompare') == null){
+    $scope.handleChosenFood = function(retVal) {
+    if (retVal == null){
       $scope.setCookie();
     } else {
-      var choosenFood = storage.get('foodCompare')
+      var choosenFood = retVal;
       if(choosenFood == 'cookie')
         $scope.setCookie();
       else if (choosenFood == 'iceCream')
         $scope.setIceCream();
       else
         $scope.setBanana();
+    }
     }
     $ionicModal.fromTemplateUrl('templates/metrics/metrics-control.html', {
       scope: $scope,
@@ -226,14 +230,31 @@ angular.module('emission.main.metrics',['nvd3', 'emission.services', 'ionic-date
                   'weight': $scope.userData.weight,
                   'age': $scope.userData.age,
                   'userDataSaved': true};
-      CalorieCal.set(info);
+      CalorieCal.set(info).then(function() {
+        $scope.savedUserData = info;
+      });
+    }
+
+    $scope.loadUserData = function() {
+        if(angular.isDefined($scope.savedUserData)) {
+            // loaded or set
+            return Promise.resolve();
+        } else {
+            return CalorieCal.get().then(function(userDataFromStorage) {
+                $scope.savedUserData = userDataFromStorage;
+            });
+        }
     }
 
     $scope.userDataSaved = function() {
-      var saved_user_data = CalorieCal.get();
-      // console.log("saved vals = "+JSON.stringify(saved_user_data));
-      return saved_user_data.userDataSaved == true;
+      // console.log("saved vals = "+JSON.stringify($scope.savedUserData));
+      if (angular.isDefined($scope.savedUserData) && $scope.savedUserData != null) {
+          return $scope.savedUserData.userDataSaved == true;
+      } else {
+          return false;
+      };
     }
+
     $scope.options = {
         chart: {
             type: 'multiBarChart',
@@ -306,6 +327,10 @@ angular.module('emission.main.metrics',['nvd3', 'emission.services', 'ionic-date
 
     var getData = function(){
       $scope.getMetricsHelper();
+      $scope.loadUserData();
+      KVStore.get(FOOD_COMPARE_KEY).then(function(retVal) {
+        $scope.handleChosenFood(retVal);
+      });
     }
 
     $scope.getMetricsHelper = function() {
@@ -655,7 +680,9 @@ angular.module('emission.main.metrics',['nvd3', 'emission.services', 'ionic-date
 
    $scope.getCorrectedMetFromUserData = function(currDurationData, currSpeedData) {
        if ($scope.userDataSaved()) {
-         var userDataFromStorage = CalorieCal.get();
+         // this is safe because userDataSaved will never be set unless there
+         // is stored user data that we have loaded
+         var userDataFromStorage = $scope.storedUserData;
          var met = CalorieCal.getMet(currDurationData.key, currSpeedData.values);
          var gender = userDataFromStorage.gender;
          var heightUnit = userDataFromStorage.heightUnit;

www/js/plugin/storage.js
@@ -90,5 +90,24 @@ angular.module('emission.plugin.kvstore', ['emission.plugin.logger',
         return getNativePlugin().removeLocalStorage(key);
     }
 
+    kvstoreJs.clearAll = function() {
+        storage.clearAll(key);
+        return getNativePlugin().clearAll();
+    }
+
+    /* 
+     * TODO: remove these two functions after we have confirmed that native
+     * storage is never deleted weirdly, and returned to only native storage.
+     * In that case, there will be only one clear - of native storage - which
+     * will be covered using clearAll.
+     */
+    kvstoreJs.clearOnlyLocal = function() {
+        return storage.clearAll();
+    }
+
+    kvstoreJs.clearOnlyNative = function() {
+        return getNativePlugin().clearAll();
+    }
+
     return kvstoreJs;
 });

www/js/splash/updatecheck.js
@@ -2,9 +2,9 @@
 
 angular.module('emission.splash.updatecheck', ['emission.plugin.logger',
                                                'emission.services',
-                                               'angularLocalStorage'])
+                                               'emission.plugin.kvstore'])
 
-.factory('UpdateCheck', function($ionicPopup, $ionicPlatform, $rootScope, $window, CommHelper, Logger, storage) {
+.factory('UpdateCheck', function($ionicPopup, $ionicPlatform, $rootScope, $window, CommHelper, Logger, KVStore) {
   var uc = {};
   var CHANNEL_KEY = 'deploy_channel';
 
@@ -14,17 +14,17 @@ angular.module('emission.splash.updatecheck', ['emission.plugin.logger',
    * to load UI updates from.
    */
   uc.getChannel = function() {
-    return storage.get(CHANNEL_KEY);
+    return KVStore.get(CHANNEL_KEY);
   };
 
   uc.setChannel = function(channelName) {
-    storage.set(CHANNEL_KEY, channelName);
-    CommHelper.updateUser({
-        client: channelName
-    });
+    return Promise.all([KVStore.set(CHANNEL_KEY, channelName),
+        CommHelper.updateUser({
+            client: channelName
+    })]);
   };
 
-  uc.setChannelPromise = function(currChannel) {
+  uc.initChannelPromise = function(currChannel) {
     var deploy = $window.IonicCordova.deploy;
     if (currChannel == null) {
         Logger.log("currChannel == null, skipping deploy init");
@@ -94,30 +94,36 @@ angular.module('emission.splash.updatecheck', ['emission.plugin.logger',
 
   uc.handleClientChangeURL = function(urlComponents) {
     Logger.log("handleClientChangeURL = "+JSON.stringify(urlComponents));
-    if (urlComponents['clear_local_storage'] == "true") {
-        Logger.log("About to clear all local storage");
-        storage.clearAll();
-    }
+    var operationArray = []
     if (urlComponents['clear_usercache'] == "true") {
         Logger.log("About to clear usercache");
-        window.cordova.plugins.BEMUserCache.clearAll();
+        operationArray.push(KVStore.clearAll());
     }
-    uc.setChannel(urlComponents['new_client']);
-    uc.checkForUpdates();
+    operationArray.push(uc.setChannel(urlComponents['new_client']));
+    Promise.all(operationArray).then(function() {
+        Logger.log("successfully set the channel to "+urlComponents['new_client']);
+        uc.checkForUpdates();
+    }).catch(function(error) {
+      var display_msg = error.message + "\n" + error.stack;
+      if (!angular.isDefined(error.message)) {
+        display_msg = JSON.stringify(error);
+      }
+      $ionicPopup.alert({"title": "Unable to handle client change",
+        "template": display_msg});
+      Logger.log('Ionic Deploy: Unable to handle client change URL '+display_msg);
+    })
   };
 
   // Default to dev
   var getChannelToUse = function() {
-      var channel = uc.getChannel();
+      return uc.getChannel().then(function(channel) {
       if (channel == null || channel == "") {
         console.log("No saved channel found, skipping channel config")
         channel = null;
       };
       console.log("Returning channel "+channel)
       return channel;
-  }
-
-  var showProgressDialog = function(title) {
+      });
   }
 
   var applyUpdate = function() {
@@ -159,8 +165,8 @@ angular.module('emission.splash.updatecheck', ['emission.plugin.logger',
     // Check Ionic Deploy for new code
   uc.checkForUpdates = function() {
     console.log('Ionic Deploy: Checking for updates');
-    var currChannel = getChannelToUse();
-    uc.setChannelPromise(currChannel).then(function() {
+    getChannelToUse().then(function(currChannel) {
+    uc.initChannelPromise(currChannel).then(function() {
     Logger.log("deploy init complete ");
     uc.checkPromise().then(function(hasUpdate) {
       Logger.log('Ionic Deploy: Update available: ' + hasUpdate);
@@ -193,6 +199,7 @@ angular.module('emission.splash.updatecheck', ['emission.plugin.logger',
         // TODO: Figure out a better way to do this using promises
         // $ionicPopup.alert({title: "Up to date!"});
       }
+    })
     })
     }).catch(function(err) {
       Logger.log('Ionic Deploy: Unable to check for updates'+err);

www/templates/control/main-control.html
@@ -7,7 +7,7 @@
     <div class="control-list-item">
       <div class="control-list-text">Tracking</div>
       <label ng-click="userStartStopTracking()" class="toggle control-list-toggle toggle-color">
-        <input type="checkbox" ng-checked="trackingOn()">
+        <input type="checkbox" ng-checked="settings.collect.trackingOn">
         <div class="track">
           <div class="handle"></div>
         </div>

commit 8deda1dbc9a38b34596c986f45d105eb83957289

Fix tracking toggle
- Fix regression caused by
https://github.com/e-mission/e-mission-phone/commit/a99c4e8798b7cfa71db5b366c73bbf4f5f2f0237
and also simplify the resulting code

- Bump up the version numbers for the data collection plugin to get the fix for
https://github.com/e-mission/e-mission-data-collection/commit/9459e18376659c255defb5c02b3ad63b3d6dd91f

Testing done (in conjunction with https://github.com/e-mission/e-mission-data-collection/commit/9459e18376659c255defb5c02b3ad63b3d6dd91f):

    - Cleared logs
    - Toggled tracking
    - Ensured that refreshScreen was not called
    - Clicked on the button in the alert
    - Ensured that refreshScreen was called and the final state was consistent with
      the toggle

config.cordovabuild.xml
@@ -110,7 +110,7 @@
     <preference name="SplashShowOnlyFirstTime" value="false" />
     <plugin name="edu.berkeley.eecs.emission.cordova.auth" spec="https://github.com/e-mission/cordova-jwt-auth.git#v1.5.0" />
     <plugin name="edu.berkeley.eecs.emission.cordova.comm" spec="https://github.com/e-mission/cordova-server-communication.git#v1.2.0" />
-    <plugin name="edu.berkeley.eecs.emission.cordova.datacollection" spec="https://github.com/e-mission/e-mission-data-collection.git#v1.3.2">
+    <plugin name="edu.berkeley.eecs.emission.cordova.datacollection" spec="https://github.com/e-mission/e-mission-data-collection.git#v1.3.3">
         <variable name="LOCATION_VERSION" value="11.0.1" />
     </plugin>
     <plugin name="edu.berkeley.eecs.emission.cordova.serversync" spec="https://github.com/e-mission/cordova-server-sync.git#v1.1.2" />

www/js/control/general-settings.js
@@ -229,6 +229,7 @@ angular.module('emission.main.control',['emission.services',
     });
 
     $scope.refreshScreen = function() {
+        console.log("Refreshing screen");
         $scope.settings = {};
         $scope.settings.collect = {};
         $scope.settings.sync = {};
@@ -250,6 +251,7 @@ angular.module('emission.main.control',['emission.services',
         $scope.getEmail();
         $scope.getState().then($scope.isTrackingOn).then(function(isTracking) {
             $scope.$apply(function() {
+                console.log("Setting settings.collect.trackingOn = "+isTracking);
                 $scope.settings.collect.trackingOn = isTracking;
             });
         });
@@ -390,23 +392,20 @@ angular.module('emission.main.control',['emission.services',
     });
     $scope.isTrackingOn = function() {
         return $ionicPlatform.ready().then(function() {
-        if($scope.isAndroid()){
-            return $scope.settings.collect.state != "local.state.tracking_stopped";
-        } else if ($scope.isIOS()) {
-            return $scope.settings.collect.state != "STATE_TRACKING_STOPPED";
-        }
+            if($scope.isAndroid()){
+                return $scope.settings.collect.state != "local.state.tracking_stopped";
+            } else if ($scope.isIOS()) {
+                return $scope.settings.collect.state != "STATE_TRACKING_STOPPED";
+            }
         });
     };
     $scope.userStartStopTracking = function() {
-        if ($scope.startStopBtnToggle){
-            ControlCollectionHelper.forceTransition('STOP_TRACKING');
-            $scope.startStopBtnToggle = false;
+        if ($scope.settings.collect.trackingOn){
+            return ControlCollectionHelper.forceTransition('STOP_TRACKING');
         } else {
-            ControlCollectionHelper.forceTransition('START_TRACKING');
-            $scope.startStopBtnToggle = true;
+            return ControlCollectionHelper.forceTransition('START_TRACKING');
         }
     }
-    $scope.startStopBtnToggle = $scope.trackingOn;
     $scope.getExpandButtonClass = function() {
         return ($scope.expanded)? "icon ion-ios-arrow-up" : "icon ion-ios-arrow-down";
     }