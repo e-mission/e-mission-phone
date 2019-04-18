cf067734c2848c4b309d55749516729ec06c9f36
Trevor Wu <trevorwu@Trevors-MacBook-Pro-4.local>
2019-04-18 11:07:07 -0700

    removed test yali's business id from code

--- www/js/diary/detail.js
+++ www/js/diary/detail.js
@@ -45,7 +45,7 @@ angular.module('emission.main.diary.detail',['ui-leaflet', 'ng-walkthrough',
   });
   $scope.name = "Cannot Retrieve Suggestion";
   $scope.mode = "Cannot Retrieve Mode";
-  $scope.bid = "yalis-stanley-hall-cafe-berkeley";
+  $scope.bid = "";
   $scope.stars = 4.5;
   $http.get('json/yelpfusion.json').then(function(result) {
         $scope.yelp = result.data;
