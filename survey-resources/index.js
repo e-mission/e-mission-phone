3a0ac0ed2f82d46ea6269e8d62d3d4004fff0ddf
Attawit Kittikrairit <atton16@gmail.com>
2019-05-26 15:31:09 +0700

    fix: Refactor survey-resources

--- survey-resources/index.js
+++ survey-resources/index.js
@@ -1,35 +1,35 @@
-const fs = require('fs');
-const transformer = require('enketo-transformer');
+const fs = require("fs");
+const transformer = require("enketo-transformer");
 
 const config = {
-  inputDir: 'data-xml',
-  outputDir: 'data-json',
+    inputDir: "data-xml",
+    outputDir: "data-json",
 };
 
-console.log('==== xform-transformer ====');
-console.log('');
+console.log("==== xform-transformer ====");
+console.log("");
 console.log(`Input directory: "${config.inputDir}"`);
 console.log(`Output directory: "${config.outputDir}"`);
-console.log('');
-console.log('Begin transforming xform files...');
-console.log('');
+console.log("");
+console.log("Begin transforming xform files...");
+console.log("");
 
 fs.readdirSync(`./${config.inputDir}`, {
-  encoding: 'utf8',
-  withFileTypes: true,
+    encoding: "utf8",
+    withFileTypes: true,
 }).filter(file => {
-  return file.match(/.xml$/);
+    return file.match(/.xml$/);
 }).forEach(file => {
-  const filename = file.substr(0, file.length - 4);
-  const inputPath = `./${config.inputDir}/${file}`;
-  const outputPath = `./${config.outputDir}/${filename}.json`;
-  console.log(`Reading "${inputPath}" ...`);
-  transformer.transform({
-    xform: fs.readFileSync(inputPath),
-    preprocess: doc => doc,
-  }).then(function(result){
-    fs.writeFileSync(outputPath, JSON.stringify(result));
-    console.log(`Successfully saved "${outputPath}"`);
-  });
+    const filename = file.substr(0, file.length - 4);
+    const inputPath = `./${config.inputDir}/${file}`;
+    const outputPath = `./${config.outputDir}/${filename}.json`;
+    console.log(`Reading "${inputPath}" ...`);
+    transformer.transform({
+        xform: fs.readFileSync(inputPath),
+        preprocess: doc => doc,
+    }).then(function(result){
+        fs.writeFileSync(outputPath, JSON.stringify(result));
+        console.log(`Successfully saved "${outputPath}"`);
+    });
 });
 
