e-mission/e-mission-phone

commit 706c57b4947ed2251b166f1141f97e6e17bdc3b6

Use the correct section id for the end section transition
Since it is async, we need to cache the old value, otherwise it is reset.
We already do this for trip, now doing it for section as well.
Also changed the start/end trip methods to return the promise, just in case we
need to use it elsewhere.

www/js/eval.js
@@ -554,31 +554,23 @@ angular.module('emission.main.eval',['emission.plugin.logger',"emission.plugin.k
         $scope.eval_trip.ongoing_trip = true;
         $scope.eval_trip.waiting_for_trip_start = false;
         $scope.eval_trip.stopped = false;
-        EvalServices.generateTransition(
+        KVStore.set(EVAL_TRIP_KEY, $scope.eval_trip);
+        KVStore.set(EVAL_SECTION_KEY, $scope.eval_section);
+        return EvalServices.generateTransition(
             EvalServices.getTransitionData(ETENUM.START_EVALUATION_TRIP,
                 $scope.eval_trip.raw.id, $scope)).then(function() {
             // using promises to ensure proper ordering between trip start and
             // section start
-            EvalServices.generateTransition(
+            return EvalServices.generateTransition(
                 EvalServices.getTransitionData(ETENUM.START_EVALUATION_SECTION,
                     $scope.eval_section.raw.id, $scope));
         });
-        KVStore.set(EVAL_TRIP_KEY, $scope.eval_trip);
-        KVStore.set(EVAL_SECTION_KEY, $scope.eval_section);
     }
 
     $scope.endEvalTrip = function() {
         // Need to do store the old trip id because otherwise it gets reset before
         // the async call is complete
         var stopping_trip_id = $scope.eval_trip.raw.id;
-        EvalServices.generateTransition(
-            EvalServices.getTransitionData(ETENUM.STOP_EVALUATION_SECTION,
-                $scope.eval_section.raw.id, $scope)).then(function() {
-            // using promises to ensure proper ordering between section end and trip end
-            EvalServices.generateTransition(
-                EvalServices.getTransitionData(ETENUM.STOP_EVALUATION_TRIP,
-                    stopping_trip_id, $scope));
-        });
         $scope.eval_trip.ongoing_trip = false;
         $scope.eval_trip.waiting_for_trip_start = true;
         $scope.eval_trip.stopped = true;
@@ -587,9 +579,20 @@ angular.module('emission.main.eval',['emission.plugin.logger',"emission.plugin.k
         $scope.eval_trip = {};
         KVStore.set(EVAL_SECTION_KEY, $scope.eval_leg);
         KVStore.set(EVAL_TRIP_KEY, $scope.eval_trip);
+        return EvalServices.generateTransition(
+            EvalServices.getTransitionData(ETENUM.STOP_EVALUATION_SECTION,
+                $scope.eval_section.raw.id, $scope)).then(function() {
+            // using promises to ensure proper ordering between section end and trip end
+            return EvalServices.generateTransition(
+                EvalServices.getTransitionData(ETENUM.STOP_EVALUATION_TRIP,
+                    stopping_trip_id, $scope));
+        });
     }
 
     var endSection = function() {
+        // Need to store the old section id because otherwise it gets reset
+        // before the async call is complete
+        var stopping_section_id = $scope.eval_section.raw.id;
         if ($scope.eval_trip.multi_leg) {
             var new_index = $scope.eval_section.index+1;
             $scope.eval_section.raw = $scope.eval_trip.raw.legs[new_index];
@@ -603,7 +606,7 @@ angular.module('emission.main.eval',['emission.plugin.logger',"emission.plugin.k
         KVStore.set(EVAL_SECTION_KEY, $scope.eval_section);
         return EvalServices.generateTransition(
             EvalServices.getTransitionData(ETENUM.STOP_EVALUATION_SECTION,
-                $scope.eval_section.raw.id, $scope));
+                stopping_section_id, $scope));
     }
 
     var startSection = function() {

commit 7e86cdb8a36f026619c73ae7385820b63167136c

Fix regression for trip end
Fix regression caused by reordering in 706c57b4947ed2251b166f1141f97e6e17bdc3b6
Since we moved the transition generation to the end, the section id was blanked
out and we got an undefined error.

We now store the initial trip **and** section id and use the to generate the
transition.

Testing done:
- Went through a local spec and ensured that the pushed values had the stop
  trip transition and the final stop section transition

www/js/eval.js
@@ -571,6 +571,7 @@ angular.module('emission.main.eval',['emission.plugin.logger',"emission.plugin.k
         // Need to do store the old trip id because otherwise it gets reset before
         // the async call is complete
         var stopping_trip_id = $scope.eval_trip.raw.id;
+        var stopping_section_id = $scope.eval_section.raw.id;
         $scope.eval_trip.ongoing_trip = false;
         $scope.eval_trip.waiting_for_trip_start = true;
         $scope.eval_trip.stopped = true;
@@ -581,7 +582,7 @@ angular.module('emission.main.eval',['emission.plugin.logger',"emission.plugin.k
         KVStore.set(EVAL_TRIP_KEY, $scope.eval_trip);
         return EvalServices.generateTransition(
             EvalServices.getTransitionData(ETENUM.STOP_EVALUATION_SECTION,
-                $scope.eval_section.raw.id, $scope)).then(function() {
+                stopping_section_id, $scope)).then(function() {
             // using promises to ensure proper ordering between section end and trip end
             return EvalServices.generateTransition(
                 EvalServices.getTransitionData(ETENUM.STOP_EVALUATION_TRIP,