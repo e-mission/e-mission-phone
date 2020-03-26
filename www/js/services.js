46cf4efb5ab52a5eaefa43d786e31122cde9db09
Tarek El Ghoul <tarek.elghoul@outlook.com>
2020-03-26 16:51:13 +0100

    Changing map tiles and attribution to oms due to loading problem on android

--- www/js/services.js
+++ www/js/services.js
@@ -564,9 +564,9 @@ angular.module('emission.services', ['emission.plugin.logger',
 
     config.getMapTiles = function() {
       return {
-          tileLayer: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.png',
+          tileLayer: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
           tileLayerOptions: {
-              attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
+              attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
               opacity: 0.9,
               detectRetina: true,
               reuseTiles: true,
