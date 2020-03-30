75bf6df65c1d5572e02124a6b768e46092709384
K. Shankari <shankari@eecs.berkeley.edu>
2020-03-30 15:32:01 -0700

    We will only support android 5.0+
    No more crosswalk

--- bin/sign_and_align_keys.sh
+++ bin/sign_and_align_keys.sh
@@ -11,30 +11,3 @@ cordova build android --release -- --minSdkVersion=21 --gradleArg=-PcdvVersionCo
 cp platforms/android/build/outputs/apk/release/android-release-unsigned.apk platforms/android/build/outputs/apk/android-L+-release-signed-unaligned.apk
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/Safety_Infrastructure/MovesConnect/production.keystore ./platforms/android/build/outputs/apk/android-L+-release-signed-unaligned.apk androidproductionkey
 ~/Library/Android/sdk/build-tools/27.0.3/zipalign -v 4 platforms/android/build/outputs/apk/android-L+-release-signed-unaligned.apk emission-L+-build-$1.apk
-
-# Re-add the plugin
-cordova plugin add cordova-plugin-crosswalk-webview
-
-# Copy the workaround to make it ready to build with gradle
-cp bin/xwalk6-workaround.gradle platforms/android/cordova-plugin-crosswalk-webview
-python bin/gradle_workaround.py -a
-
-# Rebuild the entries with crosswalk
-cordova build android --release
-
-# Sign and release arm7
-cp platforms/android/build/outputs/apk/armv7/release/android-armv7-release-unsigned.apk platforms/android/build/outputs/apk/android-arm7-release-signed-unaligned.apk
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/Safety_Infrastructure/MovesConnect/production.keystore ./platforms/android/build/outputs/apk/android-arm7-release-signed-unaligned.apk androidproductionkey
-~/Library/Android/sdk/build-tools/27.0.3/zipalign -v 4 platforms/android/build/outputs/apk/android-arm7-release-signed-unaligned.apk emission-arm7-build-$1.apk
- 
-# Sign and release x86
-cp platforms/android/build/outputs/apk/x86/release/android-x86-release-unsigned.apk platforms/android/build/outputs/apk/android-x86-release-signed-unaligned.apk
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/Safety_Infrastructure/MovesConnect/production.keystore ./platforms/android/build/outputs/apk/android-x86-release-signed-unaligned.apk androidproductionkey
-~/Library/Android/sdk/build-tools/27.0.3/zipalign -v 4 platforms/android/build/outputs/apk/android-x86-release-signed-unaligned.apk emission-x86-build-$1.apk
-
-# Remove the plugin
-cordova plugin remove cordova-plugin-crosswalk-webview
-
-# Remove the build workarounds
-python bin/gradle_workaround.py -r
-rm platforms/android/cordova-plugin-crosswalk-webview/xwalk6-workaround.gradle
