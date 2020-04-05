6abd803355c6aeec45cebfe086ec2c5c69b8fc36
K. Shankari <shankari@eecs.berkeley.edu>
2020-04-05 00:09:23 -0700

    I always forget to bump up the number of plugins!

--- setup/setup_shared_native.sh
+++ setup/setup_shared_native.sh
@@ -19,7 +19,7 @@ sed -i -e "s|/usr/bin/env node|/usr/bin/env node --unhandled-rejections=strict|"
 
 npx cordova prepare
 
-EXPECTED_COUNT=16
+EXPECTED_COUNT=17
 INSTALLED_COUNT=`npx cordova plugin list | wc -l`
 echo "Found $INSTALLED_COUNT plugins, expected $EXPECTED_COUNT"
 if [ $INSTALLED_COUNT -lt $EXPECTED_COUNT ];
