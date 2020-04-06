f949605957e7f98aa211a7611257059168275cb3
K. Shankari <shankari@eecs.berkeley.edu>
2020-04-05 17:49:42 -0700

    Bunch of minor fixes found in testing
    + bump up the cordova android version
    + use the matching kotlin SDK library
    + adapt the sign and release script to the new apk locations in cordova-android 7+

--- bin/sign_and_align_keys.sh
+++ bin/sign_and_align_keys.sh
@@ -8,6 +8,6 @@ fi
 # Sign and release the L+ version
 # Make sure the highest supported version has the biggest version code
 npx cordova build android --release -- --minSdkVersion=21
-cp platforms/android/build/outputs/apk/release/android-release-unsigned.apk platforms/android/build/outputs/apk/android-L+-release-signed-unaligned.apk
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/Safety_Infrastructure/MovesConnect/production.keystore ./platforms/android/build/outputs/apk/android-L+-release-signed-unaligned.apk androidproductionkey
-~/Library/Android/sdk/build-tools/27.0.3/zipalign -v 4 platforms/android/build/outputs/apk/android-L+-release-signed-unaligned.apk cv-19-track-L+-build-$1.apk
+cp platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk platforms/android/app/build/outputs/apk/app-L+-release-signed-unaligned.apk
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/Safety_Infrastructure/MovesConnect/production.keystore ./platforms/android/app/build/outputs/apk/app-L+-release-signed-unaligned.apk androidproductionkey
+~/Library/Android/sdk/build-tools/27.0.3/zipalign -v 4 platforms/android/app/build/outputs/apk/app-L+-release-signed-unaligned.apk cv-19-track-L+-build-$1.apk
