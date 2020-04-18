837ccdca1f6648144379951ab65e91eb064cc3ad
K. Shankari <shankari@eecs.berkeley.edu>
2020-04-18 07:44:25 -0700

    Fix dest path for GoogleServices-Info.json

--- setup/setup_shared_native.sh
+++ setup/setup_shared_native.sh
@@ -1,7 +1,7 @@
 ./bin/configure_xml_and_json.js cordovabuild
 
 echo "Copying fake FCM configurations for android and iOS"
-cp setup/GoogleService-Info.fake.for_ci.plist setup/GoogleService-Info.plist
+cp setup/GoogleService-Info.fake.for_ci.plist GoogleService-Info.plist
 cp setup/google-services.fake.for_ci.json google-services.json
 
 echo "Setting up all npm packages"
