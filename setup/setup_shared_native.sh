17810168aea707ad6c1045d9db80cfdad311c942
shankari <shankari@eecs.berkeley.edu>
2020-07-10 12:34:21 -0700

    Bump up the count to account for cordova-plugin-advanced-http
    Added to workaround CORS issues with making http calls from javascript directly.

--- setup/setup_shared_native.sh
+++ setup/setup_shared_native.sh
@@ -23,7 +23,7 @@ sed -i -e "s|/usr/bin/env node|/usr/bin/env node --unhandled-rejections=strict|"
 
 npx cordova prepare
 
-EXPECTED_COUNT=25
+EXPECTED_COUNT=26
 INSTALLED_COUNT=`npx cordova plugin list | wc -l`
 echo "Found $INSTALLED_COUNT plugins, expected $EXPECTED_COUNT"
 if [ $INSTALLED_COUNT -lt $EXPECTED_COUNT ];
