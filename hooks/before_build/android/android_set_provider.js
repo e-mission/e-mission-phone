015747b7aa2e074660781f89d2ae4228977b4571
Shankari <shankari@eecs.berkeley.edu>
2020-07-13 21:05:41 -0700

    Fix variable name in hook

--- hooks/before_build/android/android_set_provider.js
+++ hooks/before_build/android/android_set_provider.js
@@ -76,7 +76,7 @@ var changeAccountTypeAndProvider = function (file, accountType, providerName, ne
 
 module.exports = function (context) {
     // If Android platform is not installed, don't even execute
-    if (!ctx.opts.platforms.includes('android')) return;
+    if (!context.opts.platforms.includes('android')) return;
 
     console.log(LOG_NAME + "Retrieving application name...")
     var config_xml = path.join(context.opts.projectRoot, 'config.xml');
