44b7aa589493d638c773357638abcb8ba290af0a
Shankari <shankari@eecs.berkeley.edu>
2020-07-15 16:57:07 -0700

    Replace the obsolete `ionic-plugin-keyboard` by the new `cordova-plugin-ionic-keyboard`
    Per
    https://github.com/ionic-team/ionic-plugin-keyboard/issues/305
    
    Also remove related javascript code because of the two options:
    - one is already the default
    - it is unclear what the second does and whether it is needed

--- www/js/app.js
+++ www/js/app.js
@@ -38,11 +38,7 @@ angular.module('emission', ['ionic',
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     Logger.log("ionicPlatform is ready");
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
 
-    }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
