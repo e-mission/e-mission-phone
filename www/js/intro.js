42f58d1df675c11244f1e67e5813f5ee13a73ca2
Shankari <shankari@eecs.berkeley.edu>
2020-08-28 09:03:19 -0700

    Customizations specific to CEO project
    Custom survey
    Custom consent
    
    DO NOT MERGE TO MASTER

--- www/js/intro.js
+++ www/js/intro.js
@@ -137,7 +137,7 @@ angular.module('emission.intro', ['emission.splash.startprefs',
   };
   $scope.startSurvey = function () {
       SurveyLaunch.startSurveyWithXPath(
-        'https://docs.google.com/forms/d/e/1FAIpQLSd_7VICYb8AfLQPIt8pg9AYsXW_rrHQBfsv-NSviO7Dgk7fyg/viewform',
+        'https://docs.google.com/forms/d/e/1FAIpQLSfA-JCnD5Tb2olZn0UAO91_juU-wuToxJc6uHdMjEQoSi-YTw/viewform',
         '/html/body/div/div[2]/form/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input');
   };
 
