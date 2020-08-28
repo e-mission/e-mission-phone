1648c95f1c71a069aabc74d76279d45ebda3a74a
Shankari <shankari@eecs.berkeley.edu>
2020-08-28 09:18:38 -0700

    Switch the trip confirm screen to also go to the diary directly
    Avoids race conditions and confusion; highlights the diary
    We may want to merge this to master based on feedback from the CEO project

--- www/js/diary/list.js
+++ www/js/diary/list.js
@@ -245,7 +245,12 @@ angular.module('emission.main.diary.list',['ui-leaflet',
             $scope.populateBasicClasses(tripgj);
             $scope.populateCommonInfo(tripgj);
           });
-          $ionicScrollDelegate.scrollTop(true);
+          if ($rootScope.displayingIncident) {
+            $ionicScrollDelegate.scrollBottom(true);
+            $rootScope.displayingIncident = false;
+          } else {
+              $ionicScrollDelegate.scrollTop(true);
+          }
       });
     });
 
@@ -610,7 +615,6 @@ angular.module('emission.main.diary.list',['ui-leaflet',
           } else {
              Logger.log("currDay is not defined, load not complete");
           }
-          $rootScope.displayingIncident = false;
         }
       });
     });
