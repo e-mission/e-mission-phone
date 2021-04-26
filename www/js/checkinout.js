2ef04067a8dee1bccf9be89f6515f64d12bb5906
Shankari <shankari@eecs.berkeley.edu>
2021-04-25 19:41:34 -0700

    Fill in a basic, in-memory implementation of the checkin/checkout functionality
    - Add text for checkin/checkout buttons, and for indicating that bikes are checked in or not
    - come up with a format for the encoded URL (emission://checkinout?bikeNo=XXX)
    - check for the URL and extract the bike number
    - read the URL by scanning (currently untested)
    - add a debug verson to allow manual entry of the URL and use it for testing
    - test checkin/checkout functionality
    
    note that the status is not currently persisted anywhere, this is an in-memory-only version.

--- www/js/checkinout.js
+++ www/js/checkinout.js
@@ -8,5 +8,123 @@ angular.module('emission.main.checkinout',[
                                     $ionicPopup, ClientStats,
                                     KVStore, Logger, $ionicPopover, $translate) {
 
+$scope.data = {};
+$scope.checkout = {
+    status: false,
+    bikeNo: undefined
+};
+
+const getScannedNo = function(urlText) {
+    const url = new URL(urlText);
+    if (url.protocol != "emission:") {
+        throw new Error("Invalid protocol "+url.protocol+" in url "+urlText);
+    }
+    if (url.pathname != "//checkinout") {
+        throw new Error("Invalid pathname "+url.pathname+" in url "+urlText);
+    }
+    const bikeNo = url.searchParams.get("bikeNo");
+    if (bikeNo === null) {
+        throw new Error("No bike number found in url "+urlText);
+    } else {
+        return bikeNo;
+    }
+}
+
+const scanBikeNo = function() {
+    return new Promise(function(resolve, reject) {
+        if (!$scope.scanEnabled) {
+            reject(new Error("plugins not yet initialized, please retry later"));
+        } else {
+          cordova.plugins.barcodeScanner.scan(
+            function (result) {
+                if (result.format == "QR_CODE" &&
+                    result.cancelled == false) {
+                    try {
+                        const bikeNo = getScannedNo(result.text);
+                        resolve(bikeNo);
+                    } catch(e) {
+                        reject(e);
+                    }
+                } else {
+                    reject(new Error("invalid QR code"+result.text));
+                }
+            },
+            function (error) {
+                reject(error);
+            });
+        }
+    });
+}
+
+const scanBikeNoDebug = function() {
+    return new Promise(function(resolve, reject) {
+      var scanBikePopup = $ionicPopup.show({
+        template: '<input type="userEmail" ng-model="data.scannedurl">',
+        title: 'Copy and paste the bike URL',
+        scope: $scope,
+        buttons: [
+          {
+            text: '<b>Save</b>',
+            type: 'button-positive',
+            onTap: function(e) {
+              if (!$scope.data.scannedurl) {
+                //don't allow the user to close unless he enters a username
+
+                e.preventDefault();
+              } else {
+                if ($scope.data.scannedurl.indexOf(' ') >= 0) {
+                  e.preventDefault();
+                }
+                return $scope.data.scannedurl;
+              }
+            }
+          }
+        ]
+      });
+      scanBikePopup.then(function(res) {
+        console.log('Tapped!', res);
+        try {
+            const bikeNo = getScannedNo(res);
+            resolve(bikeNo);
+        } catch (e) {
+            reject(e);
+        }
+      });
+    });
+}
+
+$scope.doCheckout = function() {
+    scanBikeNoDebug().then((bikeNo) => {
+        $scope.$apply(function() {
+            $scope.checkout.status = true;
+            $scope.checkout.bikeNo = bikeNo;
+        });
+    }).catch((err) => {
+        Logger.displayError("Error while scanning QR code", err);
+    });
+}
+
+$scope.doCheckin = function() {
+    scanBikeNoDebug().then((bikeNo) => {
+        if (bikeNo === $scope.checkout.bikeNo) {
+            $scope.$apply(function() {
+                $scope.checkout.status = false;
+                $scope.checkout.bikeNo = undefined;
+            });
+        } else {
+            Logger.displayError("Error while scanning QR code",
+                new Error("Please check in bike "+$scope.checkout.bikeNo+" first"));
+        }
+    }).catch((err) => {
+        Logger.displayError("Error while scanning QR code", err);
+    });
+}
+
+$ionicPlatform.ready(function() {
+    if (cordova.plugins.barcodeScanner != undefined) {
+        $scope.scanEnabled = true;
+    }
+});
+
 
 });
