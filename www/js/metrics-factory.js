922b328c98780f1812d2004ec2598ce1bb02c738
Shankari <shankari@eecs.berkeley.edu>
2021-12-15 12:53:42 -0800

    Support custom values for footprint and calorie calculations

--- www/js/metrics-factory.js
+++ www/js/metrics-factory.js
@@ -1,18 +1,33 @@
 'use strict';
 
-angular.module('emission.main.metrics.factory', ['emission.services', 'emission.plugin.kvstore'])
+angular.module('emission.main.metrics.factory',
+        ['emission.main.metrics.mappings',
+        'emission.plugin.kvstore'])
 
-.factory('FootprintHelper', function(CarbonDatasetHelper) {
+.factory('FootprintHelper', function(CarbonDatasetHelper, CustomDatasetHelper) {
   var fh = {};
   var mtokm = function(v) {
     return v / 1000;
+  }
+  fh.useCustom = false;
+
+  fh.setUseCustomFootprint = function () {
+    fh.useCustom = true;
+  }
+
+  fh.getFootprint = function() {
+    if (this.useCustom == true) {
+        return CustomDatasetHelper.getCustomFootprint();
+    } else {
+        return CarbonDatasetHelper.getCurrentCarbonDatasetFootprint();
+    }
   }
 
   fh.readableFormat = function(v) {
     return v > 999? Math.round(v / 1000) + 'k kg CO₂' : Math.round(v) + ' kg CO₂';
   }
   fh.getFootprintForMetrics = function(userMetrics) {
-    var footprint = CarbonDatasetHelper.getCurrentCarbonDatasetFootprint();
+    var footprint = fh.getFootprint();
     var result = 0;
     for (var i in userMetrics) {
       var mode = userMetrics[i].key;
@@ -32,7 +47,7 @@ angular.module('emission.main.metrics.factory', ['emission.services', 'emission.
     return result;
   }
   fh.getLowestFootprintForDistance = function(distance) {
-    var footprint = CarbonDatasetHelper.getCurrentCarbonDatasetFootprint();
+    var footprint = fh.getFootprint();
     var lowestFootprint = Number.MAX_SAFE_INTEGER;
     for (var mode in footprint) {
       if (mode == 'WALKING' || mode == 'BICYCLING') {
@@ -45,20 +60,34 @@ angular.module('emission.main.metrics.factory', ['emission.services', 'emission.
     return lowestFootprint * mtokm(distance);
   }
   fh.getHighestFootprintForDistance = function(distance) {
-    var footprint = CarbonDatasetHelper.getCurrentCarbonDatasetFootprint();
+    var footprint = fh.getFootprint();
     var highestFootprint = 0;
     for (var mode in footprint) {
       highestFootprint = Math.max(highestFootprint, footprint[mode]);
     }
     return highestFootprint * mtokm(distance);
   }
+
   return fh;
 })
 
-.factory('CalorieCal', function(KVStore){
+.factory('CalorieCal', function(KVStore, METDatasetHelper, CustomDatasetHelper) {
 
   var cc = {};
   var USER_DATA_KEY = "user-data";
+  cc.useCustom = false;
+
+  cc.setUseCustomFootprint = function () {
+    cc.useCustom = true;
+  }
+
+  cc.getMETs = function() {
+    if (this.useCustom == true) {
+        return CustomDatasetHelper.getCustomMETs();
+    } else {
+        return METDatasetHelper.getStandardMETs();
+    }
+  }
 
   cc.set = function(info) {
     return KVStore.set(USER_DATA_KEY, info);
@@ -77,13 +106,14 @@ angular.module('emission.main.metrics.factory', ['emission.services', 'emission.
       console.log("CalorieCal.getMet() converted 'ON_FOOT' to 'WALKING'");
       mode = 'WALKING';
     }
-    if (!standardMETs[mode]) {
+    let currentMETs = cc.getMETs();
+    if (!currentMETs[mode]) {
       console.log("CalorieCal.getMet() Illegal mode: " + mode);
       return 0; //So the calorie sum does not break with wrong return type
     }
-    for (var i in standardMETs[mode]) {
-      if (mpstomph(speed).between(standardMETs[mode][i].range[0], standardMETs[mode][i].range[1])) {
-        return standardMETs[mode][i].mets;
+    for (var i in currentMETs[mode]) {
+      if (mpstomph(speed).between(currentMETs[mode][i].range[0], currentMETs[mode][i].range[1])) {
+        return currentMETs[mode][i].mets;
       } else if (mpstomph(speed) < 0 ) {
         console.log("CalorieCal.getMet() Negative speed: " + mpstomph(speed));
         return 0;
@@ -116,128 +146,5 @@ angular.module('emission.main.metrics.factory', ['emission.services', 'emission.
   cc.getCalories = function(weightInKg, durationInMin, met) {
     return weightInKg * durationInMin * met;
   }
-  var standardMETs = {
-    "WALKING": {
-      "VERY_SLOW": {
-        range: [0, 2.0],
-        mets: 2.0
-      },
-      "SLOW": {
-        range: [2.0, 2.5],
-        mets: 2.8
-      },
-      "MODERATE_0": {
-        range: [2.5, 2.8],
-        mets: 3.0
-      },
-      "MODERATE_1": {
-        range: [2.8, 3.2],
-        mets: 3.5
-      },
-      "FAST": {
-        range: [3.2, 3.5],
-        mets: 4.3
-      },
-      "VERY_FAST_0": {
-        range: [3.5, 4.0],
-        mets: 5.0
-      },
-      "VERY_FAST_!": {
-        range: [4.0, 4.5],
-        mets: 6.0
-      },
-      "VERY_VERY_FAST": {
-        range: [4.5, 5],
-        mets: 7.0
-      },
-      "SUPER_FAST": {
-        range: [5, 6],
-        mets: 8.3
-      },
-      "RUNNING": {
-        range: [6, Number.MAX_VALUE],
-        mets: 9.8
-      }
-    },
-    "BICYCLING": {
-      "VERY_VERY_SLOW": {
-        range: [0, 5.5],
-        mets: 3.5
-      },
-      "VERY_SLOW": {
-        range: [5.5, 10],
-        mets: 5.8
-      },
-      "SLOW": {
-        range: [10, 12],
-        mets: 6.8
-      },
-      "MODERATE": {
-        range: [12, 14],
-        mets: 8.0
-      },
-      "FAST": {
-        range: [14, 16],
-        mets: 10.0
-      },
-      "VERT_FAST": {
-        range: [16, 19],
-        mets: 12.0
-      },
-      "RACING": {
-        range: [20, Number.MAX_VALUE],
-        mets: 15.8
-      }
-    },
-    "IN_VEHICLE": {
-      "ALL": {
-        range: [0, Number.MAX_VALUE],
-        mets: 0
-      }
-    },
-    "CAR": {
-      "ALL": {
-        range: [0, Number.MAX_VALUE],
-        mets: 0
-      }
-    },
-    "BUS": {
-      "ALL": {
-        range: [0, Number.MAX_VALUE],
-        mets: 0
-      }
-    },
-    "LIGHT_RAIL": {
-      "ALL": {
-        range: [0, Number.MAX_VALUE],
-        mets: 0
-      }
-    },
-    "TRAIN": {
-      "ALL": {
-        range: [0, Number.MAX_VALUE],
-        mets: 0
-      }
-    },
-    "TRAM": {
-      "ALL": {
-        range: [0, Number.MAX_VALUE],
-        mets: 0
-      }
-    },
-    "SUBWAY": {
-      "ALL": {
-        range: [0, Number.MAX_VALUE],
-        mets: 0
-      }
-    },
-    "AIR_OR_HSR": {
-      "ALL": {
-        range: [0, Number.MAX_VALUE],
-        mets: 0
-      }
-    }
-  }
   return cc;
-
 });
