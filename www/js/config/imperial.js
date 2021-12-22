8616fb01c2001d65fa159e851f26c27b117c62a4
K. Shankari <shankari@eecs.berkeley.edu>
2021-12-22 09:25:43 -0800

    Fix stupid typo in imperial conversions

--- www/js/config/imperial.js
+++ www/js/config/imperial.js
@@ -30,7 +30,7 @@ angular.module('emission.config.imperial', ['emission.plugin.logger'])
     };
 
     ic.getMph = function(metersPerSecond) {
-        return (KM_TO_MILES * ic.getKmph(metersPerSec));
+        return (KM_TO_MILES * ic.getKmph(metersPerSecond));
     };
 
     ic.getFormattedDistance = USE_IMPERIAL? ic.getFormattedDistanceInMiles : ic.getFormattedDistanceInKm;
