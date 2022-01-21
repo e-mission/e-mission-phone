f953a3e9126efc3be1909115405ed82688049b7b
K. Shankari <shankari@eecs.berkeley.edu>
2022-01-20 21:47:20 -0800

    Make sure to flip the high and low for carbon avoided
    The *low* range of the carbon avoided = worst - *high* range of the carbon used

--- www/js/metrics.js
+++ www/js/metrics.js
@@ -832,8 +832,8 @@ angular.module('emission.main.metrics',['nvd3',
         // $scope.carbonData.optimalCarbon = FootprintHelper.getLowestFootprintForDistance(optimalDistance);
         $scope.carbonData.worstCarbon   = FootprintHelper.getHighestFootprintForDistance(worstDistance);
         $scope.carbonData.carbonAvoided = {
-            low: $scope.carbonData.worstCarbon - $scope.carbonData.userCarbon.low,
-            high: $scope.carbonData.worstCarbon - $scope.carbonData.userCarbon.high,
+            low: $scope.carbonData.worstCarbon - $scope.carbonData.userCarbon.high,
+            high: $scope.carbonData.worstCarbon - $scope.carbonData.userCarbon.low,
         };
       }
 
