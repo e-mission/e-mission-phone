7d98d5dcbaafce0ae449694f5aa7421d8d8a9d9e
K. Shankari <shankari@eecs.berkeley.edu>
2022-04-29 22:23:06 -0700

    Add the labels to the detail screen
    So you don't have to see the trip and then go back to the list to verify
    Fortunately, this was already a directive, so the change largely involved
    adding the directive to the detail screen.
    
    Couple of modifications:
    - we allow the specification of a linkedid to make the DOM traversal simpler
    - we choose the survey opt in the detail screen as well
    - on `recomputeDisplayTrips`, we close the detail screen
    
    TODO: Might want to configure the wait to recompute since 30 secs is too large
    for the detail screen use case.
    
    https://github.com/e-mission/e-mission-docs/issues/111

--- www/js/survey/one-click-button.js
+++ www/js/survey/one-click-button.js
@@ -3,6 +3,7 @@ angular.module('emission.survey.verifycheck', [])
   return {
     scope: {
         linkedtag: "@",
+        linkedid: "@", // specific ID, if present, simplifies the dom traversal logic
     },
     controller: "OneClickButtonCtrl",
     templateUrl: 'templates/survey/one-click-button.html'
@@ -10,15 +11,25 @@ angular.module('emission.survey.verifycheck', [])
 })
 .controller("OneClickButtonCtrl", function($scope, $element, $attrs) {
     var findLinkedLabelScope = function() {
-        console.log("$element is ", $element, "linkedtag is ",$scope.linkedtag);
-        console.log("parent row is", $element.parents("ion-item"));
-        let rowElement = $element.parents("ion-item")
-        console.log("row Element is", rowElement);
-        let linkedlabel = rowElement.find($scope.linkedtag);
-        console.log("child linkedlabel is", linkedlabel);
-        let linkedlabelScope = angular.element(linkedlabel).isolateScope();
-        console.log("linkedlabel scope is", linkedlabelScope);
-        return linkedlabelScope;
+        if ($scope.linkedid) {
+            let linkedsurveytag = angular.element(document.getElementById($scope.linkedid));
+            console.log("matching linkedsurvey with id "+$scope.linkedid+" is ",linkedsurveytag);
+            let linkedlabel = linkedsurveytag.find($scope.linkedtag);
+            console.log("child linkedlabel is", linkedlabel);
+            let linkedlabelScope = angular.element(linkedlabel).isolateScope();
+            console.log("linkedlabel scope is", linkedlabelScope);
+            return linkedlabelScope;
+        } else {
+            console.log("$element is ", $element, "linkedtag is ",$scope.linkedtag);
+            console.log("parent row is", $element.parents("ion-item"));
+            let rowElement = $element.parents("ion-item")
+            console.log("row Element is", rowElement);
+            let linkedlabel = rowElement.find($scope.linkedtag);
+            console.log("child linkedlabel is", linkedlabel);
+            let linkedlabelScope = angular.element(linkedlabel).isolateScope();
+            console.log("linkedlabel scope is", linkedlabelScope);
+            return linkedlabelScope;
+        }
     };
 
     $scope.verifyTrip = function() {
