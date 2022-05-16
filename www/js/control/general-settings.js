61132da88174c7b8ab8aadb4838e789f5be053e5
aGuttman <slaker117@gmail.com>
2022-05-16 15:57:31 -0600

    View QR Code Row
    Added a row to the profile screen to view qr code. Pressing the icon creates a popup where qr code will go.

--- www/js/control/general-settings.js
+++ www/js/control/general-settings.js
@@ -82,9 +82,10 @@ angular.module('emission.main.control',['emission.services',
     $scope.viewPrivacyPolicy = function($event) {
         // button -> list element -> scroll
         // const targetEl = $event.currentTarget.parentElement.parentElement;
-        if ($scope.ppp) {
-            $scope.ppp.show($event);
-        } else {
+
+        // if ($scope.ppp) {
+        //     $scope.ppp.show($event);
+        // } else {
             i18nUtils.geti18nFileName("templates/", "intro/consent-text", ".html").then((consentFileName) => {
                 $scope.consentTextFile = consentFileName;
                 $ionicPopover.fromTemplateUrl("templates/control/main-consent.html", {scope: $scope}).then((p) => {
@@ -92,7 +93,17 @@ angular.module('emission.main.control',['emission.services',
                     $scope.ppp.show($event);
                 });
             }).catch((err) => Logger.displayError("Error while displaying privacy policy", err));
-        }
+        // }
+    }
+
+    $scope.viewQRCode = function($event) {
+        i18nUtils.geti18nFileName("templates/", "control/qrc", ".html").then((qrcFileName) => {
+            $scope.qrcTextFile = qrcFileName;
+            $ionicPopover.fromTemplateUrl("templates/control/main-qrc.html", {scope: $scope}).then((p) => {
+                $scope.ppp = p;
+                $scope.ppp.show($event);
+            });
+        }).catch((err) => Logger.displayError("Error while displaying QR Code", err));
     }
 
     $scope.fixAppStatus = function() {
