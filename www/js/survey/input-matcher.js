0ccab071fc00796c13b91b0a2355c895fd90b500
Shankari <shankari@eecs.berkeley.edu>
2022-05-26 18:24:10 -0700

    Gracefully handle the case in which there are no user inputs
    Simple if check
    Fixes https://github.com/e-mission/e-mission-docs/issues/727#issuecomment-1139181717

--- www/js/survey/input-matcher.js
+++ www/js/survey/input-matcher.js
@@ -17,6 +17,11 @@ angular.module('emission.survey.inputmatcher', ['emission.plugin.logger'])
     // If there is only one item in the list, return it.
     // This make it compatible when fake list is given (for Enketo Survey).
     // As well as optimize the performance.
+    if (userInputList === undefined) {
+        Logger.log("In getUserInputForTrip, no user input, returning []");
+        return undefined;
+    }
+
     if (userInputList.length === 1) {
       return userInputList[0];
     }
