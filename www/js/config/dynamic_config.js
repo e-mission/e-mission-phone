e72689993bce12d02bc8232f70b3b8acde9da5e2
Shankari <shankari@eecs.berkeley.edu>
2022-06-12 13:26:22 -0700

    Store and retrieve the UI config
    - We store the labels (source, label) and the config separately
    - When the user clicks on the link, we check the labels and only download if
      the label has changed
    
    + refactor the code to pull out download into its own function so we can only
    download if necessary
    
    https://github.com/e-mission/e-mission-docs/issues/739#issuecomment-1153253324
    https://github.com/e-mission/e-mission-docs/issues/739#issuecomment-1153283742

--- www/js/config/dynamic_config.js
+++ www/js/config/dynamic_config.js
@@ -1,11 +1,11 @@
 'use strict';
 
-angular.module('emission.config.dynamic', ['emission.plugin.logger'])
-.factory('DynamicConfig', function($http, Logger) {
+angular.module('emission.config.dynamic', ['emission.plugin.logger', 'emission.plugin.kvstore'])
+.factory('DynamicConfig', function($http, $window, KVStore, Logger) {
+    const STUDY_LABEL="DYNAMIC_UI_STUDY";
+    const CONFIG_PHONE_UI="config/phone_ui_config";
     var dc = {};
-    dc.initByUser = function(urlComponents) {
-        const label = urlComponents.label;
-        const source = urlComponents.source;
+    var readConfigFromServer = function(label, source) {
         Logger.log("Received request to switch to "+label+" from "+source);
         if (source != "github") {
             Logger.displayError("Invalid source", "Configurations from "+source+" not supported, please contact the app developer");
@@ -13,11 +13,34 @@ angular.module('emission.config.dynamic', ['emission.plugin.logger'])
         };
         const downloadURL = "https://raw.githubusercontent.com/e-mission/nrel-openpath-deploy-configs/main/configs/"+label+".nrel-op.json"
         Logger.log("Downloading data from "+downloadURL);
-        $http.get(downloadURL).then((result) => {
+        return $http.get(downloadURL).then((result) => {
             Logger.log("Successfully found the "+downloadURL+", result is " + JSON.stringify(result.data).substring(0,10));
-            return result.data;
-        }).catch((err) => {
-            Logger.displayError("Unable to download study config", err);
+            const parsedConfig = result.data;
+            Logger.log("Successfully downloaded config with version "+parsedConfig.version
+                +" for "+parsedConfig.intro.translated_text.en.deployment_name
+                +" and data collection URL"+parsedConfig.server.url );
+            return parsedConfig;
+        }).catch((fetchErr) => {
+            Logger.displayError("Unable to download study config", fetchErr);
+        });
+    }
+    dc.initByUser = function(urlComponents) {
+        const newStudyLabel = urlComponents.label;
+        KVStore.get(STUDY_LABEL).then((existingStudyLabel) => {
+            if(angular.equals(existingStudyLabel, urlComponents)) {
+                Logger.log("existing label " + JSON.stringify(existingStudyLabel) +
+                    " and new one " + JSON.stringify(urlComponents), " are the same, skipping");
+                return; // labels are the same
+            }
+            // if the labels are different
+            return readConfigFromServer(urlComponents.label, urlComponents.source).then((downloadedConfig) => {
+                const storeLabelPromise = KVStore.set(STUDY_LABEL, urlComponents);
+                const storeConfigPromise = $window.cordova.plugins.BEMUserCache.putRWDocument(
+                    CONFIG_PHONE_UI, downloadedConfig);
+                return Promise.all([storeLabelPromise, storeConfigPromise])
+                .then((storeResults) => Logger.log("Stored dynamic config successfully, result = "+JSON.stringify(storeResults)))
+                .catch((storeError) => Logger.displayError("Error storing study configuration", storeError));
+            });
         });
     };
     return dc;
