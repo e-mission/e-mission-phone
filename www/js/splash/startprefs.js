9f59a7a6cedd8374c36b245e5ff54c3744a8685d
Sebastian Barry <sebbarry@comcast.net>
2022-08-23 23:16:12 -0600

    Changed default screen to Dashboard
    startprefs.js
    - replaced inf_scroll as being the default open screen, to Dashboard (aka metrics)

--- www/js/splash/startprefs.js
+++ www/js/splash/startprefs.js
@@ -177,7 +177,7 @@ angular.module('emission.splash.startprefs', ['emission.plugin.logger',
             $rootScope.redirectParams = undefined;
             return {state: redirState, params: redirParams};
           } else {
-            return {state: 'root.main.inf_scroll', params: {}};
+            return {state: 'root.main.metrics', params: {}};
           }
         } else {
           return {state: result, params: {}};
