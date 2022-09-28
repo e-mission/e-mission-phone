1fd3294c6b836cc6a791f26becd184fd4bf4668f
Shankari <shankari@eecs.berkeley.edu>
2022-09-27 21:09:15 -0700

    Change old-style token automatically to new-style token
    without any user intervention
    
    This fixes the phone part of: https://github.com/e-mission/e-mission-docs/issues/801
    as outlined in https://github.com/e-mission/e-mission-docs/issues/801#issuecomment-1260279343
    
    Testing done:
    
    Logged out and logged in again as `test_july_22`
    Then re-launched the app
    
    Before the logout + login, we were using `nrelop_dev_test_july_22`
    After the login, we were using `test_july_22`
    After the relaunch, we were again logging in using `nrelop_dev_test_july_22`
    
    ```
    2022-09-27 18:52:59,963:DEBUG:123145469603840:Using the skip method to verify id token nrelop_dev_test_july_22 of length 23
    2022-09-27 19:02:14,691:DEBUG:123145432817664:Using the skip method to verify id token nrelop_dev_test_july_22 of length 23
    2022-09-27 19:02:36,951:DEBUG:123145459093504:Using the skip method to verify id token test_july_22 of length 12
    2022-09-27 19:02:37,012:DEBUG:123145464348672:Using the skip method to verify id token test_july_22 of length 12
    2022-09-27 19:04:34,954:DEBUG:123145438072832:Using the skip method to verify id token nrelop_dev_test_july_22 of length 23
    ```
    
    After the relaunch, we prepended the `nrelop_dev` to the token
    
    ```
    [phonegap] [console.log] Running hack code to add the study as a prefix to an existing token if needed
    [phonegap] [console.log] DEBUG:Found old style token, after prepending nrelop_dev new token is nrelop_dev_test_july_22
    ```

--- www/js/config/dynamic_config.js
+++ www/js/config/dynamic_config.js
@@ -109,6 +109,19 @@ angular.module('emission.config.dynamic', ['emission.plugin.logger'])
                 config.name = "dev";
             }
         }
+        /*
+         * Second hack to support adding in the study as a prefix to any existing token.
+         */
+        // now that we have the study name, let's make sure that the token starts with it
+        // using the plugin directly so that we can avoid adding more dependencies
+        window.cordova.plugins.BEMJWTAuth.getUserEmail().then(function(response) {
+            console.log("Running hack code to add the study as a prefix to an existing token if needed");
+            if (!response.startsWith("nrelop_")) {
+                const newToken = "nrelop_"+config.name+"_"+response;
+                Logger.log("Found old style token, after prepending nrelop_"+config.name+" new token is "+newToken);
+                window.cordova.plugins.BEMJWTAuth.setPromptedAuthToken(newToken);
+            }
+        });
     }
 
     dc.initByUser = function(urlComponents) {
