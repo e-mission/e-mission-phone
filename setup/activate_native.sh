ae1e1155c0bd65f2dad5469194b41bdb223612a4
K. Shankari <shankari@eecs.berkeley.edu>
2023-02-10 10:35:36 -0800

    Fix the breaking test by changing the path during activation

--- setup/activate_native.sh
+++ setup/activate_native.sh
@@ -8,13 +8,14 @@ echo "Verifying $ANDROID_HOME or $ANDROID_SDK_ROOT is set"
 if [ -z $ANDROID_HOME ] && [ -z $ANDROID_SDK_ROOT ];
 then
     echo "ANDROID_HOME and ANDROID_SDK_ROOT not set, android SDK not found"
+    exit 1
 fi
 
 echo "Activating sdkman, and by default, gradle"
 source ~/.sdkman/bin/sdkman-init.sh
 
 echo "Ensuring that we use the most recent version of the command line tools"
-export PATH=$ANDROID_SDK_ROOT/sdk/cmdline-tools/bin:$ANDROID_SDK_ROOT/emulator:$PATH
+export PATH=$ANDROID_SDK_ROOT/cmdline-tools/latest/bin:$ANDROID_SDK_ROOT/emulator:$PATH
 
 echo "Configuring the repo for building native code"
 ./bin/configure_xml_and_json.js cordovabuild
