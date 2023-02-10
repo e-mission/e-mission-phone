591e2ed231c0607e96935486d6f03eddba1ae093
K. Shankari <shankari@eecs.berkeley.edu>
2023-02-10 14:49:42 -0800

    Lock down the version of ruby used instead of "the default"
    - install rvm, just like we install nvm
        - this involves importing keys so that the rvm install can be validated
    - source the script
    - install the desired version of ruby
    - use that to install the correct version of cocoapods

--- setup/setup_ios_native.sh
+++ setup/setup_ios_native.sh
@@ -4,8 +4,26 @@ set -e
 # Setup the development environment
 source setup/setup_shared.sh
 
-echo "Installing cocoapods"
+# Importing rvm keys
+curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
+
+# Download and install rvm
+echo "Installing stable rvm"
+curl -sSL https://get.rvm.io | bash -s stable
+
+# Enable rvm
+source /Users/kshankar/.rvm/scripts/rvm
+
+# Download and install ruby
+echo "Installing ruby $RUBY_VERSION"
+rvm install ruby-$RUBY_VERSION
+
+echo "Switching to ruby version $RUBY_VERSION"
+rvm use $RUBY_VERSION
+
 export PATH=$RUBY_PATH:$PATH
+echo "Installing cocoapods"
 gem install --no-document --user-install cocoapods -v $COCOAPODS_VERSION
 
 source setup/setup_shared_native.sh
