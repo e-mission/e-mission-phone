e-mission/e-mission-phone

commit c9fc41fc40c62a9f1376b776a9e612064b0f7793

apply surveyOpt based on dynamic config

www/js/diary/detail.js
@@ -11,10 +11,15 @@ angular.module('emission.main.diary.detail',['ui-leaflet', 'ng-walkthrough',
                                         $state, $stateParams, ClientStats, $ionicActionSheet,
                                         leafletData, leafletMapEvents, nzTour, KVStore,
                                         Logger, Timeline, DiaryHelper, SurveyOptions, Config, ImperialConfig,
-                                        CommHelper, PostTripManualMarker, $translate) {
-  console.log("controller DiaryDetailCtrl called with params = "+
-    JSON.stringify($stateParams));
-  $scope.surveyOpt = SurveyOptions.MULTILABEL;
+                                        DynamicConfig, CommHelper, PostTripManualMarker, $translate) {
+  console.log("controller DiaryDetailCtrl called with params = "+JSON.stringify($stateParams));
+
+  DynamicConfig.configReady().then((configObj) => {
+    const surveyOptKey = configObj.survey_info['trip-labels'];
+    $scope.surveyOpt = SurveyOptions[surveyOptKey];
+    console.log('surveyOpt in details.js is', $scope.surveyOpt);
+  });
+
   $scope.tripFilterFactory = $injector.get($scope.surveyOpt.filter);
   $scope.filterInputs = $scope.tripFilterFactory.configuredFilters;
 

www/js/config/dynamic_config.js
@@ -25,14 +25,14 @@ angular.module('emission.config.dynamic', ['emission.plugin.logger'])
     }
     dc.configReady = function() {
         if (dc.isConfigReady) {
-            Logger.log("UI_CONFIG in configReady function, resolving immediately");
+            // Logger.log("UI_CONFIG in configReady function, resolving immediately");
             return Promise.resolve(dc.config);
         } else {
-            Logger.log("UI_CONFIG in configReady function, about to create promise");
+            // Logger.log("UI_CONFIG in configReady function, about to create promise");
             return new Promise(function(resolve, reject) {
-                Logger.log("Registering for UI_CONFIG_READY notification in dynamic_config inside the promise");
+                // Logger.log("Registering for UI_CONFIG_READY notification in dynamic_config inside the promise");
                 $rootScope.$on(dc.UI_CONFIG_READY, (event, newConfig) => {
-                    Logger.log("Received UI_CONFIG_READY notification in dynamic_config, resolving promise");
+                    // Logger.log("Received UI_CONFIG_READY notification in dynamic_config, resolving promise");
                     resolve(newConfig)
                 });
             });

www/js/diary/diary_list_item.js
@@ -12,19 +12,18 @@ angular.module('emission.main.diary.diarylistitem', [
     return{
         restrict: 'E',
         scope: {
-            tripgj: '='
+            tripgj: '=',
+            config: '=',
         },
         controller: 'DiaryListItemCtrl',
         templateUrl: 'templates/diary/diary_list_item.html'
     };
 })
 
-.controller("DiaryListItemCtrl", function(
-                                        $scope,
-                                        SurveyOptions,
-                                        $state, Config
-){
-    $scope.surveyOpt = SurveyOptions.MULTILABEL;
+.controller("DiaryListItemCtrl", function($scope, SurveyOptions, $state, Config) {
+    const surveyOptKey = $scope.config?.survey_info?.['trip-labels'];
+    $scope.surveyOpt = SurveyOptions[surveyOptKey];
+    console.log('surveyOpt in diary_list_item.js is', $scope.surveyOpt);
     const DEFAULT_ITEM_HT = 335;
     $scope.itemHt = DEFAULT_ITEM_HT;
 

www/js/diary/infinite_scroll_detail.js
@@ -11,12 +11,17 @@ angular.module('emission.main.diary.infscrolldetail',['ui-leaflet', 'ng-walkthro
                                         $state, $stateParams, ClientStats, $ionicActionSheet,
                                         leafletData, leafletMapEvents, nzTour, KVStore,
                                         Logger, Timeline, DiaryHelper, SurveyOptions, Config, ImperialConfig,
-                                        CommHelper, PostTripManualMarker, $translate) {
+                                        DynamicConfig, CommHelper, PostTripManualMarker, $translate) {
   console.log("controller InfiniteDiaryDetailCtrl called with params = "+
     JSON.stringify($stateParams));
-  $scope.surveyOpt = SurveyOptions.MULTILABEL;
-  $scope.tripFilterFactory = $injector.get($scope.surveyOpt.filter);
-  $scope.filterInputs = $scope.tripFilterFactory.configuredFilters;
+
+  DynamicConfig.configReady().then((configObj) => {
+    const surveyOptKey = configObj.survey_info['trip-labels'];
+    $scope.surveyOpt = SurveyOptions[surveyOptKey];
+    console.log('surveyOpt in infinite_scroll_details.js is', $scope.surveyOpt);
+    $scope.tripFilterFactory = $injector.get($scope.surveyOpt.filter);
+    $scope.filterInputs = $scope.tripFilterFactory.configuredFilters;
+  });
 
   $scope.mapCtrl = {};
   angular.extend($scope.mapCtrl, {

www/js/diary/infinite_scroll_list.js
@@ -36,34 +36,44 @@ angular.module('emission.main.diary.infscrolllist',['ui-leaflet',
   // TODO: load only a subset of entries instead of everything
 
   console.log("controller InfiniteDiaryListCtrl called");
+  
+  const DEFAULT_ITEM_HT = 274;
+  $scope.itemHt = DEFAULT_ITEM_HT;
+
+  const placeLimiter = new Bottleneck({ maxConcurrent: 2, minTime: 500 });
+  const mapLimiter = new Bottleneck({ maxConcurrent: 3, minTime: 100 });
+  $scope.data = {};
 
   DynamicConfig.configReady().then((configObj) => {
     // Logger.log("Resolved UI_CONFIG_READY promise in infinite_scroll_list.js, filling in templates");
     $scope.$apply(() => {
       $scope.ui_config = configObj;
-      $scope.placeNotesConfig = configObj.survey_info.buttons['place-notes'];
-      $scope.tripNotesConfig = configObj.survey_info.buttons['trip-notes'];
+      const surveyOptKey = configObj.survey_info['trip-labels'];
+      $scope.surveyOpt = SurveyOptions[surveyOptKey];
+      console.log('surveyOpt in infinite_scroll_list.js is', $scope.surveyOpt);
+      $scope.showPlaces = configObj.survey_info?.buttons?.['place-notes'];
+
       // if we decide to load dynamic surveys by surveys.json instead of config, this will need to be updated
       $scope.surveys = configObj.survey_info.surveys;
     });
+    $scope.initFilters();
   })
-  
-  const DEFAULT_ITEM_HT = 274;
-  $scope.surveyOpt = SurveyOptions.MULTILABEL;
-  $scope.itemHt = DEFAULT_ITEM_HT;
-  // Add option
 
-  const placeLimiter = new Bottleneck({ maxConcurrent: 2, minTime: 500 });
-  const mapLimiter = new Bottleneck({ maxConcurrent: 3, minTime: 100 });
-  $scope.data = {};
-  $scope.tripFilterFactory = $injector.get($scope.surveyOpt.filter);
-  $scope.filterInputs = $scope.tripFilterFactory.configuredFilters;
-
-  $scope.labelPopulateFactory = $injector.get($scope.surveyOpt.service);
+  $scope.initFilters = function() {
+    $scope.tripFilterFactory = $injector.get($scope.surveyOpt.filter);
+    $scope.filterInputs = $scope.tripFilterFactory.configuredFilters;
+    $scope.labelPopulateFactory = $injector.get($scope.surveyOpt.service);
+    $scope.filterInputs.forEach((f) => {
+      f.state = false;
+    });
+    $scope.filterInputs[0].state = true;
+    ClientStats.addReading(ClientStats.getStatKeys().LABEL_TAB_SWITCH, {"source": null, "dest": $scope.getActiveFilters()});
+    $scope.allTrips = false;
+  }
 
   $scope.getTripHeight = function(trip) {
-    let height = trip.INPUTS[2] ? 438 : 384;
-    if ($scope.placeNotesConfig) {
+    let height = trip.INPUTS?.[2] ? 438 : 384;
+    if ($scope.showPlaces) {
       height += 120;
     }
     return height;
@@ -72,12 +82,7 @@ angular.module('emission.main.diary.infscrolllist',['ui-leaflet',
   $scope.getActiveFilters = function() {
     return $scope.filterInputs.filter(sf => sf.state).map(sf => sf.key);
   }
-  $scope.filterInputs.forEach((f) => {
-    f.state = false;
-  });
-  $scope.filterInputs[0].state = true;
-  ClientStats.addReading(ClientStats.getStatKeys().LABEL_TAB_SWITCH, {"source": null, "dest": $scope.getActiveFilters()});
-  $scope.allTrips = false;
+  
   const ONE_WEEK = 7 * 24 * 60 * 60; // seconds
   const ONE_DAY = 24 * 60 * 60; // seconds
 

www/js/diary/infinite_scroll_place_item.js
@@ -17,21 +17,23 @@ angular.module('emission.main.diary.infscrollplaceitem',
         'emission.stats.clientstats',
         'emission.survey.enketo.add-note-button',])
 
-.directive("infiniteScrollPlaceItem", function(){
-    return{
+  .directive("infiniteScrollPlaceItem", function () {
+    return {
       restrict: 'E',
       scope: {
         trip: '=',
-        notesConfig: '=',
-        surveys:  '=',
+        config: '=',
+        surveys: '=',
       },
-      // controller: 'PlaceItemCtrl',
+      controller: 'PlaceItemCtrl',
       templateUrl: 'templates/diary/place_list_item.html'
     };
   })
 
-// controller is not needed for now
+  .controller("PlaceItemCtrl", function ($scope, $state, DynamicConfig) {
+    console.log("Place Item Controller called");
 
-//   .controller("PlaceItemCtrl", function($scope, $state, DynamicConfig) {
-//     console.log("Place Item Controller called");
-// });
+    console.log('config is ', $scope.config);
+
+    $scope.configPlaceNotes = () => $scope.config?.survey_info?.buttons?.['place-notes'];
+  });

www/js/diary/infinite_scroll_trip_item.js
@@ -22,7 +22,7 @@ angular.module('emission.main.diary.infscrolltripitem',
       restrict: 'E',
       scope: {
         trip: '=',
-        notesConfig: '=', // if notesConfig is not defined, then we don't show 'add note button'
+        config: '=',
         surveys: '=',
         mapLimiter: '='
       },
@@ -39,8 +39,12 @@ angular.module('emission.main.diary.infscrolltripitem',
     console.log("Trip Item Controller called");
 
     const DEFAULT_ITEM_HT = 274;
-    $scope.surveyOpt = SurveyOptions.MULTILABEL;
-    $scope.itemHt = DEFAULT_ITEM_HT;
+    const surveyOptKey = $scope.config?.survey_info?.['trip-labels'];
+    $scope.surveyOpt = SurveyOptions[surveyOptKey];
+    console.log('surveyOpt in infinite_scroll_trip_item.js is', $scope.surveyOpt);
+    
+     // if trip-notes is not present, then we won't show 'add note button'
+    $scope.configTripNotes = () => $scope.config?.survey_info?.buttons?.['trip-notes'];
 
     // Added function from infiniteScrollListCtrl
     $scope.showDetail = function($event) {
@@ -62,7 +66,7 @@ angular.module('emission.main.diary.infscrolltripitem',
     }
 
     $scope.getMapHeight = function() {
-      return $scope.notesConfig ? '80%' : '100%';
+      return $scope.configTripNotes() ? '80%' : '100%';
     }
 
     // In-Line Map, functionality pulled from Infinite Scroll Detail

www/js/diary/list.js
@@ -28,16 +28,23 @@ angular.module('emission.main.diary.list',['ui-leaflet',
                                     $timeout,
                                     ionicDatePicker,
                                     leafletData, Timeline, CommonGraph, DiaryHelper,
-                                    SurveyOptions,
-    Config, ImperialConfig, PostTripManualMarker, nzTour, KVStore, Logger, UnifiedDataLoader, $ionicPopover, $translate) {
+                                    SurveyOptions, Config, ImperialConfig, DynamicConfig,
+                                    PostTripManualMarker, nzTour, KVStore, Logger,
+                                    UnifiedDataLoader, $ionicPopover, $translate) {
   console.log("controller DiaryListCtrl called");
+
   const DEFAULT_ITEM_HT = 335;
-  $scope.surveyOpt = SurveyOptions.MULTILABEL;
   ClientStats.addReading(ClientStats.getStatKeys().LABEL_TAB_SWITCH,
     {"source": null, "dest": $scope.data? $scope.data.currDay : undefined});
-  // Add option
-  $scope.labelPopulateFactory = $injector.get($scope.surveyOpt.service);
   $scope.itemHt = DEFAULT_ITEM_HT;
+
+  DynamicConfig.configReady().then((configObj) => {
+    $scope.$apply(() => $scope.ui_config = configObj);
+    const surveyOptKey = configObj.survey_info['trip-labels'];
+    $scope.surveyOpt = SurveyOptions[surveyOptKey];
+    console.log('surveyOpt in list.js is', $scope.surveyOpt);
+    $scope.labelPopulateFactory = $injector.get($scope.surveyOpt.service);
+  })
 
   var readAndUpdateForDay = function(day) {
     // This just launches the update. The update can complete in the background

commit f9df726bd31f73fb07cb92cab3466f8ff0c0d7fe

Initizalize survey configs only after the survey config is read from the dynamic config
This fixes https://github.com/e-mission/e-mission-docs/issues/874
regression introduced in
https://github.com/e-mission/e-mission-phone/commit/c9fc41fc40c62a9f1376b776a9e612064b0f7793

www/js/diary/detail.js
@@ -19,11 +19,10 @@ angular.module('emission.main.diary.detail',['ui-leaflet', 'ng-walkthrough',
       const surveyOptKey = configObj.survey_info['trip-labels'];
       $scope.surveyOpt = SurveyOptions[surveyOptKey];
       console.log('surveyOpt in details.js is', $scope.surveyOpt);
+      $scope.tripFilterFactory = $injector.get($scope.surveyOpt.filter);
+      $scope.filterInputs = $scope.tripFilterFactory.configuredFilters;
     });
   });
-
-  $scope.tripFilterFactory = $injector.get($scope.surveyOpt.filter);
-  $scope.filterInputs = $scope.tripFilterFactory.configuredFilters;
 
   $scope.mapCtrl = {};
   angular.extend($scope.mapCtrl, {