e-mission/e-mission-phone

commit 566a64604a57077635fe90e0e4358261b88b9844

use callback to nextEntry instead of direct reference
nextEntry is needed for input matching in specific cases. This was being put in the trip/place object, but caused deeply-nested references between trips/places, their next entry, that entry's next entry, etc...
To remedy this, we will instead use a callback function getNextEntry() that can be called in the scenarios it is needed.

www/js/survey/multilabel/multi-label-ui.js
@@ -325,7 +325,7 @@ angular.module('emission.survey.multilabel.buttons',
         // console.log("Inferred labels from server: "+JSON.stringify(trip.inferred_labels));
         trip.userInput = {};
         ConfirmHelper.INPUTS.forEach(function(item, index) {
-            mls.populateManualInputs(trip, trip.nextEntry, item,
+            mls.populateManualInputs(trip, trip.getNextEntry(), item,
                 manualResultMap[item]);
         });
         trip.finalInference = {};

www/js/diary/infinite_scroll_list.js
@@ -155,13 +155,13 @@ angular.module('emission.main.diary.infscrolllist',['ui-leaflet',
           
           if ($scope.showPlaces && ct.confirmed_place) {
             const cp = ct.confirmed_place;
-            cp.nextEntry = ctList[i + 1];
+            cp.getNextEntry = () => ctList[i + 1];
             $scope.populateBasicClasses(cp);
             $scope.labelPopulateFactory.populateInputsAndInferences(cp, $scope.data.manualResultMap);
             $scope.enbs.populateInputsAndInferences(cp, $scope.data.enbsResultMap);
-            ct.nextEntry = cp;
+            ct.getNextEntry = () => cp;
           } else {
-            ct.nextEntry = ctList[i + 1];
+            ct.getNextEntry = () => ctList[i + 1];
           }
           $scope.populateBasicClasses(ct);
           $scope.labelPopulateFactory.populateInputsAndInferences(ct, $scope.data.manualResultMap);

www/js/survey/enketo/enketo-add-note-button.js
@@ -153,8 +153,7 @@ angular.module('emission.survey.enketo.add-note-button',
     if (angular.isDefined(timelineEntry)) {
         // initialize additions array as empty if it doesn't already exist
         timelineEntry.additionsList ||= [];
-        enbs.populateManualInputs(timelineEntry, timelineEntry.nextEntry, enbs.SINGLE_KEY,
-            manualResultMap[enbs.SINGLE_KEY]);
+        enbs.populateManualInputs(timelineEntry, enbs.SINGLE_KEY, manualResultMap[enbs.SINGLE_KEY]);
         timelineEntry.finalInference = {};
     } else {
         console.log("timelineEntry information not yet bound, skipping fill");
@@ -166,7 +165,7 @@ angular.module('emission.survey.enketo.add-note-button',
    * This is the version that is called from the list, which focuses only on
    * manual inputs. It also sets some additional values 
    */
-  enbs.populateManualInputs = function (timelineEntry, nextTimelineEntry, inputType, inputList) {
+  enbs.populateManualInputs = function (timelineEntry, inputType, inputList) {
       // Check unprocessed labels first since they are more recent
       const unprocessedLabelEntry = InputMatcher.getAdditionsForTimelineEntry(timelineEntry, inputList);
       var userInputEntry = unprocessedLabelEntry;

www/js/survey/input-matcher.js
@@ -72,8 +72,9 @@ angular.module('emission.survey.inputmatcher', ['emission.plugin.logger'])
         var endChecks = (userInput.data.end_ts <= entryEnd ||
             (userInput.data.end_ts - entryEnd) <= 15 * 60);
         if (startChecks && !endChecks) {
-            if (tlEntry.nextEntry) {
-            nextEntryEnd = tlEntry.nextEntry.end_ts || tlEntry.nextEntry.exit_ts;
+            const nextEntryObj = tlEntry.getNextEntry();
+            if (nextEntryObj) {
+            nextEntryEnd = nextEntryObj.end_ts || nextEntryObj.exit_ts;
             if (!nextEntryEnd) { // the last place will not have an exit_ts
                 endChecks = true; // so we will just skip the end check
             } else {

commit 0f48124afa9f3bf2435ba8d0f10a892b2e2e140a

Continue using `trip.nextTrip` in the diary since it does not use composite trips
This fixes
https://github.com/e-mission/e-mission-docs/issues/873
a regression introduced in
https://github.com/e-mission/e-mission-phone/commit/566a64604a57077635fe90e0e4358261b88b9844

Testing done:
- Labels are displayed

www/js/survey/multilabel/multi-label-ui.js
@@ -325,7 +325,7 @@ angular.module('emission.survey.multilabel.buttons',
         // console.log("Inferred labels from server: "+JSON.stringify(trip.inferred_labels));
         trip.userInput = {};
         ConfirmHelper.INPUTS.forEach(function(item, index) {
-            mls.populateManualInputs(trip, trip.getNextEntry(), item,
+            mls.populateManualInputs(trip, trip.nextTrip, item,
                 manualResultMap[item]);
         });
         trip.finalInference = {};