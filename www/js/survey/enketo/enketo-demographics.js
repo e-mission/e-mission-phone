12b0aba0fd0d6dafe9aa2c06b9b4a0173edfae2e
K. Shankari <shankari@eecs.berkeley.edu>
2023-04-04 20:36:11 -0700

    Display a loading screen while waiting for the prior survey to load
    - Set and unset a `loading` scope variable
    - Display a loading message if the loading variable is set and the survey if not
    - Add an internationalizable message indicating that the data is loading
    
    Testing done:
    - when the user clicks on the "next" button in the save token quickly, the loading screen is displayed
    - when the user takes some time to click on the "next" button, the loading screen is not displayed

--- www/js/survey/enketo/enketo-demographics.js
+++ www/js/survey/enketo/enketo-demographics.js
@@ -86,6 +86,7 @@ angular.module('emission.survey.enketo.demographics',
   }
 
   $scope.initForm = function() {
+    $scope.loading = true;
     return EnketoDemographicsService.loadPriorDemographicSurvey().then((lastSurvey) => {
         $scope.$apply(() => $scope.existingSurvey = lastSurvey);
         console.log("ENKETO: existing survey ", $scope.existingSurvey);
@@ -105,6 +106,8 @@ angular.module('emission.survey.enketo.demographics',
                 console.log("demographic survey result ", result);
               }).catch(e => console.trace(e));
         }
+    }).finally(() => {
+        $scope.$apply(() => {$scope.loading = false;});
     });
   };
 
@@ -117,7 +120,7 @@ angular.module('emission.survey.enketo.demographics',
 
   $ionicPlatform.ready(() => $scope.init());
 })
-.factory("EnketoDemographicsService", function(UnifiedDataLoader, $window) {
+.factory("EnketoDemographicsService", function(UnifiedDataLoader, $window, $ionicLoading, $translate) {
   var eds = {};
   console.log("Creating EnketoDemographicsService");
   eds.key = "manual/demographic_survey";
@@ -138,7 +141,7 @@ angular.module('emission.survey.enketo.demographics',
   eds.loadPriorDemographicSurvey = function() {
     const tq = $window.cordova.plugins.BEMUserCache.getAllTimeQuery();
     return UnifiedDataLoader.getUnifiedMessagesForInterval(eds.key, tq)
-        .then(answers => _getMostRecent(answers));
+        .then(answers => _getMostRecent(answers))
   }
 
   return eds;
