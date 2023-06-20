5db9dd8e0e08102d578aecf54154a9dff5900f95
Abby Wheelis <abby.wheelis@centre.edu>
2023-06-20 11:34:48 -0600

    text working on sample list item
    after I learned how to pass strings, I was able to render the row as a list item, which will hopefully eventually be good for showing everything as a list, and can have icons built in
    
    Icons and passing through functionality are next

--- www/js/control/SettingRow.jsx
+++ www/js/control/SettingRow.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import { angularize } from "../angular-react-helper";
-import { Text } from 'react-native-paper';
+import { List } from 'react-native-paper';
 import { useTranslation } from "react-i18next";
-import { string, func, object} from "prop-types";
-
-/*
-biggest struggle here is how to pass i18n keys as parameters (or strings in general)
-*/
+import { string, func} from "prop-types";
 
 const SettingRow = ({textKey, iconName}) => {
     const { t } = useTranslation(); //this accesses the translations
-    console.log("row created!!!!!", "translate hard-code" + t('general-settings.are-you-sure') + "translate via param" + t(textKey)+ " and the icon (plain string) " + iconName);
-    //from the above print statement the textKey is "0" and iconName is "undefined" but the hard-coded translation key works great!
+    // console.log("row created!!!!!", "translate hard-code" + t('general-settings.are-you-sure') + "translate via param" + t(textKey)+ " and the icon (plain string) " + iconName);
+    
     return (
-        <Text variant="headlineSmall">
-        {"translated text" + t(textKey)} 
-        </Text>
+       <List.Item
+       title = {t(textKey)}
+       description = {iconName}
+       ></List.Item>
     );
 };
 SettingRow.propTypes = {
