ebeca540dd0fab3529313910495a9d45b6349644
Abby Wheelis <abby.wheelis@centre.edu>
2023-06-26 12:27:01 -0600

    introduce toggle switches
    the two items that had switches are now interactive again, there is work to be done with getting the switches to have the proper starting value
    
    toggle switch: https://callstack.github.io/react-native-paper/docs/components/Switch/

--- www/js/control/SettingRow.jsx
+++ www/js/control/SettingRow.jsx
@@ -1,29 +1,63 @@
 import React from "react";
 import { angularize} from "../angular-react-helper";
-import { List, IconButton} from 'react-native-paper';
+import { List, IconButton, Switch} from 'react-native-paper';
 import { useTranslation } from "react-i18next";
-import { string, func} from "prop-types";
+import { string, func, bool} from "prop-types";
 
-const SettingRow = ({textKey, iconName, action}) => {
+const ToggleSwitch = ({onSwitch, getVal}) => {
+    // const startResult = getVal();
+    const [isSwitchOn, setIsSwitchOn] = React.useState(true);
+  
+    const onToggleSwitch = function() {
+        onSwitch();
+        setIsSwitchOn(!isSwitchOn);
+    };
+  
+    return <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />;
+};
+ToggleSwitch.propTypes = {
+    onSwitch: func,
+    startVal: func
+}
+
+const SettingRow = ({textKey, iconName, action, isToggle, toggleVal}) => {
     const { t } = useTranslation(); //this accesses the translations
-
-    return (
-          <List.Item
+    if(!isToggle)
+    {
+        return (
+            <List.Item
             title={t(textKey)}
             onPress={() => console.log("empty")}
             right={() => (
-              <IconButton
+                <IconButton
                 icon={iconName}
                 onPress={(e) => action(e)}
-              />
+                />
             )}
-          />
-      );
+            />
+        );
+    }
+   else{
+        return (
+            <List.Item
+            title={t(textKey)}
+            onPress={() => console.log("empty")}
+            right={() => (
+                <ToggleSwitch
+                onSwitch={action}
+                getVal={toggleVal}
+                />
+            )}
+            />
+        );
+    }
 };
 SettingRow.propTypes = {
     textKey: string,
     iconName: string,
-    action: func
+    action: func,
+    isToggle: bool,
+    toggleVal: func
 }
 
 angularize(SettingRow, 'emission.main.control.settingRow'); 
