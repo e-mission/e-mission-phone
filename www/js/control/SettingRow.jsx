e8b7b19a3e90230206063b66b888ffc1032ced9c
Abby Wheelis <abby.wheelis@centre.edu>
2023-06-26 10:22:39 -0600

    convert to use of IconButtons
    now the button is what has action, not the entire row - there's a bug requiring an empty onPress in the list Item for the one in IconButton to work see issue https://github.com/callstack/react-native-paper/issues/3898 and hopefully soon to be fix  https://github.com/callstack/react-native-paper/pull/3908

--- www/js/control/SettingRow.jsx
+++ www/js/control/SettingRow.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { angularize} from "../angular-react-helper";
-import { List } from 'react-native-paper';
+import { List, IconButton} from 'react-native-paper';
 import { useTranslation } from "react-i18next";
 import { string, func} from "prop-types";
 
@@ -8,13 +8,17 @@ const SettingRow = ({textKey, iconName, action}) => {
     const { t } = useTranslation(); //this accesses the translations
 
     return (
-       <List.Item
-       title = {t(textKey)}
-       onPress = {(e) => action(e)}
-       right={props => <List.Icon {...props} icon={iconName} />}
-       >
-       </List.Item>
-    );
+          <List.Item
+            title={t(textKey)}
+            onPress={() => console.log("empty")}
+            right={() => (
+              <IconButton
+                icon={iconName}
+                onPress={(e) => action(e)}
+              />
+            )}
+          />
+      );
 };
 SettingRow.propTypes = {
     textKey: string,
