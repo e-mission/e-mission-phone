b4c70b318f4a8828408f51dee526bc4eba869883
Abby Wheelis <abby.wheelis@centre.edu>
2023-06-27 17:24:42 -0600

    different method of passing info to Profile Settings

--- www/js/control/ProfileSettings.jsx
+++ www/js/control/ProfileSettings.jsx
@@ -1,37 +1,49 @@
 import React from "react";
 import { angularize, getAngularService, createScopeWithVars } from "../angular-react-helper";
-import { List, IconButton, Switch} from 'react-native-paper';
-import { useTranslation } from "react-i18next";
 import { object } from "prop-types";
 import ExpansionSection from "./ExpandMenu";
 import SettingRow from "./SettingRow";
 import ControlDataTable from "./ControlDataTable";
+import DemographicsSettingRow from "./DemographicsSettingRow";
 
-const ProfileSettings = ({tempFuncBundle}) => {
+const ProfileSettings = ({ settingsScope }) => {
+
+    // settingsScope is the $scope of general-settings.js
+    // grab any variables or functions you need from it like this:
+    const { logOut, viewPrivacyPolicy, viewQRCode, userStartStopTracking, 
+        fixAppStatus, toggleLowAccuracy, changeCarbonDataset,
+        forceSync, share, openDatePicker, uploadLog, emailLog,
+        eraseUserData, userData, carbonString, settings } = settingsScope;
 
     return (
         <>
-           <SettingRow textKey={tempFuncBundle.opcode} iconName='logout' action={tempFuncBundle.logOut}></SettingRow>
-           <SettingRow textKey='control.view-privacy' iconName='eye' action={tempFuncBundle.viewPrivacyPolicy}></SettingRow>
-           <SettingRow textKey="control.view-qrc" iconName="grid" action={tempFuncBundle.viewQRCode}></SettingRow>
+           {/* <SettingRow textKey={settings.auth.opcode} iconName='logout' action={logOut}></SettingRow> */}
+           <DemographicsSettingRow></DemographicsSettingRow>
+           <SettingRow textKey='control.view-privacy' iconName='eye' action={viewPrivacyPolicy}></SettingRow>
+           <SettingRow textKey="control.view-qrc" iconName="grid" action={viewQRCode}></SettingRow>
            {/* this toggle only kinda works */}
-           <SettingRow textKey="control.tracking" action={tempFuncBundle.userStartStopTracking} isToggle={true}></SettingRow>
-           <SettingRow textKey="control.app-status" iconName="check" action={tempFuncBundle.fixAppStatus}></SettingRow>
+           <SettingRow textKey="control.tracking" action={userStartStopTracking} isToggle={true}></SettingRow>
+           <SettingRow textKey="control.app-status" iconName="check" action={fixAppStatus}></SettingRow>
            {/* this switch is also a troublemaker (it's just not fully implemented well yet) */}
-           <SettingRow textKey="control.medium-accuracy" action={tempFuncBundle.toggleLowAccuracy} isToggle={true}></SettingRow>
-           <SettingRow textKey={tempFuncBundle.carbonString} iconName="database-cog" action={tempFuncBundle.changeCarbonDataset}></SettingRow>
-           <SettingRow textKey="control.force-sync" iconName="sync" action={tempFuncBundle.forceSync}></SettingRow>
-           <SettingRow textKey="control.share" iconName="share" action={tempFuncBundle.share}></SettingRow>
-           <SettingRow textKey="control.download-json-dump" iconName="calendar" action={tempFuncBundle.openDatePicker}></SettingRow>
+           <SettingRow textKey="control.medium-accuracy" action={toggleLowAccuracy} isToggle={true}></SettingRow>
+           <SettingRow textKey={carbonString} iconName="database-cog" action={changeCarbonDataset}></SettingRow>
+           <SettingRow textKey="control.force-sync" iconName="sync" action={forceSync}></SettingRow>
+           <SettingRow textKey="control.share" iconName="share" action={share}></SettingRow>
+           <SettingRow textKey="control.download-json-dump" iconName="calendar" action={openDatePicker}></SettingRow>
            {/* this row missing condition!!! Should only show iff ui_config.profile_controls.support_upload == true */}
-           <SettingRow textKey="control.upload-log" iconName="cloud" action={tempFuncBundle.uploadLog}></SettingRow>
-           <SettingRow textKey="control.email-log" iconName="email" action={tempFuncBundle.emailLog}></SettingRow>
+           <SettingRow textKey="control.upload-log" iconName="cloud" action={uploadLog}></SettingRow>
+           <SettingRow textKey="control.email-log" iconName="email" action={emailLog}></SettingRow>
+
+           <ExpansionSection>
+               <SettingRow textKey="control.erase-data" iconName="delete-forever" action={eraseUserData}></SettingRow>
+               <ControlDataTable controlData={userData}></ControlDataTable>
+           </ExpansionSection>
         </>
     );
     };
  
   ProfileSettings.propTypes = {
-      tempFuncBundle: object
+      settingsScope: object
     }
    
   angularize(ProfileSettings, 'emission.main.control.profileSettings'); 
