df5da3567b76c8397fd60c83fb93dcf476eccc63
Abby Wheelis <abby.wheelis@centre.edu>
2023-06-27 17:24:58 -0600

    introduce Demographics Setting Row

---
+++ www/js/control/DemographicsSettingRow.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { angularize } from "../angular-react-helper";
+import { getAngularService } from "../angular-react-helper";
+import SettingRow from "./SettingRow";
+
+const DemographicsSettingRow = ({ }) => {
+
+  const EnketoDemographicsService = getAngularService('EnketoDemographicsService');
+  const EnketoSurveyLaunch = getAngularService('EnketoSurveyLaunch');
+  const $rootScope = getAngularService('$rootScope');
+
+  // copied from /js/survey/enketo/enketo-demographics.js
+  function openPopover() {
+    return EnketoDemographicsService.loadPriorDemographicSurvey().then((lastSurvey) => {
+      return EnketoSurveyLaunch
+        .launch($rootScope, 'UserProfileSurvey', {
+          prefilledSurveyResponse: lastSurvey?.data?.xmlResponse,
+          showBackButton: true, showFormFooterJumpNav: true
+        })
+        .then(result => {
+          console.log("demographic survey result ", result);
+        });
+    });
+  }
+
+  return <SettingRow action={openPopover} iconName="account"
+          textKey="control.edit-demographics" isToggle={false} />
+};
+
+angularize(DemographicsSettingRow, 'emission.main.control.demographicsSettingRow');
+export default DemographicsSettingRow;
