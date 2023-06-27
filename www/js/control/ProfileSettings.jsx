757936cbd7400c36528319137f8f0c43f97f2328
Abby Wheelis <abby.wheelis@centre.edu>
2023-06-27 16:48:34 -0600

    first set settings now in ProfileSettingsComponent
    creating an over-arching component for the ProfileSettings -- still passing in functions, some things have bugs/functional differences noted in comments

--- www/js/control/ProfileSettings.jsx
+++ www/js/control/ProfileSettings.jsx
@@ -1,32 +1,37 @@
 import React from "react";
-import { angularize} from "../angular-react-helper";
+import { angularize, getAngularService, createScopeWithVars } from "../angular-react-helper";
 import { List, IconButton, Switch} from 'react-native-paper';
 import { useTranslation } from "react-i18next";
-import { string, func, bool} from "prop-types";
+import { object } from "prop-types";
 import ExpansionSection from "./ExpandMenu";
 import SettingRow from "./SettingRow";
 import ControlDataTable from "./ControlDataTable";
 
+const ProfileSettings = ({tempFuncBundle}) => {
 
-const ProfileSettings = ({ }) => {
     return (
         <>
-            {/* <div ng-if="settings.notification.prefReminderTime" class="control-list-item">
-                <div class="control-icon-button" style="position: relative">
-                    <i class="ion-clock"></i>
-                    <input type="time" name="timeOfDay" value="{{settings.notification.prefReminderTimeOnLoad}}"
-                    ng-model="settings.notification.prefReminderTimeVal" ng-change="updatePrefReminderTime()" ng-blur="updatePrefReminderTime()"
-                    style="position: absolute; height: 100%; inset: 0; display: flex; opacity: 0;"></input>
-                </div>
-            </div> */}
-            <enketo-demographics-button></enketo-demographics-button>
-            <SettingRow textKey="control.view-privacy" iconName="'eye'" action = "viewPrivacyPolicy" isToggle="False"></SettingRow>
+           <SettingRow textKey={tempFuncBundle.opcode} iconName='logout' action={tempFuncBundle.logOut}></SettingRow>
+           <SettingRow textKey='control.view-privacy' iconName='eye' action={tempFuncBundle.viewPrivacyPolicy}></SettingRow>
+           <SettingRow textKey="control.view-qrc" iconName="grid" action={tempFuncBundle.viewQRCode}></SettingRow>
+           {/* this toggle only kinda works */}
+           <SettingRow textKey="control.tracking" action={tempFuncBundle.userStartStopTracking} isToggle={true}></SettingRow>
+           <SettingRow textKey="control.app-status" iconName="check" action={tempFuncBundle.fixAppStatus}></SettingRow>
+           {/* this switch is also a troublemaker (it's just not fully implemented well yet) */}
+           <SettingRow textKey="control.medium-accuracy" action={tempFuncBundle.toggleLowAccuracy} isToggle={true}></SettingRow>
+           <SettingRow textKey={tempFuncBundle.carbonString} iconName="database-cog" action={tempFuncBundle.changeCarbonDataset}></SettingRow>
+           <SettingRow textKey="control.force-sync" iconName="sync" action={tempFuncBundle.forceSync}></SettingRow>
+           <SettingRow textKey="control.share" iconName="share" action={tempFuncBundle.share}></SettingRow>
+           <SettingRow textKey="control.download-json-dump" iconName="calendar" action={tempFuncBundle.openDatePicker}></SettingRow>
+           {/* this row missing condition!!! Should only show iff ui_config.profile_controls.support_upload == true */}
+           <SettingRow textKey="control.upload-log" iconName="cloud" action={tempFuncBundle.uploadLog}></SettingRow>
+           <SettingRow textKey="control.email-log" iconName="email" action={tempFuncBundle.emailLog}></SettingRow>
         </>
     );
-  };
+    };
  
   ProfileSettings.propTypes = {
-      
+      tempFuncBundle: object
     }
    
   angularize(ProfileSettings, 'emission.main.control.profileSettings'); 
