12f462c046f6049c6c04a56c51abebc2a8bdce03
Abby Wheelis <abby.wheelis@centre.edu>
2023-06-28 17:01:04 -0600

    styling for Setting Row
    adding some styling for setting row, referenced other stylings in code and https://reactnative.dev/docs/view-style-props and https://github.com/callstack/react-native-paper/blob/master/src/components/List/ListItem.tsx

--- www/js/control/SettingRow.jsx
+++ www/js/control/SettingRow.jsx
@@ -1,26 +1,11 @@
 import React from "react";
 import { angularize} from "../angular-react-helper";
+import { StyleSheet } from 'react-native';
 import { List, IconButton, Switch} from 'react-native-paper';
 import { useTranslation } from "react-i18next";
 import { string, func, bool} from "prop-types";
 
-// const ToggleSwitch = ({onSwitch, switchVal}) => {
-//     // const startResult = getVal();
-//     const [isSwitchOn, setIsSwitchOn] = React.useState(true);
-  
-//     const onToggleSwitch = function() {
-//         onSwitch();
-//         setIsSwitchOn(!isSwitchOn);
-//     };
-  
-//     return <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />;
-// };
-// ToggleSwitch.propTypes = {
-//     onSwitch: func,
-//     startVal: func
-// }
-
-const SettingRow = ({textKey, iconName, action, switchValue}) => {
+const SettingRow = ({textKey, iconName, action, desc, switchValue}) => {
     const { t } = useTranslation(); //this accesses the translations
 
     let rightComponent;
@@ -28,21 +13,48 @@ const SettingRow = ({textKey, iconName, action, switchValue}) => {
     if (switchValue) {
         rightComponent = <Switch value={switchValue} onValueChange={(e) => action(e)}/>;
     } else {
-        rightComponent = <IconButton icon={iconName} onPress={(e) => action(e)} />;
+        rightComponent = <IconButton icon={iconName} onPress={(e) => action(e)} style={styles.icon}/>;
+    }
+    let descriptionText;
+    if(desc) {
+        descriptionText = {desc};
+    } else {
+        descriptionText = "";
     }
 
     return (
-        <List.Item
+        <List.Item 
+        style={styles.item}
         title={t(textKey)}
+        titleStyle={styles.title}
+        description={desc}
         onPress={() => console.log("empty")}
         right={() => rightComponent}
         />
     );
 };
+const styles = StyleSheet.create({
+    item:{
+        flex: .75,
+        justifyContent: 'space-between',
+        backgroundColor: '#fff',
+        padding: 5,
+        margin: 5,
+    },
+    title: {
+        fontSize: 16,
+        marginVertical: 2,
+    },
+    icon: {
+        marginVertical: 2,
+        color: "#99FF66",
+    }
+  });
 SettingRow.propTypes = {
     textKey: string,
     iconName: string,
     action: func,
+    desc: string,
     switchValue: bool
 }
 
