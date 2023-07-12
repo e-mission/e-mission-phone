d2966a15fbb1996a9a8989c6c9e33a30106cfd88
Abby Wheelis <abby.wheelis@centre.edu>
2023-07-12 11:31:07 -0600

    patch back over to reliance on ProfileSettings rather than html

--- www/js/control/general-settings.js
+++ www/js/control/general-settings.js
@@ -3,6 +3,7 @@
 import angular from 'angular';
 import ControlDataTable from './ControlDataTable';
 import QrCode from '../components/QrCode';
+import ProfileSettings from './ProfileSettings';
 
 angular.module('emission.main.control',['emission.services',
                                         'emission.i18n.utils',
@@ -21,7 +22,8 @@ angular.module('emission.main.control',['emission.services',
                                         'emission.plugin.logger',
                                         'emission.config.dynamic',
                                         QrCode.module,
-                                        ControlDataTable.module])
+                                        ControlDataTable.module,
+                                        ProfileSettings.module])
 
 .controller('ControlCtrl', function($scope, $window,
                $ionicScrollDelegate, $ionicPlatform,
