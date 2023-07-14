bfac9c83f50973c5436615aaa4bffc9f52a0197c
Jack Greenlee <greenljk@mail.uc.edu>
2023-07-13 22:50:00 -0400

    profile settings: tweak styling

--- www/js/control/ExpandMenu.jsx
+++ www/js/control/ExpandMenu.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { angularize} from "../angular-react-helper";
 import { StyleSheet } from 'react-native';
-import { List } from 'react-native-paper';
+import { List, useTheme } from 'react-native-paper';
 import { useTranslation } from "react-i18next";
 import { string } from "prop-types";
 
 const ExpansionSection = (props) => {
     const { t } = useTranslation(); //this accesses the translations
+    const { colors } = useTheme(); // use this to get the theme colors instead of hardcoded #hex colors
     const [expanded, setExpanded] = React.useState(false);
 
     const handlePress = () => setExpanded(!expanded);
 
   return (
     <List.Accordion
-      style={styles.section}
+      style={styles.section(colors.surface)}
       title={t(props.sectionTitle)}
       titleStyle={styles.title}
       expanded={expanded}
@@ -23,12 +24,11 @@ const ExpansionSection = (props) => {
   );
 };
 const styles = StyleSheet.create({
-  section:{
+  section: (surfaceColor) => ({
       justifyContent: 'space-between',
-      backgroundColor: '#fff',
-      height: 75,
-      margin: 5,
-  },
+      backgroundColor: surfaceColor,
+      margin: 1,
+  }),
   title: {
       fontSize: 16,
       marginVertical: 2,
