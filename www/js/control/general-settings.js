d4156793d9c6c30bb25bda7b116c8b809c1fc833
Abby Wheelis <abby.wheelis@centre.edu>
2023-07-19 11:50:08 -0600

    remove unused modules
    everything is now handled by ProfileSettings - so need to import react elements through general-settings!

--- www/js/control/general-settings.js
+++ www/js/control/general-settings.js
@@ -1,8 +1,6 @@
 'use strict';
 
 import angular from 'angular';
-import ControlDataTable from './ControlDataTable';
-import QrCode from '../components/QrCode';
 import ProfileSettings from './ProfileSettings';
 
 angular.module('emission.main.control',['emission.services',
@@ -21,8 +19,6 @@ angular.module('emission.main.control',['emission.services',
                                         'emission.survey.enketo.demographics',
                                         'emission.plugin.logger',
                                         'emission.config.dynamic',
-                                        QrCode.module,
-                                        ControlDataTable.module,
                                         ProfileSettings.module])
 
 .controller('ControlCtrl', function($scope, $window,
