aaf6c0601080953a9c5345680a977f9eef2dba55
Jack Greenlee <greenljk@mail.uc.edu>
2023-08-15 12:43:12 -0400

    use Dialogs for multilabel popups
    Instead of using Ionic popups and separate templates for the mode and purpose select menus, we can utilize a Dialog containing Radio buttons using components from React Native Paper.
    
    This is much better in terms of accessibility - the Ionic popups were not able to be navigated to by keyboard / screen reader controls, while the Dialog gains focus when opened because it is wrapped in a Modal.
    
    The UX for inputting 'other' has been changed as well. Instead of having 'other' entry be a separate popup, I incorporated it into the same Dialog.
    When the "Other" radio button is chosen, a text entry and Save button will appear right below it allowing the user to specify their custom label.

--- www/js/survey/multilabel/MultiLabelButtonGroup.tsx
+++ www/js/survey/multilabel/MultiLabelButtonGroup.tsx
@@ -2,26 +2,31 @@
   In the default configuration, these are the "Mode" and "Purpose" buttons.
   Next to the buttons is a small checkmark icon, which marks inferrel labels as confirmed */
 
-import React, { useContext, useEffect, useState } from "react";
-import { angularize, createScopeWithVars, getAngularService } from "../../angular-react-helper";
-import { object, number } from "prop-types";
-import { View } from "react-native";
-import { IconButton, Text, useTheme } from "react-native-paper";
+import React, { useContext, useEffect, useState, useMemo } from "react";
+import { getAngularService } from "../../angular-react-helper";
+import { View, Modal, ScrollView, Pressable, useWindowDimensions } from "react-native";
+import { IconButton, Text, Dialog, useTheme, RadioButton, Button, TextInput } from "react-native-paper";
 import DiaryButton from "../../diary/DiaryButton";
 import { useTranslation } from "react-i18next";
 import { LabelTabContext } from "../../diary/LabelTab";
+import { displayErrorMsg, logDebug } from "../../plugin/logger";
 
 const MultilabelButtonGroup = ({ trip }) => {
   const { colors } = useTheme();
   const { t } = useTranslation();
   const { repopulateTimelineEntry } = useContext(LabelTabContext);
+  const { height: windowHeight } = useWindowDimensions();
 
   const [ inputParams, setInputParams ] = useState({});
-  let closePopover;
+  // modal visible for which input type? (mode or purpose or replaced_mode, null if not visible)
+  const [ modalVisibleFor, setModalVisibleFor ] = useState<'MODE'|'PURPOSE'|'REPLACED_MODE'|null>(null);
+  const [otherLabel, setOtherLabel] = useState<string|null>(null);
+  const chosenLabel = useMemo<string>(() => {
+    if (otherLabel != null) return 'other';
+    return trip.userInput[modalVisibleFor]?.value
+  }, [modalVisibleFor, otherLabel]);
 
   const ConfirmHelper = getAngularService("ConfirmHelper");
-  const $ionicPopup = getAngularService("$ionicPopup");
-  const $ionicPopover = getAngularService("$ionicPopover");
 
   useEffect(() => {
     console.log("During initialization, trip is ", trip);
@@ -38,39 +43,22 @@ const MultilabelButtonGroup = ({ trip }) => {
     }
   }
 
-  function openPopover(e, inputType) {
-    let popoverPath = 'templates/diary/'+inputType.toLowerCase()+'-popover.html';
-    const scope = createScopeWithVars({inputParams, choose});
-    $ionicPopover.fromTemplateUrl(popoverPath, {scope}).then((pop) => {
-      closePopover = () => pop.hide();
-      pop.show(e);
-    });
+  function onChooseLabel(chosenValue) {
+    logDebug(`onChooseLabel with chosen ${modalVisibleFor} as ${chosenValue}`);
+    if (chosenValue == 'other') {
+      setOtherLabel('');
+    } else {
+      store(modalVisibleFor, chosenValue, false);
+    }
   }
 
-  function choose(inputType, chosenLabel, wasOther=false) {
-    if (chosenLabel && chosenLabel != "other") {
-      store(inputType, chosenLabel, wasOther);
-    } else {
-      const scope = createScopeWithVars({other: {text: ''}});
-      $ionicPopup.show({
-        scope,
-        title: t("trip-confirm.services-please-fill-in", { text: inputType.toLowerCase() }),
-        template: '<input type="text" ng-model="other.text">',
-        buttons: [
-          {
-            text: t('trip-confirm.services-cancel')
-          },
-          {
-            text: `<b>${t('trip-confirm.services-save')}</b>`,
-            type: 'button-positive',
-            onTap: () => choose(inputType, scope.other.text, true)
-          }
-        ]
-      });
-    }
-  };
+  function dismiss() {
+    setModalVisibleFor(null);
+    setOtherLabel(null);
+  }
 
   function store(inputType, chosenLabel, isOther) {
+    if (!chosenLabel) return displayErrorMsg("Label is empty");
     if (isOther) {
       /* Let's make the value for user entered inputs look consistent with our other values
        (i.e. lowercase, and with underscores instead of spaces) */
@@ -84,14 +72,14 @@ const MultilabelButtonGroup = ({ trip }) => {
 
     const storageKey = ConfirmHelper.inputDetails[inputType].key;
     window['cordova'].plugins.BEMUserCache.putMessage(storageKey, inputDataToStore).then(() => {
-      closePopover?.();
+      dismiss();
       repopulateTimelineEntry(trip._id.$oid);
-      console.debug("Successfully stored input data "+JSON.stringify(inputDataToStore));
+      logDebug("Successfully stored input data "+JSON.stringify(inputDataToStore));
     });
   }
 
   const inputKeys = Object.keys(trip.inputDetails);
-  return (
+  return (<>
     <View style={{flexDirection: 'row', alignItems: 'center'}}>
       <View style={{width: 'calc(100% - 20px)'}}>
         {inputKeys.map((key, i) => {
@@ -110,7 +98,7 @@ const MultilabelButtonGroup = ({ trip }) => {
             <View key={i}>
               <Text>{t(input.labeltext)}</Text>
               <DiaryButton text={t(btnText)}
-                onPress={(e) => openPopover(e, input.name)}
+                onPress={(e) => setModalVisibleFor(input.name)}
                 fillColor={fillColor} />
             </View>
           )
@@ -122,13 +110,37 @@ const MultilabelButtonGroup = ({ trip }) => {
                     style={{width: 20, height: 20, margin: 3}}/>
       </View>
     </View>
-  );
+    <Modal visible={modalVisibleFor} transparent={true} onDismiss={() => dismiss()}>
+      <Dialog visible={modalVisibleFor} onDismiss={() => dismiss()}>
+        <Pressable>
+          <Dialog.Title style={{elevation: 2}}>
+            {(modalVisibleFor == 'MODE') && t('diary.select-mode-scroll') ||
+              (modalVisibleFor == 'PURPOSE') && t('diary.select-purpose-scroll') ||
+              (modalVisibleFor == 'REPLACED_MODE') && t('diary.select-replaced-mode-scroll')}
+          </Dialog.Title>
+          <Dialog.Content style={{maxHeight: windowHeight/2, paddingBottom: 0}}>
+            <ScrollView style={{paddingBottom: 24}}>
+              <RadioButton.Group onValueChange={val => onChooseLabel(val)} value={chosenLabel}>
+                {inputParams[modalVisibleFor]?.options?.map((o, i) => (
+                  // @ts-ignore
+                  <RadioButton.Item key={i} label={t(o.text)} value={o.value} style={{paddingVertical: 2}} />
+                ))}
+              </RadioButton.Group>
+            </ScrollView>
+          </Dialog.Content>
+          {otherLabel != null && <>
+            <TextInput label={t('trip-confirm.services-please-fill-in', { text: modalVisibleFor?.toLowerCase() })}
+              value={otherLabel || ''} onChangeText={(t) => setOtherLabel(t)} />
+            <Dialog.Actions>
+              <Button onPress={() => store(modalVisibleFor, otherLabel, true)}>
+                {t('trip-confirm.services-save')}
+              </Button>
+            </Dialog.Actions>
+          </>}
+        </Pressable>
+      </Dialog>
+    </Modal>
+  </>);
 };
 
-MultilabelButtonGroup.propTypes = {
-  trip: object,
-  recomputeDelay: number,
-}
-
-angularize(MultilabelButtonGroup, 'MultilabelButtonGroup', 'emission.main.diary.multilabelbtngroup');
 export default MultilabelButtonGroup;
