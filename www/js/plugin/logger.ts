374553597cd51b545d8ec6f8b32e60961938ff89
Jack Greenlee <greenljk@mail.uc.edu>
2023-08-14 23:36:10 -0400

    add more levels to logger, also add types

--- www/js/plugin/logger.ts
+++ www/js/plugin/logger.ts
@@ -23,16 +23,21 @@ angular.module('emission.plugin.logger', [])
     return loggerJs;
 });
 
-export function log(message) {
+export const logDebug = (message: string) =>
   window['Logger'].log(window['Logger'].LEVEL_DEBUG, message);
-}
 
-export function displayError(error, title?) {
+export const logInfo = (message: string) =>
+  window['Logger'].log(window['Logger'].LEVEL_INFO, message);
+
+export const logWarn = (message: string) =>
+  window['Logger'].log(window['Logger'].LEVEL_WARN, message);
+
+export function displayError(error: Error, title?: string) {
   const errorMsg = error.message ? error.message + '\n' + error.stack : JSON.stringify(error);
   displayErrorMsg(errorMsg, title);
 }
 
-export function displayErrorMsg(errorMsg, title?) {
+export function displayErrorMsg(errorMsg: string, title?: string) {
   // Check for OPcode 'Does Not Exist' errors and prepend the title with "Invalid OPcode"
   if (errorMsg.includes?.("403")) {
     title = "Invalid OPcode: " + (title || '');
