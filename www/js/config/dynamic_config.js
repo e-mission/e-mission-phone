1bf3312357ebcc8f997aacf25b0c580ffa65f928
Abby Wheelis <abby.wheelis@centre.edu>
2023-08-19 10:41:28 -0600

    restore "error-loading" key
    There was one key that I eliminated because I could not find it used, it turns out the wrong key was being referenced so I restored the key and corrected the reference here

--- www/js/config/dynamic_config.js
+++ www/js/config/dynamic_config.js
@@ -296,7 +296,7 @@ angular.module('emission.config.dynamic', ['emission.plugin.logger',
                 $rootScope.$apply(() => dc.saveAndNotifyConfigReady(existingConfig));
             }
         }).catch((err) => {
-            Logger.displayError(i18next.t('config.loading-config-app-start', err))
+            Logger.displayError(i18next.t('config.error-loading-config-app-start', err))
         });
     };
     $ionicPlatform.ready().then(function() {
