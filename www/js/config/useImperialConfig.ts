364d1c4798198c89cec3a5b95468137641baa9cc
Jack Greenlee <greenljk@mail.uc.edu>
2023-08-20 15:49:32 -0400

    unify imperial miles/km functions
    and add comment describing what it does

--- www/js/config/useImperialConfig.ts
+++ www/js/config/useImperialConfig.ts
@@ -1,22 +1,33 @@
 import React, { useEffect, useState } from "react";
 import useAppConfig from "../useAppConfig";
 
-const getFormattedDistanceInKm = (dist_in_meters) => {
-  if (dist_in_meters >= 1000)
-    return Number.parseFloat((dist_in_meters / 1000).toFixed(0));
-  return Number.parseFloat((dist_in_meters / 1000).toPrecision(3));
+const KM_TO_MILES = 0.621371;
+/* formatting distances for display:
+  - if distance >= 100, round to the nearest integer
+   e.g. "105 mi", "167 km"
+  - if 1 <= distance < 100, round to 3 significant digits
+    e.g. "7.02 mi", "11.3 km"
+  - if distance < 1, round to 2 significant digits
+    e.g. "0.47 mi", "0.75 km" */
+const formatDistance = (dist: number) => {
+  if (dist < 1)
+    return dist.toPrecision(2);
+  if (dist < 100)
+    return dist.toPrecision(3);
+  return Math.round(dist).toString();
 }
 
-const getFormattedDistanceInMiles = (dist_in_meters) =>
-  Number.parseFloat((KM_TO_MILES * getFormattedDistanceInKm(dist_in_meters)).toFixed(1));
+const getFormattedDistanceInKm = (distInMeters: string) =>
+  formatDistance(Number.parseFloat(distInMeters) / 1000);
+
+const getFormattedDistanceInMiles = (distInMeters: string) =>
+  formatDistance((Number.parseFloat(distInMeters) / 1000) * KM_TO_MILES);
 
 const getKmph = (metersPerSec) =>
   (metersPerSec * 3.6).toFixed(2);
 
 const getMph = (metersPerSecond) =>
   (KM_TO_MILES * Number.parseFloat(getKmph(metersPerSecond))).toFixed(2);
-
-const KM_TO_MILES = 0.621371;
 
 export function useImperialConfig() {
   const { appConfig, loading } = useAppConfig();
