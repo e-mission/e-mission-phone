d074cab770b3e1cb216ccf37a1135961ffb7f209
Jack Greenlee <greenljk@mail.uc.edu>
2023-08-24 16:35:54 -0400

    update BarChart for categorical data & flat inputs
    We have been showing time-based data on the BarChart, where one of the axes (depending if horizontal or vertical) has values as Unix timestamps.
    
    We'd like this component to also work with categorical data, so we will have a boolean prop `timeAxis` to say whether it should be timestamps (true) or categories (false).
    
    Also, the way BarChart receives input data has been changed. ChartJS accepts input, categorized by 'datasets' which have their own label. For us, this correlates to the different labeled modes.
    
    However, the metrics we get from the server lend themselves better to a 'flattened' structure - we receive an array of days, and what labels were for that day; we don't receive an array of labels with what their value was on each day.
    
    So we would have to flip this data around in each place that the BarChart was used. Instead, it's simpler for BarChart to accept a flattened structure - and we can do the flipping here in BarChart before the flipped data is fed into ChartJS.
    
    And to better keep track of all this, let's introduce typings for the input, flat 'records' and for the resulting, flipped 'chartData'.

--- www/js/components/BarChart.tsx
+++ www/js/components/BarChart.tsx
@@ -1,10 +1,9 @@
 
-import React, { useRef, useState } from 'react';
-import { array, string, bool } from 'prop-types';
+import React, { useRef, useState, useMemo } from 'react';
 import { angularize } from '../angular-react-helper';
 import { View } from 'react-native';
 import { useTheme } from 'react-native-paper';
-import { Chart, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, TimeScale } from 'chart.js';
+import { Chart, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, TimeScale, ChartData } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 import Annotation, { AnnotationOptions } from 'chartjs-plugin-annotation';
 
@@ -19,27 +18,47 @@ Chart.register(
   Annotation,
 );
 
-const BarChart = ({ chartData, axisTitle, lineAnnotations=null, isHorizontal=false }) => {
+type BarChartData = {
+  label: string,
+  data: { x: number|string, y: number|string }[],
+}[];
+
+type Props = {
+  records: { label: string, x: number|string, y: number|string }[],
+  axisTitle: string,
+  lineAnnotations?: { value: number, label: string }[],
+  isHorizontal?: boolean,
+  timeAxis?: boolean,
+}
+const BarChart = ({ records, axisTitle, lineAnnotations=null, isHorizontal=false, timeAxis }: Props) => {
 
   const { colors } = useTheme();
   const [ numVisibleDatasets, setNumVisibleDatasets ] = useState(1);
 
+  const indexAxis = isHorizontal ? 'y' : 'x';
   const barChartRef = useRef<Chart>(null);
 
-  const defaultPalette = [
-    '#c95465', // red oklch(60% 0.15 14)
-    '#4a71b1', // blue oklch(55% 0.11 260)
-    '#d2824e', // orange oklch(68% 0.12 52)
-    '#856b5d', // brown oklch(55% 0.04 50)
-    '#59894f', // green oklch(58% 0.1 140)
-    '#e0cc55', // yellow oklch(84% 0.14 100)
-    '#b273ac', // purple oklch(64% 0.11 330)
-    '#f09da6', // pink oklch(78% 0.1 12)
-    '#b3aca8', // grey oklch(75% 0.01 55)
-    '#80afad', // teal oklch(72% 0.05 192)
-  ]
-
-  const indexAxis = isHorizontal ? 'y' : 'x';
+  // group records by label (this is the format that Chart.js expects)
+  const chartData = useMemo(() => {
+    return records?.reduce((acc, record) => {
+      const existing = acc.find(e => e.label == record.label);
+      if (!existing) {
+        acc.push({
+          label: record.label,
+          data: [{
+            x: record.x,
+            y: record.y,
+          }],
+        });
+      } else {
+        existing.data.push({
+          x: record.x,
+          y: record.y,
+        });
+      }
+      return acc;
+    }, [] as BarChartData);
+  }, [records]);
 
   function getChartHeight() {
     /* when horizontal charts have more data, they should get taller
@@ -47,7 +66,7 @@ const BarChart = ({ chartData, axisTitle, lineAnnotations=null, isHorizontal=fal
     if (isHorizontal) {
       // 'ideal' chart height is based on the number of datasets and number of unique index values
       const uniqueIndexVals = [];
-      chartData.forEach(e => e.records.forEach(r => {
+      chartData.forEach(e => e.data.forEach(r => {
         if (!uniqueIndexVals.includes(r[indexAxis])) uniqueIndexVals.push(r[indexAxis]);
       }));
       const numIndexVals = uniqueIndexVals.length;
@@ -60,21 +79,17 @@ const BarChart = ({ chartData, axisTitle, lineAnnotations=null, isHorizontal=fal
       // return whichever is greater
       return { height: Math.max(idealChartHeight, minChartHeight) };
     }
-    // vertical charts will just match the parent container
-    return { height: '100%' };
+    return {};
   }
 
   return (
     <View style={[getChartHeight()]}>
       <Bar ref={barChartRef}
-        data={{
-          datasets: chartData.map((d, i) => ({
-            label: d.label,
-            data: d.records,
-            // cycle through the default palette, repeat if necessary
-            backgroundColor: defaultPalette[i % defaultPalette.length],
-          }))
-        }}
+        data={{datasets: chartData.map((e, i) => ({
+          ...e,
+          // cycle through the default palette, repeat if necessary
+          backgroundColor: defaultPalette[i % defaultPalette.length],
+        }))}}
         options={{
           indexAxis: indexAxis,
           responsive: true,
@@ -84,14 +99,14 @@ const BarChart = ({ chartData, axisTitle, lineAnnotations=null, isHorizontal=fal
             ...(isHorizontal ? {
               y: {
                 offset: true,
-                type: 'time',
-                adapters: {
+                type: timeAxis ? 'time' : 'category',
+                adapters: timeAxis ? {
                   date: { zone: 'utc' },
-                },
-                time: {
+                } : {},
+                time: timeAxis ? {
                   unit: 'day',
                   tooltipFormat: 'DDD', // Luxon "localized date with full month": e.g. August 6, 2014
-                },
+                } : {},
                 beforeUpdate: (axis) => {
                   setNumVisibleDatasets(axis.chart.getVisibleDatasetCount())
                 },
@@ -103,14 +118,14 @@ const BarChart = ({ chartData, axisTitle, lineAnnotations=null, isHorizontal=fal
             } : {
               x: {
                 offset: true,
-                type: 'time',
-                adapters: {
+                type: timeAxis ? 'time' : 'category',
+                adapters: timeAxis ? {
                   date: { zone: 'utc' },
-                },
-                time: {
+                } : {},
+                time: timeAxis ? {
                   unit: 'day',
                   tooltipFormat: 'DDD', // Luxon "localized date with full month": e.g. August 6, 2014
-                },
+                } : {},
               },
               y: {
                 title: { display: true, text: axisTitle },
@@ -146,56 +161,19 @@ const BarChart = ({ chartData, axisTitle, lineAnnotations=null, isHorizontal=fal
   )
 }
 
-BarChart.propTypes = {
-  chartData: array,
-  axisTitle: string,
-  lineAnnotations: array,
-  isHorizontal: bool,
-};
+const defaultPalette = [
+  '#c95465', // red oklch(60% 0.15 14)
+  '#4a71b1', // blue oklch(55% 0.11 260)
+  '#d2824e', // orange oklch(68% 0.12 52)
+  '#856b5d', // brown oklch(55% 0.04 50)
+  '#59894f', // green oklch(58% 0.1 140)
+  '#e0cc55', // yellow oklch(84% 0.14 100)
+  '#b273ac', // purple oklch(64% 0.11 330)
+  '#f09da6', // pink oklch(78% 0.1 12)
+  '#b3aca8', // grey oklch(75% 0.01 55)
+  '#80afad', // teal oklch(72% 0.05 192)
+];
+
 
 angularize(BarChart, 'BarChart', 'emission.main.barchart');
 export default BarChart;
-
-// const sampleAnnotations = [
-//   { value: 35, label: 'Target1' },
-//   { value: 65, label: 'Target2' },
-// ];
-
-// const sampleChartData = [
-//   {
-//     label: 'Primary',
-//     records: [
-//       { x: moment('2023-06-20'), y: 20 },
-//       { x: moment('2023-06-21'), y: 30 },
-//       { x: moment('2023-06-23'), y: 80 },
-//       { x: moment('2023-06-24'), y: 40 },
-//     ],
-//   },
-//   {
-//     label: 'Secondary',
-//     records: [
-//       { x: moment('2023-06-21'), y: 10 },
-//       { x: moment('2023-06-22'), y: 50 },
-//       { x: moment('2023-06-23'), y: 30 },
-//       { x: moment('2023-06-25'), y: 40 },
-//     ],
-//   },
-//   {
-//     label: 'Tertiary',
-//     records: [
-//       { x: moment('2023-06-20'), y: 30 },
-//       { x: moment('2023-06-22'), y: 40 },
-//       { x: moment('2023-06-24'), y: 10 },
-//       { x: moment('2023-06-25'), y: 60 },
-//     ],
-//   },
-//   {
-//     label: 'Quaternary',
-//     records: [
-//       { x: moment('2023-06-22'), y: 10 },
-//       { x: moment('2023-06-23'), y: 20 },
-//       { x: moment('2023-06-24'), y: 30 },
-//       { x: moment('2023-06-25'), y: 40 },
-//     ],
-//   },
-// ];
