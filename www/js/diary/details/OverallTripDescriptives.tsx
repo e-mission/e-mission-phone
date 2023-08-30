d8ffec53db9b97b488977aec7bfcdccf00f19819
Jack Greenlee <greenljk@mail.uc.edu>
2023-08-30 15:00:20 -0400

    add "Overall" title to OverallTripDescriptives
    -fix padding since this adds another container

--- www/js/diary/details/OverallTripDescriptives.tsx
+++ www/js/diary/details/OverallTripDescriptives.tsx
@@ -11,32 +11,35 @@ const OverallTripDescriptives = ({ trip }) => {
   const { displayTime, formattedDistance, distanceSuffix, detectedModes } = useDerivedProperties(trip);
 
   return (
-    <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 24, marginVertical: 5 }}>
-      <View style={{ justifyContent: 'center' }}>
-        <Text style={{ fontSize: 15 }}>
-          {t('diary.distance')}
-        </Text>
-        <Text style={{ fontSize: 13, fontWeight: 'bold' }}>
-          {`${formattedDistance} ${distanceSuffix}`}
-        </Text>
-      </View>
-      <View style={{ justifyContent: 'center' }}>
-        <Text style={{ fontSize: 15 }}>
-          {t('diary.time')}
-        </Text>
-        <Text style={{ fontSize: 13, fontWeight: 'bold' }}>
-          {displayTime}
-        </Text>
-      </View>
-      <View style={{ justifyContent: 'center' }}>
-        {detectedModes?.map?.((pct, i) => (
-          <View key={i} style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Icon icon={pct.icon} size={16} iconColor={pct.color} />
-            <Text style={{ fontSize: 13, fontWeight: 'bold' }}>
-              {pct.pct}%
-            </Text>
-          </View>
-        ))}
+    <View style={{ paddingHorizontal: 10, marginVertical: 5 }}>
+      <Text variant='titleMedium'>Overall</Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <View style={{ justifyContent: 'center' }}>
+          <Text style={{ fontSize: 15 }}>
+            {t('diary.distance')}
+          </Text>
+          <Text style={{ fontSize: 13, fontWeight: 'bold' }}>
+            {`${formattedDistance} ${distanceSuffix}`}
+          </Text>
+        </View>
+        <View style={{ justifyContent: 'center' }}>
+          <Text style={{ fontSize: 15 }}>
+            {t('diary.time')}
+          </Text>
+          <Text style={{ fontSize: 13, fontWeight: 'bold' }}>
+            {displayTime}
+          </Text>
+        </View>
+        <View style={{ justifyContent: 'center' }}>
+          {detectedModes?.map?.((pct, i) => (
+            <View key={i} style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Icon icon={pct.icon} size={16} iconColor={pct.color} />
+              <Text style={{ fontSize: 13, fontWeight: 'bold' }}>
+                {pct.pct}%
+              </Text>
+            </View>
+          ))}
+        </View>
       </View>
     </View>
   );
