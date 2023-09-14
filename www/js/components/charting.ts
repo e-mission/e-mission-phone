a3264e234f6e652d1384ad2a779f3028c87ce032
Jack Greenlee <greenljk@mail.uc.edu>
2023-09-14 01:43:46 -0400

    adjust carbon meter gradient
    We will now use the same yellow-orange-red gradient for all bars that have a gradient, instead of making a specific gradient for each bar. This allows the gradients on each bar to line up.
    
    The gradient will only apply to bars that exceed meter.low. Anything below low is green. Anything in middle is yellow, becoming orange-red as it gets higher. If it goes above high, it will start to become dark red up to high+20.
    
    https://github.com/e-mission/e-mission-phone/pull/1018#issuecomment-1718083794

--- www/js/components/charting.ts
+++ www/js/components/charting.ts
@@ -90,49 +90,34 @@ export function getMeteredBackgroundColor(meter, currDataset, barCtx, colors, da
   return meteredColor;
 }
 
-/* Gradient created using Josh Comeau's gradient-generator:
-    https://www.joshwcomeau.com/gradient-generator?colors=00da71|eccd00|ff8c00|ba0000|440000&angle=90&colorMode=lab&precision=16&easingCurve=0.05172413793103448|0.7243063038793104|1.05|0.3708580280172414
-  Reformatted here as arrays to be used programatically */
-const gradientStops: [string, number][] = [
-  ['151, 100%, 43%', 0], ['117, 61%,  62%', 2], ['91,  62%,  58%', 5],
-  ['73,  63%,  54%', 10], ['60,  66%,  50%', 16], ['52,  100%, 46%', 22],
-  ['48,  100%, 47%', 29], ['44,  100%, 48%', 36], ['40,  100%, 49%', 43],
-  ['37,  100%, 49%', 50], ['33,  100%, 50%', 58], ['30,  100%, 47%', 65],
-  ['26,  100%, 45%', 71], ['21,  100%, 42%', 78], ['14,  100%, 39%', 83],
-  ['0,   100%, 36%', 88], ['359, 100%, 32%', 93], ['358, 100%, 27%', 96],
-  ['358, 100%, 22%', 99], ['358, 100%, 18%', 100], // ['0, 100%, 13%', 100],
-];
-
-const clamp = (val: number, min: number, max: number) =>
-  Math.min(max, Math.max(min, val));
-
-function getColorsForPercentage(percentage: number, alpha = 1) {
-  const clampedEndPct = clamp(percentage, 0, 1);
-  const clampedStartPct = clamp(clampedEndPct - .1 - (clampedEndPct * .4), 0, 1);
-  const startIndex = gradientStops.findIndex(([_, pct]) => pct >= clampedStartPct * 100);
-  const endIndex = gradientStops.findIndex(([_, pct]) => pct >= clampedEndPct * 100);
-  const slice = gradientStops.slice(startIndex, endIndex + 1);
-  const colors = slice.map(([hsl, pct]) => [`hsla(${hsl}, ${alpha})`, pct / 100]);
-  return colors as [string, number][];
+const meterColors = {
+  below: '#00cc95', // green oklch(75% 0.3 165)
+  // https://www.joshwcomeau.com/gradient-generator?colors=fcab00|ba0000&angle=90&colorMode=lab&precision=3&easingCurve=0.25|0.75|0.75|0.25
+  between: ['#fcab00', '#ef8215', '#db5e0c', '#ce3d03', '#b70100'], // yellow-orange-red
+  above: '#440000', // dark red
 }
 
 export function getGradient(chart, meter, currDataset, barCtx, alpha = null, darken = 0) {
   const { ctx, chartArea, scales } = chart;
   if (!chartArea) return null;
   let gradient: CanvasGradient;
-  const chartWidth = chartArea.right - chartArea.left;
   const total = getBarHeight(barCtx.parsed._stacks);
-  const approxLength = clamp(total / scales.x._range.max, 0, 1) * chartWidth;
-  gradient = ctx.createLinearGradient(chartArea.left, 0, chartArea.left + approxLength, 0);
   alpha = alpha || (currDataset.label == meter.dash_key ? 0.2 : 1);
-  let colors = getColorsForPercentage(total / 60, alpha);
-  colors.forEach((c, i) => {
-    if (darken) {
-      const darkened = color(c[0]).darken(darken);
-      c = [darkened.hex(), c[1]];
-    }
-    gradient.addColorStop(c[1], c[0]);
+  if (total < meter.middle) {
+    const adjColor = darken||alpha ? color(meterColors.below).darken(0.2).alpha(alpha).rgb().string() : meterColors.below;
+    return adjColor;
+  }
+  const scaleMaxX = scales.x._range.max;
+  gradient = ctx.createLinearGradient(chartArea.left, 0, chartArea.right, 0);
+  meterColors.between.forEach((clr, i) => {
+    const clrPosition = ((i + 1) / meterColors.between.length) * (meter.high - meter.middle) + meter.middle;
+    const adjColor = darken || alpha ? color(clr).darken(darken).alpha(alpha).rgb().string() : clr;
+    gradient.addColorStop(Math.min(clrPosition, scaleMaxX) / scaleMaxX, adjColor);
   });
+  if (scaleMaxX > meter.high + 20) {
+    const adjColor = darken||alpha ? color(meterColors.above).darken(0.2).alpha(alpha).rgb().string() : meterColors.above;
+    gradient.addColorStop((meter.high+20) / scaleMaxX, adjColor);
+  }
   return gradient;
 }
 
