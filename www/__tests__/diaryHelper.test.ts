df60ea4b25ac6965149fd5cb400094e2eaa15be1
Abby Wheelis <abby.wheelis@centre.edu>
2023-09-20 10:43:37 -0600

    more diaryHelper tests
    continuing to improve testing coverage for diaryHelper by adding tests for more functions
    
    For some of the functions related to a trip, I found it easiest to look at a trip in the logs, create a test trip object with the relevant parts, and then also mock the results

--- www/__tests__/diaryHelper.test.ts
+++ www/__tests__/diaryHelper.test.ts
@@ -1,9 +1,22 @@
-import { getFormattedDate, motionTypeOf, isMultiDay } from "../js/diary/diaryHelper";
+import { getFormattedSectionProperties, getFormattedDate, motionTypeOf, isMultiDay, getFormattedDateAbbr, getFormattedTimeRange, getPercentages } from "../js/diary/diaryHelper";
+import { useImperialConfig } from "../js/config/useImperialConfig";
 
 it('returns a formatted date', () => {
   expect(getFormattedDate("2023-09-18T00:00:00-07:00")).toBe("Mon September 18, 2023");
   expect(getFormattedDate("")).toBeUndefined();
   expect(getFormattedDate("2023-09-18T00:00:00-07:00", "2023-09-21T00:00:00-07:00")).toBe("Mon September 18, 2023 - Thu September 21, 2023");
+});
+
+it('returns an abbreviated formatted date', () => {
+  expect(getFormattedDateAbbr("2023-09-18T00:00:00-07:00")).toBe("Mon, Sep 18");
+  expect(getFormattedDateAbbr("")).toBeUndefined();
+  expect(getFormattedDateAbbr("2023-09-18T00:00:00-07:00", "2023-09-21T00:00:00-07:00")).toBe("Mon, Sep 18 - Thu, Sep 21");
+});
+
+it('returns a human readable time range', () => {
+  expect(getFormattedTimeRange("2023-09-18T00:00:00-07:00", "2023-09-18T00:00:00-09:20")).toBe("2 hours");
+  expect(getFormattedTimeRange("2023-09-18T00:00:00-07:00", "2023-09-18T00:00:00-09:30")).toBe("3 hours");
+  expect(getFormattedTimeRange("", "2023-09-18T00:00:00-09:30")).toBeFalsy();
 });
 
 it("returns a MotionType object", () => {
@@ -17,3 +30,36 @@ it('returns true/false is multi day', () => {
   expect(isMultiDay("2023-09-18T00:00:00-07:00", "2023-09-18T00:00:00-09:00")).toBeFalsy();
   expect(isMultiDay("", "2023-09-18T00:00:00-09:00")).toBeFalsy();
 });
+
+//created a fake trip with relevant sections by examining log statements
+let myFakeTrip = {sections: [
+  { "sensed_mode_str": "BICYCLING", "distance": 6013.73657416706 },
+  { "sensed_mode_str": "WALKING", "distance": 715.3078629361006 }
+]};
+let myFakeTrip2 = {sections: [
+  { "sensed_mode_str": "BICYCLING", "distance": 6013.73657416706 },
+  { "sensed_mode_str": "BICYCLING", "distance": 715.3078629361006 }
+]};
+
+let myFakePcts = [
+  { mode: "BICYCLING",
+    icon: "bike",
+    color: '#007e46',
+    pct: 89 },
+  { mode: "WALKING",
+    icon: "walk",
+    color: '#0068a5',
+    pct: 11 }];
+
+let myFakePcts2 = [
+  { mode: "BICYCLING",
+    icon: "bike",
+    color: '#007e46',
+    pct: 100 }];
+
+it('returns the percetnages by mode for a trip', () => {
+  expect(getPercentages(myFakeTrip)).toEqual(myFakePcts);
+  expect(getPercentages(myFakeTrip2)).toEqual(myFakePcts2);
+  expect(getPercentages({})).toEqual({});
+})
+
