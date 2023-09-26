957425fade6aac170ec9efb6d81ca6cf36df0031
Jack Greenlee <greenljk@mail.uc.edu>
2023-09-26 02:55:25 -0400

    remove old enketo code
    The EnketoSurveyLaunch and EnketoSurvey services have been replaced.
    
    The EnketoModal component (EnketoModal.tsx) and the enketoHelper.ts file handle all the survey responsibilities that EnketoSurveyLaunch and EnketoSurvey previously handled

--- www/js/survey/enketo/enketo-add-note-button.js
+++ www/js/survey/enketo/enketo-add-note-button.js
@@ -7,7 +7,6 @@ import angular from 'angular';
 angular.module('emission.survey.enketo.add-note-button',
     ['emission.stats.clientstats',
         'emission.services',
-        'emission.survey.enketo.launch',
         'emission.survey.enketo.answer',
         'emission.survey.inputmatcher'])
 .factory("EnketoNotesButtonService", function(InputMatcher, EnketoSurveyAnswer, Logger, $timeout) {
