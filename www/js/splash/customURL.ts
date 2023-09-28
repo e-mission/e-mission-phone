46301f7db29f191c2489b8c49ec78ad078a3c0dc
Jijeong Lee <jijeong0114@gmail.com>
2023-09-28 11:42:11 -0700

    write new customURL function

---
+++ www/js/splash/customURL.ts
@@ -0,0 +1,18 @@
+type UrlComponents = {
+    [key : string] : string
+}
+
+type OnLaunchCustomURL = (rawUrl: string, callback: (url: string, urlComponents: UrlComponents) => void ) => void;
+
+
+export const onLaunchCustomURL: OnLaunchCustomURL = (rawUrl, handler) => {
+    const url = rawUrl.split('//')[1];
+    const [ route, paramString ] = url.split('?');
+    const paramsList = paramString.split('&');
+    const urlComponents: UrlComponents = { route : route };
+    for (let i = 0; i < paramsList.length; i++) {
+        const [key, value] = paramsList[i].split('=');
+        urlComponents[key] = value;
+    }
+    handler(url, urlComponents);
+};
