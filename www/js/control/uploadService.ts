eaf4be5dfa45925c638ac9dfdcb23f7a4e2b8e53
Abby Wheelis <abby.wheelis@centre.edu>
2023-09-29 11:27:07 -0600

    adapt promise handling to async/await
    using async await is a clearer way of handling the asynchronous processes in this file, so it is easier to figure out what is happening and properly set up the typescript version of this service.

--- www/js/control/uploadService.ts
+++ www/js/control/uploadService.ts
@@ -4,25 +4,27 @@ import { useTranslation } from "react-i18next";
 /**
  * @returns A promise that resolves with an upload URL or rejects with an error
  */
-function getUploadConfig() {
-    return new Promise<string[]>(function (resolve, reject) {
-        //logInfo( "About to get email config");
+async function getUploadConfig() {
+    return new Promise<string[]>(async function (resolve, reject) {
+        logInfo( "About to get email config");
         let url = [];
-        fetch("json/uploadConfig.json").then( function (uploadConfig) {
-            //logDebug("uploadConfigString = " + JSON.stringify(uploadConfig['data']));
+        try {
+            let uploadConfig = await fetch("json/uploadConfig.json");
+            logDebug("uploadConfigString = " + JSON.stringify(uploadConfig['data']));
             url.push(uploadConfig["data"].url);
             resolve(url);
-        }).catch(function (err) {
-            fetch("json/uploadConfig.json.sample"). then(function (uploadConfig) {
-                //logDebug("default uploadConfigString = " + JSON.stringify(uploadConfig['data']));
+        } catch (err) {
+            try{
+                let uploadConfig = await fetch("json/uploadConfig.json.sample");
+                logDebug("default uploadConfigString = " + JSON.stringify(uploadConfig['data']));
                 console.log("default uploadConfigString = " + JSON.stringify(uploadConfig['data']))
                 url.push(uploadConfig["data"].url);
                 resolve(url);
-            }).catch(function (err) {
-                //logError("Error while reading default upload config" + err);
+            } catch (err) {
+                logError("Error while reading default upload config" + err);
                 reject(err);
-            })
-        })
+            }
+        } 
     })
 }
 
@@ -80,101 +82,63 @@ const sendToServer = function upload(url, binArray, params) {
         transformRequest: identity,
         params: params
     };
-    return fetch (url, config);
+    return fetch(url, config);
 }
 
 
 //only export of this file, used in ProfileSettings and passed the argument (""loggerDB"")
-export function uploadFile(database) {
+export async function uploadFile(database, reason) {
     const { t } = useTranslation();
-    getUploadConfig().then((uploadConfig) => {
-      var parentDir = "unknown";
+    try {
+        let uploadConfig = await getUploadConfig();
+        var parentDir = "unknown";
 
-      if (window['cordova'].platformId.toLowerCase() == "android") {
-          parentDir = window['cordova'].file.applicationStorageDirectory+"/databases";
-      }
-      else if (window['cordova'].platformId.toLowerCase() == "ios") {
-          parentDir = window['cordova'].file.dataDirectory + "../LocalDatabase";
-      } else {
-          alert("parentDir unexpectedly = " + parentDir + "!")
-      }
-
-      const newScope = {};
-      newScope["data"] = {};
-      newScope["fromDirText"] = t('upload-service.upload-from-dir',  {parentDir: parentDir});
-      newScope["toServerText"] = t('upload-service.upload-to-server',  {serverURL: uploadConfig});
-
-      let didCancel = true;
-      let detailsPopup = () => console.log("I need a popup");
-
-    //   const detailsPopup = $ionicPopup.show({
-    //     title: i18next.t("upload-service.upload-database", { db: database }),
-    //     template: newScope.toServerText
-    //         + '<input type="text" ng-model="data.reason"'
-    //         +' placeholder="{{ \'upload-service.please-fill-in-what-is-wrong \' | translate}}">',
-    //     scope: newScope,
-    //     buttons: [
-    //       { 
-    //         text: 'Cancel',
-    //         onTap: function(e) {
-    //             didCancel = true;
-    //             detailsPopup.close();
-    //         }
-    //       },
-    //       {
-    //         text: '<b>Upload</b>',
-    //         type: 'button-positive',
-    //         onTap: function(e) {
-    //           if (!newScope.data.reason) {
-    //             //don't allow the user to close unless he enters wifi password
-    //             didCancel = false;
-    //             e.preventDefault();
-    //           } else {
-    //             didCancel = false;
-    //             return newScope.data.reason;
-    //           }
-    //         }
-    //       }
-    //     ]
-    // });
-
-      logInfo("Going to upload " + database);
-      const readFileAndInfo = [readDBFile(parentDir, database, detailsPopup)];
-      Promise.all(readFileAndInfo).then(([binString, reason]) => {
-        if(!didCancel)
-        {  
-          console.log("Uploading file of size "+binString['byteLength']);
-          const progressScope = {...newScope}; //make a child copy of the current scope
-          const params = {
-              reason: reason,
-              tz: Intl.DateTimeFormat().resolvedOptions().timeZone
-          }
-          uploadConfig.forEach((url) => {
-            alert(t("upload-service.upload-database", {db: database})
-                + "\n"
-                + t("upload-service.upload-progress", {filesizemb: binString['byteLength'] / (1000 * 1000), serverURL: uploadConfig})
-            );
-            //   const progressPopup = $ionicPopup.show({
-            //       title: t("upload-service.upload-database",
-            //           {db: database}),
-            //       template: t("upload-service.upload-progress",
-            //           {filesizemb: binString['byteLength'] / (1000 * 1000),
-            //            serverURL: uploadConfig})
-            //           + '<center><ion-spinner></ion-spinner></center>',
-            //       scope: progressScope,
-            //       buttons: [
-            //           { text: '<b>Cancel</b>', type: 'button-cancel',  },
-            //       ]
-            //   });
-              sendToServer(url, binString, params).then((response) => {
-                console.log(response);
-                //progressPopup.close();
-                displayErrorMsg(t("upload-service.upload-details", 
-                    {filesizemb: binString['byteLength'] / (1000 * 1000), serverURL: uploadConfig}), 
-                    t("upload-service.upload-success"));
-              }).catch(onUploadError);
-          });
+        if (window['cordova'].platformId.toLowerCase() == "android") {
+            parentDir = window['cordova'].file.applicationStorageDirectory+"/databases";
         }
-      }).catch(onReadError);
-    }).catch(onReadError);
-  };
+        else if (window['cordova'].platformId.toLowerCase() == "ios") {
+            parentDir = window['cordova'].file.dataDirectory + "../LocalDatabase";
+        } else {
+            alert("parentDir unexpectedly = " + parentDir + "!")
+        }
+
+        const newScope = {};
+        newScope["data"] = {};
+        newScope["fromDirText"] = t('upload-service.upload-from-dir',  {parentDir: parentDir});
+        newScope["toServerText"] = t('upload-service.upload-to-server',  {serverURL: uploadConfig});
+
+        logInfo("Going to upload " + database);
+        try {
+            let binString = await readDBFile(parentDir, database, undefined);
+            console.log("Uploading file of size "+binString['byteLength']);
+            const progressScope = {...newScope}; //make a child copy of the current scope
+            const params = {
+                reason: reason,
+                tz: Intl.DateTimeFormat().resolvedOptions().timeZone
+            }
+            uploadConfig.forEach(async (url) => {
+                alert(t("upload-service.upload-database", {db: database})
+                    + "\n"
+                    + t("upload-service.upload-progress", {filesizemb: binString['byteLength'] / (1000 * 1000), serverURL: uploadConfig})
+                );
+
+                window.alert(t("upload-service.upload-database", {db: database}));
+
+                try {
+                    let response = await sendToServer(url, binString, params);
+                    console.log(response);
+                    window.alert(t("upload-service.upload-details", 
+                        {filesizemb: binString['byteLength'] / (1000 * 1000), serverURL: uploadConfig})
+                        + t("upload-service.upload-success"));
+                } catch (error) {
+                    onUploadError(error);
+                }
+            });
+        }
+        catch (error){
+            onReadError(error);
+        }
+    } catch (error) {
+        onReadError(error);
+    }
+};
