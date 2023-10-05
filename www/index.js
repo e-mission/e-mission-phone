e0dd682f8afc78ec14aa5276d7419abb5edcedf9
Abby Wheelis <abby.wheelis@centre.edu>
2023-10-05 11:13:17 -0600

    remove old angular upload service

--- www/index.js
+++ www/index.js
@@ -26,7 +26,6 @@ import './js/survey/enketo/infinite_scroll_filters.js';
 import './js/survey/enketo/enketo-trip-button.js';
 import './js/survey/enketo/enketo-add-note-button.js';
 import './js/control/emailService.js';
-import './js/control/nguploadService.js';
 import './js/metrics-factory.js';
 import './js/metrics-mappings.js';
 import './js/plugin/logger.ts';
