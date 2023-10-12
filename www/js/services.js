83a4939ad8e8669342a12c0c6e0f6f2b6c6542ab
Katie Rischpater <98350084+the-bay-kay@users.noreply.github.com>
2023-10-12 10:50:54 -0700

    Removed stale code
    The 'Chats' and 'ReferHelper' services, alongside their derivative
    services, have not been used for quite some time.  These were
    removed in preparation for the UnifiedDataLoader-rewrite.

--- www/js/services.js
+++ www/js/services.js
@@ -5,23 +5,6 @@ import { getRawEntries } from './commHelper';
 
 angular.module('emission.services', ['emission.plugin.logger'])
 
-.service('ReferHelper', function($http) {
-
-    this.habiticaRegister = function(groupid, successCallback, errorCallback) {
-        window.cordova.plugins.BEMServerComm.getUserPersonalData("/join.group/"+groupid, successCallback, errorCallback);
-    };
-    this.joinGroup = function(groupid, userid) {
-
-    // TODO:
-    return new Promise(function(resolve, reject) {
-        window.cordova.plugins.BEMServerComm.postUserPersonalData("/join.group/"+groupid, "inviter", userid, resolve, reject);
-      })
-
-    //function firstUpperCase(string) {
-    //  return string[0].toUpperCase() + string.slice(1);
-    //}*/
-    }
-})
 .service('UnifiedDataLoader', function($window, Logger) {
     var combineWithDedup = function(list1, list2) {
       var combinedList = list1.concat(list2);
@@ -216,77 +199,3 @@ angular.module('emission.services', ['emission.plugin.logger'])
     };
 
 })
-
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }, {
-    id: 5,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way again?',
-    face: 'img/ben.png'
-  }, {
-    id: 6,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me again',
-    face: 'img/max.png'
-  }, {
-    id: 7,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat again',
-    face: 'img/adam.jpg'
-  }, {
-    id: 8,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks again!',
-    face: 'img/perry.png'
-  }, {
-    id: 9,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream again.',
-    face: 'img/mike.png'
-  }];
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
-    }
-  };
-});
