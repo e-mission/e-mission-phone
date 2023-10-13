16673e805621566835ebef1321514fb21d2a88f1
Jijeong Lee <jijeong0114@gmail.com>
2023-10-13 13:18:09 -0700

    Done with input-matcher.test

--- www/__tests__/input-matcher.test.ts
+++ www/__tests__/input-matcher.test.ts
@@ -1,59 +1,76 @@
 import { 
     TlEntry, 
-    Trip, 
     UserInputForTrip, 
     fmtTs, 
     printUserInput, 
     validUserInputForDraftTrip, 
     validUserInputForTimelineEntry,
-    getNotDeletedCandidates
+    getNotDeletedCandidates,
+    getUserInputForTrip,
+    getAdditionsForTimelineEntry,
+    getUniqueEntries
 } from '../js/survey/input-matcher';
 
 describe('input-matcher', () => {
     let userTrip: UserInputForTrip;
+    let trip: TlEntry;
 
     beforeEach(() => {
-        // create a userTrip object before each test case.
+        // create valid userTrip and trip object before each test case.
         userTrip  = { 
             data: {
                 end_ts: 1437604764,
                 start_ts: 1437601247,
-                label: "FOO"
+                label: 'FOO',
+                status: 'ACTIVE'
             },
             metadata: {
-                time_zone: "America/Los_Angeles",
-                plugin: "none",
-                write_ts: 1695921991.013001,
-                platform: "ios",
+                time_zone: 'America/Los_Angeles',
+                plugin: 'none',
+                write_ts: 1695921991,
+                platform: 'ios',
                 read_ts: 0,
-                key: "manual/mode_confirm"
+                key: 'manual/mode_confirm'
             },
-            key: "manual/place"
+            key: 'manual/place'
         }
+        trip = {
+            key: 'FOO',
+            origin_key: 'FOO',
+            start_ts: 1437601000,
+            end_ts: 1437605000,
+            enter_ts: 1437605000,
+            exit_ts: 1437605000,
+            duration: 100,
+            getNextEntry: jest.fn()
+        }
+
+        // mock Logger
+        window['Logger'] = { log: console.log };
     });
 
     it('tests fmtTs with valid input', () => {
-        const pstTime = fmtTs(1437601247.8459613, "America/Los_Angeles");
-        const estTime = fmtTs(1437601247.8459613, "America/New_York");
+        const pstTime = fmtTs(1437601247.8459613, 'America/Los_Angeles');
+        const estTime = fmtTs(1437601247.8459613, 'America/New_York');
+        
         // Check if it contains correct year-mm-dd hr:mm 
-        expect(pstTime).toContain("2015-07-22T14:40");
-        expect(estTime).toContain("2015-07-22T17:40");
+        expect(pstTime).toContain('2015-07-22T14:40');
+        expect(estTime).toContain('2015-07-22T17:40');
     });
 
     it('tests fmtTs with invalid input', () => {
-        const formattedTeim = fmtTs(0, "");
-        // Check if it contains correct year-mm-dd hr:mm 
-        expect(formattedTeim).toBeFalsy();
+        const formattedTime = fmtTs(0, '');
+        expect(formattedTime).toBeFalsy();
     });
 
     it('tests printUserInput prints the trip log correctly', () => {
         const userTripLog = printUserInput(userTrip);
-        expect(userTripLog).toContain("1437604764");
-        expect(userTripLog).toContain("1437601247");
-        expect(userTripLog).toContain("FOO");
+        expect(userTripLog).toContain('1437604764');
+        expect(userTripLog).toContain('1437601247');
+        expect(userTripLog).toContain('FOO');
     });
 
-    it('tests validUserInputForDraftTrip with valid trip', () => {
+    it('tests validUserInputForDraftTrip with valid trip input', () => {
         const validTrp = {   
             end_ts: 1437604764,
             start_ts: 1437601247
@@ -62,7 +79,7 @@ describe('input-matcher', () => {
         expect(validUserInput).toBeTruthy();
     });
 
-    it('tests validUserInputForDraftTrip with invalid trip', () => {
+    it('tests validUserInputForDraftTrip with invalid trip input', () => {
         const invalidTrip = {
             end_ts: 0,
             start_ts: 0
@@ -71,42 +88,24 @@ describe('input-matcher', () => {
         expect(invalidUserInput).toBeFalsy();
     });
 
-    it('tests validUserInputForTimelineEntry with valid tlEntry object', () => {
-        const tlEntry: TlEntry = {
-            key: "analysis/confirmed_place",
-            origin_key: "analysis/confirmed_place",
-            start_ts: 1437601000,
-            end_ts: 1437605000,
-            enter_ts: 1437605000,
-            exit_ts: 1437605000,
-            duration: 100,
-            getNextEntry: jest.fn()
-        }
-
-        const validTimelineEntry = validUserInputForTimelineEntry(tlEntry, userTrip, false);
+    it('tests validUserInputForTimelineEntry with valid trip object', () => {
+        // we need valid key and origin_key for validUserInputForTimelineEntry test
+        trip['key'] = 'analysis/confirmed_place';
+        trip['origin_key'] = 'analysis/confirmed_place';
+        const validTimelineEntry = validUserInputForTimelineEntry(trip, userTrip, false);
         expect(validTimelineEntry).toBeTruthy();
     });
 
-    it('tests validUserInputForTimelineEntry with invalid tlEntry key', () => {
-        const tlEntry: TlEntry = {
-            key: "FOO",
-            origin_key: "FOO",
-            start_ts: 1437601000,
-            end_ts: 1437605000,
-            enter_ts: 1437605000,
-            exit_ts: 1437605000,
-            duration: 100,
-            getNextEntry: jest.fn()
-        }
-
-        const invalidTimelineEntry = validUserInputForTimelineEntry(tlEntry, userTrip, false);
+    it('tests validUserInputForTimelineEntry with tlEntry with invalid key and origin_key', () => {
+        const invalidTlEntry = trip;
+        const invalidTimelineEntry = validUserInputForTimelineEntry(invalidTlEntry, userTrip, false);
         expect(invalidTimelineEntry).toBeFalsy();
     });
 
-    it('tests validUserInputForTimelineEntry with invalid tlEntry start & end time', () => {
-        const tlEntry: TlEntry = {
-            key: "analysis/confirmed_place",
-            origin_key: "analysis/confirmed_place",
+    it('tests validUserInputForTimelineEntry with tlEntry with invalie start & end time', () => {
+        const invalidTlEntry: TlEntry = {
+            key: 'analysis/confirmed_place',
+            origin_key: 'analysis/confirmed_place',
             start_ts: 1,
             end_ts: 1,
             enter_ts: 1,
@@ -114,8 +113,7 @@ describe('input-matcher', () => {
             duration: 1,
             getNextEntry: jest.fn()
         }
-
-        const invalidTimelineEntry = validUserInputForTimelineEntry(tlEntry, userTrip, false);
+        const invalidTimelineEntry = validUserInputForTimelineEntry(invalidTlEntry, userTrip, false);
         expect(invalidTimelineEntry).toBeFalsy();
     });
 
@@ -129,15 +127,123 @@ describe('input-matcher', () => {
 
     });
 
-    it('tests getUserInputForTrip', () => {
-        
+    it('tests getNotDeletedCandidates called with multiple candidates', () => {
+        const activeTrip = userTrip;
+        const deletedTrip = {
+            data: {
+                end_ts: 1437604764,
+                start_ts: 1437601247,
+                label: 'FOO',
+                status: 'DELETED',
+                match_id: 'FOO'
+            },
+            metadata: {
+                time_zone: 'America/Los_Angeles',
+                plugin: 'none',
+                write_ts: 1695921991,
+                platform: 'ios',
+                read_ts: 0,
+                key: 'manual/mode_confirm'
+            },
+            key: 'manual/place'
+        }
+        const candidates = [ activeTrip, deletedTrip ];
+        const validCandidates = getNotDeletedCandidates(candidates);
+
+        // check if the result has only 'ACTIVE' data
+        expect(validCandidates).toHaveLength(1);
+        expect(validCandidates[0]).toMatchObject(userTrip);
+
     });
 
-    it('tests getAdditionsForTimelineEntry', () => {
+    it('tests getUserInputForTrip with valid userInputList', () => {
+        const userInputWriteFirst = {
+            data: {
+                end_ts: 1437607732,
+                label: 'bus',
+                start_ts: 1437606026
+            },
+            metadata: {
+                time_zone: 'America/Los_Angeles',
+                plugin: 'none',
+                write_ts: 1695830232,
+                platform: 'ios',
+                read_ts: 0,
+                key:'manual/mode_confirm',
+                type:'message'
+            }
+        }
+        const userInputWriteSecond = { 
+            data: {
+                end_ts: 1437598393,
+                label: 'e-bike',
+                start_ts: 1437596745
+            },
+            metadata: {
+                time_zone: 'America/Los_Angeles',
+                plugin: 'none',
+                write_ts: 1695838268,
+                platform: 'ios',
+                read_ts: 0,
+                key:'manual/mode_confirm',
+                type:'message'
+            }
+        }
+        const userInputWriteThird = {
+            data: {
+                end_ts: 1437604764,
+                label: 'e-bike',
+                start_ts: 1437601247
+            },
+            metadata: {
+                time_zone: 'America/Los_Angeles',
+                plugin: 'none',
+                write_ts: 1695921991,
+                platform: 'ios',
+                read_ts: 0,
+                key:'manual/mode_confirm',
+                type:'message'
+            }
+        }
 
+        // make the linst unsorted and then check if userInputWriteThird(latest one) is return output
+        const userInputList = [userInputWriteSecond, userInputWriteThird, userInputWriteFirst];
+        const mostRecentEntry = getUserInputForTrip(trip, {}, userInputList);
+        expect(mostRecentEntry).toMatchObject(userInputWriteThird);
     });
 
-    it('tests getUniqueEntries', () => {
+    it('tests getUserInputForTrip with invalid userInputList', () => {
+        const userInputList = undefined;
+        const mostRecentEntry = getUserInputForTrip(trip, {}, userInputList);
+        expect(mostRecentEntry).toBe(undefined);
+    });
 
+    it('tests getAdditionsForTimelineEntry with valid additionsList', () => {
+        const additionsList = new Array(5).fill(userTrip);
+        trip['key'] = 'analysis/confirmed_place';
+        trip['origin_key'] = 'analysis/confirmed_place';
+
+        // check if the result keep the all valid userTrip items
+        const matchingAdditions = getAdditionsForTimelineEntry(trip, additionsList);
+        expect(matchingAdditions).toHaveLength(5);
+    });
+
+    it('tests getAdditionsForTimelineEntry with invalid additionsList', () => {
+        const additionsList = undefined;
+        const matchingAdditions = getAdditionsForTimelineEntry(trip, additionsList);
+        expect(matchingAdditions).toMatchObject([]);
+    });
+
+    it('tests getUniqueEntries with valid combinedList', () => {
+        const combinedList = new Array(5).fill(userTrip);
+
+        // check if the result keeps only unique userTrip items
+        const uniqueEntires = getUniqueEntries(combinedList);
+        expect(uniqueEntires).toHaveLength(1);
+    });
+
+    it('tests getUniqueEntries with empty combinedList', () => {
+        const uniqueEntires = getUniqueEntries([]);
+        expect(uniqueEntires).toMatchObject([]);
     });
 })
