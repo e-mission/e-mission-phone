e7fe7a8005c039240c6a4bab32141ecee4a612d5
Abby Wheelis <abby.wheelis@centre.edu>
2023-10-13 11:03:30 -0600

    don't mock i18n, use the real thing
    with the changes from #1049, we are now able to test using i18n, no need to mock!

--- www/__mocks__/i18nextMocks.ts
+++
@@ -1,8 +0,0 @@
-const i18next = jest.createMockFromModule('i18next');
-
-let resolvedLanugage;
-
-function  _setUpLanguage(language) {
-    console.log("setting resolved language to ", language, " for testing");
-    resolvedLanugage = language;
-}
