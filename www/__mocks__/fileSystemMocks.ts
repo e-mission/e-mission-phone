4f3d9fb1a1ecc2982ce0b81f2f80d80cc262d502
Abby Wheelis <abby.wheelis@centre.edu>
2023-10-16 11:52:34 -0600

    fix indentations
    now consistent 2-space indentation

--- www/__mocks__/fileSystemMocks.ts
+++ www/__mocks__/fileSystemMocks.ts
@@ -1,18 +1,17 @@
 export const mockFileSystem = () => {
-    window['resolveLocalFileSystemURL'] = function (parentDir, handleFS) {
-        const fs = {"filesystem": 
-                        {"root":
-                            {"getFile": (path, options, onSuccess) => {
-                                    let fileEntry = {"file": (handleFile) => {
-                                        let file = new File(["this is a mock"], "loggerDB");
-                                        handleFile(file);
-                                    }}
-                                    onSuccess(fileEntry);
-                                }
-                            }
-                        }
-                    }
-        console.log("in mock, fs is ", fs, " get File is ", fs.filesystem.root.getFile);
-        handleFS(fs);
+  window['resolveLocalFileSystemURL'] = function (parentDir, handleFS) {
+    const fs = {"filesystem": 
+      {"root":
+        {"getFile": (path, options, onSuccess) => {
+          let fileEntry = {"file": (handleFile) => {
+            let file = new File(["this is a mock"], "loggerDB");
+              handleFile(file);
+          }}
+          onSuccess(fileEntry);
+        }}
+      }
     }
+  console.log("in mock, fs is ", fs, " get File is ", fs.filesystem.root.getFile);
+  handleFS(fs);
   }
+}
