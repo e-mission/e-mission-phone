49e1757809780eff193adf552f992532e8b4cee3
Katie Rischpater <98350084+the-bay-kay@users.noreply.github.com>
2023-10-20 15:56:21 -0700

    Added types for `controlHelper.ts`

---
+++ www/js/types/fileShareTypes.ts
@@ -0,0 +1,22 @@
+import { ServerData } from './serverData'; 
+
+export type TimeStampData = ServerData<RawTimelineData>;
+
+export type RawTimelineData = {
+    name: string, 
+    ts: number,
+    reading: number,
+};
+
+export interface FsWindow extends Window {
+  requestFileSystem: (
+    type: number,
+    size: number,
+    successCallback: (fs: any) => void,
+    errorCallback?: (error: any) => void
+  ) => void;
+  LocalFileSystem: {
+    TEMPORARY: number;
+    PERSISTENT: number;
+  };
+};
