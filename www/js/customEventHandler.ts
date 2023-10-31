ea3fb10d468689dddb6c89a72d947030b1668272
Abby Wheelis <abby.wheelis@centre.edu>
2023-10-31 14:11:31 -0600

    create event handler
    React doesn't quite support custom events, so we're writing our own event handling code
    
    this will apply to our custom events, rather than the built-in events like 'onClick'

---
+++ www/js/customEventHandler.ts
@@ -0,0 +1,30 @@
+/**
+ * since react doesn't quite support custom events, writing our own handler
+ * having the ability to broadcast and emit events prevents files from being tightly coupled
+ * if we want something else to happen when an event is emitted, we can just listen for it
+ * instead of having to change the code at the point the event is emitted
+ * 
+ * looser coupling = point of broadcast doesn't 'know' what is triggered by that event
+ * leads to more extensible code
+ * consistent event names help us know what happens when
+ * 
+ * code based on: https://blog.logrocket.com/using-custom-events-react/
+ */
+
+import { logDebug } from './plugin/logger';
+
+export function subscribe(eventName: string, listener) {
+  logDebug("adding " + eventName + " listener");
+  document.addEventListener(eventName, listener);
+}
+
+export function unsubscribe(eventName: string, listener){
+  logDebug("removing " + eventName + " listener");
+  document.removeEventListener(eventName, listener);
+}
+
+export function publish(eventName, data) {
+  logDebug("publishing " + eventName);
+  const event = new CustomEvent(eventName, { detail: data });
+  document.dispatchEvent(event);
+}
