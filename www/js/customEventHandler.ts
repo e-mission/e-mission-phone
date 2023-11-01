82e864bea38c071802810a3eed7a06ddf917df51
Abby Wheelis <abby.wheelis@centre.edu>
2023-11-01 12:06:40 -0600

    add docstrings to customEventHandler

--- www/js/customEventHandler.ts
+++ www/js/customEventHandler.ts
@@ -13,23 +13,42 @@
 
 import { logDebug } from './plugin/logger';
 
+/**
+ * central source for event names
+ */
 export const EVENT_NAMES = {
   CLOUD_NOTIFICATION_EVENT: 'cloud:push:notification',
   CONSENTED_EVENT: "data_collection_consented",
   INTRO_DONE_EVENT: "intro_done",
 }
 
+/**
+ * @function starts listening to an event
+ * @param eventName {string} the name of the event 
+ * @param listener event listener, function to execute on event
+ */
 export function subscribe(eventName: string, listener) {
   logDebug("adding " + eventName + " listener");
   document.addEventListener(eventName, listener);
 }
 
+/**
+ * @function stops listening to an event
+ * @param eventName {string} the name of the event 
+ * @param listener event listener, function to execute on event
+ */
 export function unsubscribe(eventName: string, listener){
   logDebug("removing " + eventName + " listener");
   document.removeEventListener(eventName, listener);
 }
 
-export function publish(eventName, data) {
+/**
+ * @function broadcasts an event
+ * the data is stored in the "detail" of the event
+ * @param eventName {string} the name of the event
+ * @param data any additional data to be added to event
+ */
+export function publish(eventName: string, data) {
   logDebug("publishing " + eventName + " with data " + JSON.stringify(data));
   const event = new CustomEvent(eventName, { detail: data });
   document.dispatchEvent(event);
