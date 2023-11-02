ede23473eed48016128db1f95fa6d4958ee234e9
Abby Wheelis <abby.wheelis@centre.edu>
2023-11-02 11:16:17 -0600

    rewrite service to typscript
    in this rewrite, also include the subscription to events so that events can be called on consent and on intro done

--- www/js/splash/storeDeviceSettings.ts
+++ www/js/splash/storeDeviceSettings.ts
@@ -1,48 +1,70 @@
-import angular from 'angular';
+
 import { updateUser } from '../commHelper';
 import { isConsented, readConsentState } from "./startprefs";
+import i18next from 'i18next';
+import { displayError, logDebug } from '../plugin/logger';
+import { readIntroDone } from '../onboarding/onboardingHelper';
+import { subscribe, EVENT_NAMES } from '../customEventHandler';
 
-angular.module('emission.splash.storedevicesettings', ['emission.plugin.logger',
-                                             'emission.services'])
-.factory('StoreDeviceSettings', function($window, $state, $rootScope, $ionicPlatform,
-    $ionicPopup, Logger ) {
+const storeDeviceSettings = function () {
+  var lang = i18next.resolvedLanguage;
+  var manufacturer = window['device'].manufacturer;
+  var osver = window['device'].version;
+  return window['cordova'].getAppVersion.getVersionNumber().then(function (appver) {
+    var updateJSON = {
+      phone_lang: lang,
+      curr_platform: window['cordova'].platformId,
+      manufacturer: manufacturer,
+      client_os_version: osver,
+      client_app_version: appver
+    };
+    logDebug("About to update profile with settings = " + JSON.stringify(updateJSON));
+    return updateUser(updateJSON);
+  }).then(function (updateJSON) {
+    // alert("Finished saving token = "+JSON.stringify(t.token));
+  }).catch(function (error) {
+    displayError(error, "Error in updating profile to store device settings");
+  });
+}
 
-    var storedevicesettings = {};
-
-    storedevicesettings.storeDeviceSettings = function() {
-      var lang = i18next.resolvedLanguage;
-      var manufacturer = $window.device.manufacturer;
-      var osver = $window.device.version;
-      return $window.cordova.getAppVersion.getVersionNumber().then(function(appver) {
-        var updateJSON = {
-          phone_lang: lang,
-          curr_platform: ionic.Platform.platform(),
-          manufacturer: manufacturer,
-          client_os_version: osver,
-          client_app_version: appver
-        };
-        Logger.log("About to update profile with settings = "+JSON.stringify(updateJSON));
-        return updateUser(updateJSON);
-      }).then(function(updateJSON) {
-         // alert("Finished saving token = "+JSON.stringify(t.token));
-      }).catch(function(error) {
-        Logger.displayError("Error in updating profile to store device settings", error);
-      });
-    }
-
-    $ionicPlatform.ready().then(function() {
-      storedevicesettings.datacollect = $window.cordova.plugins.BEMDataCollection;
-      readConsentState()
-        .then(isConsented)
-        .then(function(consentState) {
-          if (consentState == true) {
-              storedevicesettings.storeDeviceSettings();
-          } else {
-            Logger.log("no consent yet, waiting to store device settings in profile");
-          }
-        });
-      Logger.log("storedevicesettings startup done");
+/**
+ * @function stores device settings on reconsent
+ * @param event that called this function
+ * @param data data from the conesnt event
+ */
+ const onConsentEvent = function (event, data) {
+  console.log("got consented event " + JSON.stringify(event['name'])
+    + " with data " + JSON.stringify(data));
+  readIntroDone()
+    .then((isIntroDone) => {
+      if (isIntroDone) {
+        logDebug("intro is done -> reconsent situation, we already have a token -> store device settings");
+        storeDeviceSettings();
+      }
     });
+}
 
-    return storedevicesettings;
-});
+/**
+ * @function stores device settings after intro received
+ * @param event that called this function
+ * @param data from the event
+ */
+const onIntroEvent = function (event, data) {
+  logDebug("intro is done -> original consent situation, we should have a token by now -> store device settings");
+  storeDeviceSettings();
+}
+
+const initStoreDeviceSettings = function () {
+  readConsentState()
+    .then(isConsented)
+    .then(function (consentState) {
+      if (consentState == true) {
+        storeDeviceSettings();
+      } else {
+        logDebug("no consent yet, waiting to store device settings in profile");
+      }
+      subscribe(EVENT_NAMES.CONSENTED_EVENT, (event) => onConsentEvent(event, event.detail));
+      subscribe(EVENT_NAMES.INTRO_DONE_EVENT, (event) => onIntroEvent(event, event.detail));
+    });
+  logDebug("storedevicesettings startup done");
+}
