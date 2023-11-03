68e0701dad82dcc796cbadc7d7b5b0321f1a9843
Abby Wheelis <abby.wheelis@centre.edu>
2023-11-03 12:03:38 -0600

    formatting changes to controller.js
    prettier wanted to make a bunch of changes to code that I didn't edit, so putting it in a dedicated commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,67 +11,66 @@ angular
 
   .controller('DashCtrl', function ($scope) {})
 
-      // alert("attach debugger!");
-      // PushNotify.startupInit();
+  // alert("attach debugger!");
+  // PushNotify.startupInit();
   .controller('SplashCtrl', function ($scope, $state, $interval, $rootScope, LocalNotify) {
     console.log('SplashCtrl invoked');
 
-      $rootScope.$on(
-        '$stateChangeSuccess',
-        function (event, toState, toParams, fromState, fromParams) {
-          console.log(
-            'Finished changing state from ' +
-              JSON.stringify(fromState) +
-              ' to ' +
-              JSON.stringify(toState),
-          );
-          addStatReading(statKeys.STATE_CHANGED, fromState.name + '-2-' + toState.name);
-        },
-      );
-      $rootScope.$on(
-        '$stateChangeError',
-        function (event, toState, toParams, fromState, fromParams, error) {
-          console.log(
-            'Error ' +
-              error +
-              ' while changing state from ' +
-              JSON.stringify(fromState) +
-              ' to ' +
-              JSON.stringify(toState),
-          );
-          addStatError(statKeys.STATE_CHANGED, fromState.name + '-2-' + toState.name + '_' + error);
-        },
-      );
-      $rootScope.$on('$stateNotFound', function (event, unfoundState, fromState, fromParams) {
-        console.log('unfoundState.to = ' + unfoundState.to); // "lazy.state"
-        console.log('unfoundState.toParams = ' + unfoundState.toParams); // {a:1, b:2}
-        console.log('unfoundState.options = ' + unfoundState.options); // {inherit:false} + default options
-        addStatError(statKeys.STATE_CHANGED, fromState.name + '-2-' + unfoundState.name);
-      });
+    $rootScope.$on(
+      '$stateChangeSuccess',
+      function (event, toState, toParams, fromState, fromParams) {
+        console.log(
+          'Finished changing state from ' +
+            JSON.stringify(fromState) +
+            ' to ' +
+            JSON.stringify(toState),
+        );
+        addStatReading(statKeys.STATE_CHANGED, fromState.name + '-2-' + toState.name);
+      },
+    );
+    $rootScope.$on(
+      '$stateChangeError',
+      function (event, toState, toParams, fromState, fromParams, error) {
+        console.log(
+          'Error ' +
+            error +
+            ' while changing state from ' +
+            JSON.stringify(fromState) +
+            ' to ' +
+            JSON.stringify(toState),
+        );
+        addStatError(statKeys.STATE_CHANGED, fromState.name + '-2-' + toState.name + '_' + error);
+      },
+    );
+    $rootScope.$on('$stateNotFound', function (event, unfoundState, fromState, fromParams) {
+      console.log('unfoundState.to = ' + unfoundState.to); // "lazy.state"
+      console.log('unfoundState.toParams = ' + unfoundState.toParams); // {a:1, b:2}
+      console.log('unfoundState.options = ' + unfoundState.options); // {inherit:false} + default options
+      addStatError(statKeys.STATE_CHANGED, fromState.name + '-2-' + unfoundState.name);
+    });
 
-      var isInList = function (element, list) {
-        return list.indexOf(element) != -1;
-      };
+    var isInList = function (element, list) {
+      return list.indexOf(element) != -1;
+    };
 
-      $rootScope.$on(
-        '$stateChangeStart',
-        function (event, toState, toParams, fromState, fromParams, options) {
-          var personalTabs = ['root.main.common.map', 'root.main.control', 'root.main.metrics'];
-          if (isInList(toState.name, personalTabs)) {
-            // toState is in the personalTabs list
-            getPendingOnboardingState().then(function (result) {
-              if (result != null) {
-                event.preventDefault();
-                $state.go(result);
-              }
-              // else, will do default behavior, which is to go to the tab
-            });
-          }
-        },
-      );
-      console.log('SplashCtrl invoke finished');
-    },
-  )
+    $rootScope.$on(
+      '$stateChangeStart',
+      function (event, toState, toParams, fromState, fromParams, options) {
+        var personalTabs = ['root.main.common.map', 'root.main.control', 'root.main.metrics'];
+        if (isInList(toState.name, personalTabs)) {
+          // toState is in the personalTabs list
+          getPendingOnboardingState().then(function (result) {
+            if (result != null) {
+              event.preventDefault();
+              $state.go(result);
+            }
+            // else, will do default behavior, which is to go to the tab
+          });
+        }
+      },
+    );
+    console.log('SplashCtrl invoke finished');
+  })
 
   .controller('ChatsCtrl', function ($scope, Chats) {
     // With the new view caching in Ionic, Controllers are only called
