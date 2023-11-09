7820646de8433ab19c2eadbce5389095ac0a559a
Abby Wheelis <abby.wheelis@centre.edu>
2023-11-09 10:13:09 -0700

    remove old MET code
    we no longer store user's height/weight/etc because we are not calculating calories burned, for that reason the code related to storing user's data or calculating calories is not needed
    
    we will keep MET handling so we can bin activity into high/med/low intensity later
    
    not currently using the "highestMET" calculation code, so commenting that out for now

--- www/js/metrics/metHelper.ts
+++ www/js/metrics/metHelper.ts
@@ -1,12 +1,9 @@
 import { getCustomMETs } from './customMetricsHelper';
 import { standardMETs } from './metDataset';
-import { storageGet, storageSet, storageRemove } from '../plugin/storage';
 
-var highestMET = 0;
-var USER_DATA_KEY = 'user-data';
 let useCustom = false;
 
-const setUseCustomMET = function () {
+export const setUseCustomMET = function () {
   useCustom = true;
 };
 
@@ -18,38 +15,15 @@ const getMETs = function () {
   }
 };
 
-const set = function (info) {
-  return storageSet(USER_DATA_KEY, info);
-};
-
-const get = function () {
-  return storageGet(USER_DATA_KEY);
-};
-
-const remove = function () {
-  return storageRemove(USER_DATA_KEY);
-};
-
 const between = function (num, min, max) {
   return num >= min && num <= max;
 };
 
-const getHighestMET = function () {
-  if (!highestMET) {
-    var met = getMETs();
-    let metList = [];
-    for (var mode in met) {
-      var rangeList = met[mode];
-      for (var range in rangeList) {
-        metList.push(rangeList[range].mets);
-      }
-    }
-    highestMET = Math.max(...metList);
-  }
-  return highestMET;
+const mpstomph = function (mps) {
+  return 2.23694 * mps;
 };
 
-const getMet = function (mode, speed, defaultIfMissing) {
+export const getMet = function (mode, speed, defaultIfMissing) {
   if (mode == 'ON_FOOT') {
     console.log("getMet() converted 'ON_FOOT' to 'WALKING'");
     mode = 'WALKING';
@@ -69,41 +43,18 @@ const getMet = function (mode, speed, defaultIfMissing) {
   }
 };
 
-const mpstomph = function (mps) {
-  return 2.23694 * mps;
-};
-
-const lbtokg = function (lb) {
-  return lb * 0.453592;
-};
-
-const fttocm = function (ft) {
-  return ft * 30.48;
-};
-
-const getCorrectedMet = function (met, gender, age, height, heightUnit, weight, weightUnit) {
-  var height = heightUnit == 0 ? fttocm(height) : height;
-  var weight = weightUnit == 0 ? lbtokg(weight) : weight;
-  let calcMet;
-  if (gender == 1) {
-    //male
-    calcMet =
-      (met * 3.5) /
-      (((66.473 + 5.0033 * height + 13.7516 * weight - 6.755 * age) / 1440 / 5 / weight) * 1000);
-    return met;
-  } else if (gender == 0) {
-    //female
-    let met =
-      (calcMet * 3.5) /
-      (((655.0955 + 1.8496 * height + 9.5634 * weight - 4.6756 * age) / 1440 / 5 / weight) * 1000);
-    return calcMet;
-  }
-};
-
-const getuserCalories = function (durationInMin, met) {
-  return 65 * durationInMin * met;
-};
-
-const getCalories = function (weightInKg, durationInMin, met) {
-  return weightInKg * durationInMin * met;
-};
+// var highestMET = 0;
+// const getHighestMET = function () {
+//   if (!highestMET) {
+//     var met = getMETs();
+//     let metList = [];
+//     for (var mode in met) {
+//       var rangeList = met[mode];
+//       for (var range in rangeList) {
+//         metList.push(rangeList[range].mets);
+//       }
+//     }
+//     highestMET = Math.max(...metList);
+//   }
+//   return highestMET;
+// };
