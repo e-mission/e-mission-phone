95ffe34a1502e2497a311bf4efe021c3350e7ce1
Jijeong Lee <jijeong0114@gmail.com>
2023-11-10 12:36:50 -0800

    Apply prettier to e2e-tests directory

--- e2e-tests/specs/sample.js
+++ e2e-tests/specs/sample.js
@@ -3,8 +3,10 @@ const { expect, $ } = require('@wdio/globals');
 describe('Connect test', () => {
   it('should call app successfully', async () => {
     const selectorIOS = await $('UIATarget.localTarget().frontMostApp().mainWindow()');
-    const selectorAndroid = await $('android=new UiSelector().className("android.widget.FrameLayout")');
-    const selector = driver.isAndroid ? selectorAndroid : selectorIOS
+    const selectorAndroid = await $(
+      'android=new UiSelector().className("android.widget.FrameLayout")',
+    );
+    const selector = driver.isAndroid ? selectorAndroid : selectorIOS;
     expect(selector).toBeDisplayed();
   });
 });
