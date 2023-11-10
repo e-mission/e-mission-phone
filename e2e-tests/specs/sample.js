bfa1bd3622b0ce613876751d22c3323f1d7be8f9
Jiji14 <jijeong0114@gmail.com>
2023-11-09 22:43:54 -0800

    add Android selector and detect if device is android or ios

--- e2e-tests/specs/sample.js
+++ e2e-tests/specs/sample.js
@@ -2,8 +2,9 @@ const { expect, $ } = require('@wdio/globals');
 
 describe('Connect test', () => {
   it('should call app successfully', async () => {
-    // todo : add selectorAndroid
     const selectorIOS = await $('UIATarget.localTarget().frontMostApp().mainWindow()');
-    expect(selectorIOS).toBeDisplayed();
+    const selectorAndroid = await $('android=new UiSelector().className("android.widget.FrameLayout")');
+    const selector = driver.isAndroid ? selectorAndroid : selectorIOS
+    expect(selector).toBeDisplayed();
   });
 });
