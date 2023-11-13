7d66e57a10d868d17d461d37f80fbeb176fdc291
Nitish Ramakrishnan <write2gamenugget@gmail.com>
2023-11-13 10:45:44 -0700

    ran prettier

--- www/js/control/emailService.ts
+++ www/js/control/emailService.ts
@@ -1,31 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import i18next from "i18next";
-import { logInfo, logDebug, displayError } from "../plugin/logger";
+import i18next from 'i18next';
+import { logInfo, logDebug, displayError } from '../plugin/logger';
 
 async function hasAccount(): Promise<boolean> {
   return new Promise<boolean>((resolve, reject) => {
-    window['cordova'].plugins['email'].hasAccount(hasAct => {
+    window['cordova'].plugins['email'].hasAccount((hasAct) => {
       resolve(hasAct);
     });
   });
 }
 
 export async function sendEmail(database: string) {
-  let parentDir = "unknown";
+  let parentDir = 'unknown';
 
-  if (window['ionic'].Platform.isIOS() && !(await hasAccount())) { 
+  if (window['ionic'].Platform.isIOS() && !(await hasAccount())) {
     alert(i18next.t('email-service.email-account-not-configured'));
     return;
   }
 
   if (window['ionic'].Platform.isAndroid()) {
-    parentDir = "app://databases";
+    parentDir = 'app://databases';
   }
 
   if (window['ionic'].Platform.isIOS()) {
     alert(i18next.t('email-service.email-account-mail-app'));
     console.log(window['cordova'].file.dataDirectory);
-    parentDir = window['cordova'].file.dataDirectory + "../LocalDatabase";
+    parentDir = window['cordova'].file.dataDirectory + '../LocalDatabase';
   }
 
   if (parentDir === 'unknown') {
@@ -43,10 +43,14 @@ export async function sendEmail(database: string) {
     to: emailConfig,
     attachments: [parentDir],
     subject: i18next.t('email-service.email-log.subject-logs'),
-    body: i18next.t('email-service.email-log.body-please-fill-in-what-is-wrong')
+    body: i18next.t('email-service.email-log.body-please-fill-in-what-is-wrong'),
   };
 
   window['cordova'].plugins['email'].open(emailData, () => {
-    logInfo('Email app closed while sending, ' + JSON.stringify(emailData) + ' not sure if we should do anything');
+    logInfo(
+      'Email app closed while sending, ' +
+        JSON.stringify(emailData) +
+        ' not sure if we should do anything',
+    );
   });
 }
