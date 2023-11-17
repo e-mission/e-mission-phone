6272fb0d11d6e9e1558798f3cc2a7e865b6c17ff
Abby Wheelis <abby.wheelis@centre.edu>
2023-11-17 14:44:13 -0700

    logDebug statements
    logDebug/logWarn instead of the console equivalents

--- www/js/metrics/metHelper.ts
+++ www/js/metrics/metHelper.ts
@@ -1,3 +1,4 @@
+import { logDebug, logWarn } from '../plugin/logger';
 import { getCustomMETs } from './customMetricsHelper';
 import { standardMETs } from './metDataset';
 
@@ -52,19 +53,19 @@ const mpstomph = function (mps) {
  */
 export const getMet = function (mode, speed, defaultIfMissing) {
   if (mode == 'ON_FOOT') {
-    console.log("getMet() converted 'ON_FOOT' to 'WALKING'");
+    logDebug("getMet() converted 'ON_FOOT' to 'WALKING'");
     mode = 'WALKING';
   }
   let currentMETs = getMETs();
   if (!currentMETs[mode]) {
-    console.warn('getMet() Illegal mode: ' + mode);
+    logWarn('getMet() Illegal mode: ' + mode);
     return defaultIfMissing; //So the calorie sum does not break with wrong return type
   }
   for (var i in currentMETs[mode]) {
     if (between(mpstomph(speed), currentMETs[mode][i].range[0], currentMETs[mode][i].range[1])) {
       return currentMETs[mode][i].mets;
     } else if (mpstomph(speed) < 0) {
-      console.log('getMet() Negative speed: ' + mpstomph(speed));
+      logWarn('getMet() Negative speed: ' + mpstomph(speed));
       return 0;
     }
   }
