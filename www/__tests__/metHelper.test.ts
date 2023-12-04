23e0270cdfbf0e369aa325823ac0d18302f377c0
Abby Wheelis <abby.wheelis@centre.edu>
2023-12-04 14:35:59 -0700

    cover edge cases in metHelper
    checking the 'ON_FOOT' case
    verifying negative speed returns 0, even if there is a fallback

--- www/__tests__/metHelper.test.ts
+++ www/__tests__/metHelper.test.ts
@@ -26,6 +26,8 @@ it('gets met for mode and speed', () => {
   expect(getMet('BICYCLING', 4.5, 0)).toBe(6.8); //4.5 mps = 10.07 mph =  6.8 METs
   expect(getMet('UNICYCLE', 100, 0)).toBe(0); //unkown mode, 0 METs
   expect(getMet('CAR', 25, 1)).toBe(0); //0 METs in CAR
+  expect(getMet('ON_FOOT', 1.47523, 0)).toBe(4.3); //same as walking!
+  expect(getMet('WALKING', -2, 0)).toBe(0); //negative speed -> 0
 });
 
 it('gets custom met for mode and speed', async () => {
@@ -37,4 +39,5 @@ it('gets custom met for mode and speed', async () => {
   expect(getMet('drove_alone', 25, 1)).toBe(0); //0 METs IN_VEHICLE
   expect(getMet('e-bike', 6, 1)).toBe(4.9); //e-bike is 4.9 for all speeds
   expect(getMet('e-bike', 12, 1)).toBe(4.9); //e-bike is 4.9 for all speeds
+  expect(getMet('walk', -2, 1)).toBe(0); //negative speed -> 0
 });
