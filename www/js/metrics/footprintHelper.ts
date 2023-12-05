5a0467b6947b9eada6cb8f3ef798fbd1faa1547a
Abby Wheelis <abby.wheelis@centre.edu>
2023-12-05 14:15:43 -0700

    remove deprecated code
    After we talked about it in our meeting today, I'm going to go ahead and take this out, since we now have custom modes, really, either it's in the list or it is not, so we can't do this type of fallback anymore (at least not right now) it could be something to think about as we overhaul the metrics in upcoming months

--- www/js/metrics/footprintHelper.ts
+++ www/js/metrics/footprintHelper.ts
@@ -47,18 +47,9 @@ export function getFootprintForMetrics(userMetrics, defaultIfMissing = 0) {
   for (let i in userMetrics) {
     let mode = userMetrics[i].key;
 
+    //either the mode is in our custom footprint or it is not
     if (mode in footprint) {
       result += footprint[mode] * mtokm(userMetrics[i].values);
-    } else if (mode == 'IN_VEHICLE') {
-      //this... could be deprecated
-      const sum =
-        footprint['CAR'] +
-        footprint['BUS'] +
-        footprint['LIGHT_RAIL'] +
-        footprint['TRAIN'] +
-        footprint['TRAM'] +
-        footprint['SUBWAY'];
-      result += (sum / 6) * mtokm(userMetrics[i].values);
     } else {
       logWarn(
         `WARNING getFootprintFromMetrics() was requested for an unknown mode: ${mode} metrics JSON: ${JSON.stringify(
