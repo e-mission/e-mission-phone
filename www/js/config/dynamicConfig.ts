b011bcae895bb5966fb0e52ceb4379ee46c82672
Jack Greenlee <greenljk@mail.uc.edu>
2023-12-09 00:29:28 -0500

    add JSdoc and types to fns in dynamicConfigs.ts

--- www/js/config/dynamicConfig.ts
+++ www/js/config/dynamicConfig.ts
@@ -16,7 +16,12 @@ export const _test_resetStoredConfig = () => {
   storedConfig = null;
 };
 
-function _getStudyName(connectUrl) {
+/**
+ * @param connectUrl The URL endpoint specified in the config
+ * @returns The study name (like 'stage' or whatever precedes 'openpath' in the URL),
+ *   or undefined if it can't be determined
+ */
+function _getStudyName(connectUrl: `https://${string}`) {
   const orig_host = new URL(connectUrl).hostname;
   const first_domain = orig_host.split('.')[0];
   if (first_domain == 'openpath-stage') {
@@ -30,6 +35,10 @@ function _getStudyName(connectUrl) {
   return study_name;
 }
 
+/**
+ * @param config The app config which might be missing 'name'
+ * @returns Shallow copy of the app config with 'name' filled in if it was missing
+ */
 function _fillStudyName(config: Partial<AppConfig>): AppConfig {
   if (config.name) return config as AppConfig;
   if (config.server) {
@@ -39,6 +48,10 @@ function _fillStudyName(config: Partial<AppConfig>): AppConfig {
   }
 }
 
+/**
+ * @param config The app config which might be missing 'survey_info'
+ * @returns Shallow copy of the app config with the default 'survey_info' filled in if it was missing
+ */
 function _backwardsCompatSurveyFill(config: Partial<AppConfig>): AppConfig {
   if (config.survey_info) return config as AppConfig;
   return {
@@ -61,11 +74,14 @@ function _backwardsCompatSurveyFill(config: Partial<AppConfig>): AppConfig {
   } as AppConfig;
 }
 
-/* Fetch and cache any surveys resources that are referenced by URL in the config,
-  as well as the label_options config if it is present.
-  This way they will be available when the user needs them, and we won't have to
-  fetch them again unless local storage is cleared. */
-function cacheResourcesFromConfig(config) {
+/**
+ * @description Fetch and cache any surveys resources that are referenced by URL in the config,
+ *   as well as the label_options config if it is present.
+ *   This way they will be available when the user needs them, and we won't have to
+ *   fetch them again unless local storage is cleared.
+ * @param config The app config
+ */
+function cacheResourcesFromConfig(config: AppConfig) {
   if (config.survey_info?.surveys) {
     Object.values(config.survey_info.surveys).forEach((survey) => {
       if (!survey?.['formPath']) throw new Error(i18next.t('config.survey-missing-formpath'));
@@ -77,8 +93,14 @@ function cacheResourcesFromConfig(config) {
   }
 }
 
-async function readConfigFromServer(label) {
-  const fetchedConfig = await fetchConfig(label);
+/**
+ * @description Fetch the config from the server, fill in any missing fields, and cache any
+ *  resources referenced in the config
+ * @param studyLabel The 'label' of the study, like 'open-access' or 'dev-emulator-study'
+ * @returns The filled in app config
+ */
+async function readConfigFromServer(studyLabel: string) {
+  const fetchedConfig = await fetchConfig(studyLabel);
   logDebug(`Successfully found config,
     fetchedConfig = ${JSON.stringify(fetchedConfig).substring(0, 10)}`);
   const filledConfig = _backwardsCompatSurveyFill(_fillStudyName(fetchedConfig));
@@ -96,23 +118,30 @@ async function readConfigFromServer(label) {
   return filledConfig;
 }
 
-async function fetchConfig(label, alreadyTriedLocal = false) {
-  logDebug('Received request to join ' + label);
-  const downloadURL = `https://raw.githubusercontent.com/e-mission/nrel-openpath-deploy-configs/main/configs/${label}.nrel-op.json`;
+/**
+ * @description Fetch the config for a particular study, either from github, or if in dev mode, from
+ *  localhost:9090 and github if that fails
+ * @param studyLabel The 'label' of the study, like 'open-access' or 'dev-emulator-study'
+ * @param alreadyTriedLocal Flag for dev environment, if true, will try to fetch from github
+ * @returns The fetched app config
+ */
+async function fetchConfig(studyLabel: string, alreadyTriedLocal?: boolean) {
+  logDebug('Received request to join ' + studyLabel);
+  const downloadURL = `https://raw.githubusercontent.com/e-mission/nrel-openpath-deploy-configs/main/configs/${studyLabel}.nrel-op.json`;
   if (!__DEV__ || alreadyTriedLocal) {
     logDebug('Fetching config from github');
     const r = await fetch(downloadURL);
     if (!r.ok) throw new Error('Unable to fetch config from github');
-    return r.json();
+    return r.json(); // TODO: validate, make sure it has required fields
   } else {
     logDebug('Running in dev environment, checking for locally hosted config');
     try {
-      const r = await fetch('http://localhost:9090/configs/' + label + '.nrel-op.json');
+      const r = await fetch('http://localhost:9090/configs/' + studyLabel + '.nrel-op.json');
       if (!r.ok) throw new Error('Local config not found');
       return r.json();
     } catch (err) {
       logDebug('Local config not found');
-      return fetchConfig(label, true);
+      return fetchConfig(studyLabel, true);
     }
   }
 }
@@ -129,7 +158,7 @@ async function fetchConfig(label, alreadyTriedLocal = false) {
  *
  * So let's support two separate functions here - extractStudyName and extractSubgroup
  */
-function extractStudyName(token) {
+function extractStudyName(token: string): string {
   const tokenParts = token.split('_');
   if (tokenParts.length < 3) {
     // all tokens must have at least nrelop_[study name]_...
@@ -141,7 +170,7 @@ function extractStudyName(token) {
   return tokenParts[1];
 }
 
-function extractSubgroup(token, config) {
+function extractSubgroup(token: string, config: AppConfig): string {
   if (config.opcode) {
     // new style study, expects token with sub-group
     const tokenParts = token.split('_');
@@ -185,13 +214,12 @@ function extractSubgroup(token, config) {
 }
 
 /**
- * loadNewConfig download and load a new config from the server if it is a differ
- * @param {[]} newToken the new token, which includes parts for the study label, subgroup, and user
- * @param {} thenGoToIntro whether to go to the intro screen after loading the config
- * @param {} [existingVersion=null] if the new config's version is the same, we won't update
- * @returns {boolean} boolean representing whether the config was updated or not
+ * @description Download and load a new config from the server if it is a different version
+ * @param newToken The new token, which includes parts for the study label, subgroup, and user
+ * @param existingVersion If the new config's version is the same, we won't update
+ * @returns boolean representing whether the config was updated or not
  */
-function loadNewConfig(newToken, existingVersion = null) {
+function loadNewConfig(newToken: string, existingVersion?: number): Promise<boolean> {
   const newStudyLabel = extractStudyName(newToken);
   return readConfigFromServer(newStudyLabel)
     .then((downloadedConfig) => {
@@ -223,16 +251,19 @@ function loadNewConfig(newToken, existingVersion = null) {
           configChanged = true;
           return true;
         })
-        .catch((storeError) =>
-          displayError(storeError, i18next.t('config.unable-to-store-config')),
-        );
+        .catch((storeError) => {
+          displayError(storeError, i18next.t('config.unable-to-store-config'));
+          return false;
+        });
     })
     .catch((fetchErr) => {
       displayError(fetchErr, i18next.t('config.unable-download-config'));
+      return false;
     });
 }
 
-export function initByUser(urlComponents) {
+// exported wrapper around loadNewConfig that includes error handling
+export function initByUser(urlComponents: { token: string }) {
   const { token } = urlComponents;
   try {
     return loadNewConfig(token).catch((fetchErr) => {
@@ -244,12 +275,15 @@ export function initByUser(urlComponents) {
   }
 }
 
-export function resetDataAndRefresh() {
-  // const resetNativePromise = window['cordova'].plugins.BEMUserCache.putRWDocument(CONFIG_PHONE_UI, {});
+/** @description Clears all local and native storage, then triggers a refresh */
+export const resetDataAndRefresh = () =>
   storageClear({ local: true, native: true }).then(() => window.location.reload());
-}
 
-export function getConfig() {
+/**
+ * @returns The app config, either from a cached copy, retrieved from local storage, or retrieved
+ *   from user cache with getDocument()
+ */
+export function getConfig(): Promise<AppConfig> {
   if (storedConfig) return Promise.resolve(storedConfig);
   return storageGet(CONFIG_PHONE_UI_KVSTORE).then((config) => {
     if (config && Object.keys(config).length) {
