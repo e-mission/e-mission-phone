fe236fd5acae064df2bd03a8aea630c9fd1dcbf0
Jijeong Lee <jijeong0114@gmail.com>
2024-06-05 18:01:05 -0700

    import 'setImmediate' to test 'react-native-gesture-handler' from DatePickerModal
    reference - https://github.com/necolas/react-native-web/issues/1891

--- www/js/diary/list/DateSelect.tsx
+++ www/js/diary/list/DateSelect.tsx
@@ -20,6 +20,8 @@ import i18next from 'i18next';
 import { useTranslation } from 'react-i18next';
 import { NavBarButton } from '../../components/NavBar';
 import { isoDateRangeToTsRange } from '../timelineHelper';
+// we need to import 'setImmediate' to test 'react-native-gesture-handler' from DatePickerModal
+import 'setimmediate';
 
 type Props = Partial<DatePickerModalSingleProps | DatePickerModalRangeProps> & {
   mode: 'single' | 'range';
