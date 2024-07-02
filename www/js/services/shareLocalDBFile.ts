d5ab4aba3b25999e031105c98d5548ec4da048c8
louisg1337 <louisgrassi1337@gmail.com>
2024-07-02 14:54:56 -0400

    Added in fix to get Android to work

--- www/js/services/shareLocalDBFile.ts
+++ www/js/services/shareLocalDBFile.ts
@@ -6,8 +6,8 @@ function localDBHelpers(fileName: string, fileExtension: string = '.txt') {
     return new Promise<void>((resolve, reject) => {
       let pathToFile, parentDirectory;
       if (window['cordova'].platformId == 'android') {
-        parentDirectory = window['cordova'].file.dataDirectory;
-        pathToFile = fileName;
+        parentDirectory = window['cordova'].file.dataDirectory.replace('files', 'databases')
+        pathToFile = parentDirectory.replace('file://', '') + fileName;
       } else if (window['cordova'].platformId == 'ios') {
         parentDirectory = window['cordova'].file.dataDirectory + '../';
         pathToFile = 'LocalDatabase/' + fileName;
