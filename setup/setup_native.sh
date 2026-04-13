d27ef1c4d6229639c1ce57f2a2e45c9f10a68261
Jack Greenlee <15843932+JGreenlee@users.noreply.github.com>
2026-04-13 18:54:37 -0400

    🗑️ Remove unneeded config files and scripts

--- setup/setup_native.sh
+++ setup/setup_native.sh
@@ -84,8 +84,6 @@ else
     sdk install gradle $GRADLE_VERSION
 fi
 
-./bin/configure_xml_and_json.js cordovabuild
-
 echo "Copying fake FCM configurations for android and iOS"
 cp setup/GoogleService-Info.fake.for_ci.plist GoogleService-Info.plist
 cp setup/google-services.fake.for_ci.json google-services.json
